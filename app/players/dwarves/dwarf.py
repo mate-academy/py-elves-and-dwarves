18c4cf1dcc05b3692b17785234817db5ca52d3c0
NikitaZH1812 <zhukovna1812@gmail.com>
2022-01-27 21:17:24 +0200

    fixes for favourite_dish and Dwarf and Elf classes

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,10 +1,11 @@
 from app.players.player import Player
+from abc import ABC
 
 
-class Dwarf(Player):
+class Dwarf(Player, ABC):
     def __init__(self, favourite_dish, nickname):
         super().__init__(nickname)
-        self.__favourite_dish = favourite_dish
+        self._favourite_dish = favourite_dish
 
     def eat_favourite_dish(self):
-        print(f"{self.nickname} is eating {self.__favourite_dish}")
+        print(f"{self.nickname} is eating {self._favourite_dish}")
