17f9565258fed97c4b9f10286d0d566b2114171e
NikitaZH1812 <zhukovna1812@gmail.com>
2022-01-27 20:57:05 +0200

    solution

---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,14 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, favourite_dish, skill_level, nickname):
+        super().__init__(favourite_dish, nickname)
+        self._skill_level = skill_level
+
+    def player_info(self):
+        return f"Dwarf blacksmith {self.nickname}" \
+               f" with skill of the {self._skill_level} level"
+
+    def get_rating(self):
+        return self._skill_level
---
+++ app/players/elves/druid.py
@@ -0,0 +1,14 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(self, musical_instrument, favourite_spell, nickname):
+        super().__init__(musical_instrument, nickname)
+        self._favourite_spell = favourite_spell
+
+    def player_info(self):
+        return f"Druid {self.nickname}. {self.nickname}" \
+               f" has a favourite spell: {self._favourite_spell}"
+
+    def get_rating(self):
+        return len(self._favourite_spell)
