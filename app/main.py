a393adc0560863fc8168aceeb20e6689961386b1
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-27 16:22:13 +0200

    list_elf -> list_elves

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def calculate_team_total_rating(list_player: list):
     return sum([player.get_rating() for player in list_player])
 
 
-def elves_concert(list_elf: list):
-    for elf in list_elf:
+def elves_concert(list_elves: list):
+    for elf in list_elves:
         elf.play_elf_song()
 
 
