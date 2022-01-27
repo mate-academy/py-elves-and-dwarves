6a4382aad4a59e9d08539cccd62f8d8649b0de5a
Rybak Dmytro <codingclaud@gmail.com>
2022-01-27 20:21:23 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ def calculate_team_total_rating(teams: list):
     return sum(team.get_rating() for team in teams)
 
 
-def elves_concert(elfs: tuple):
-    for elf in elfs:
-        elf.play_elf_song()
+def elves_concert(elves: tuple):
+    for elv in elves:
+        elv.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list):
-    for dwarf in dwarfs:
+def feast_of_the_dwarves(dwarves: list):
+    for dwarf in dwarves:
         dwarf.eat_favourite_dish()
