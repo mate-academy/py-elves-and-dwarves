579812c219df9aa33c9acae1d9dacba6f2c92908
Nazar-kob <kobrinnazar05@mail.com>
2022-01-27 16:00:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(team: list):
+    return sum([some_one.get_rating() for some_one in team])
+
+
+def elves_concert(team: list):
+    for some_elf in team:
+        some_elf.play_elf_song()
+
+
+def feast_of_the_dwarves(team: list):
+    for some_dwarve in team:
+        some_dwarve.eat_favourite_dish()
