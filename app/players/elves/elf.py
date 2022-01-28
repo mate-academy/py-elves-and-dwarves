113a526331da6f602461e551cefcbba18468d24f
Denys Lipin <lipin.transstream@gmail.com>
2022-01-28 12:50:11 +0200

    'Solution2'

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,9 +1,9 @@
-from abc import abstractmethod
+from abc import ABC
 
 from app.players.player import Player
 
 
-class Elf(Player):
+class Elf(Player, ABC):
     def __init__(self, nickname: str, musical_instrument: str):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
@@ -11,11 +11,3 @@ class Elf(Player):
     def play_elf_song(self):
         print(f"{self.nickname} is playing a song on the "
               f"{self._musical_instrument}")
-
-    @abstractmethod
-    def get_rating(self):
-        pass
-
-    @abstractmethod
-    def player_info(self):
-        pass
