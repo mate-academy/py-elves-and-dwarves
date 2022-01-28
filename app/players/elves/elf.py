e7ce1a360b27985b78734d2d708835ac4424a411
nikon4uk <nikonchukmax4@gmail.com>
2022-01-28 18:36:13 +0200

    was fixed eld and dwarf classes

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,19 +1,10 @@
 from app.players.player import Player
-from abc import ABC, abstractmethod
 
 
-class Elf(Player, ABC):
+class Elf(Player):
     def __init__(self, nickname: str, musical_instrument: str):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
-
-    @abstractmethod
-    def get_rating(self):
-        pass
-
-    @abstractmethod
-    def player_info(self):
-        pass
 
     def play_elf_song(self):
         print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
