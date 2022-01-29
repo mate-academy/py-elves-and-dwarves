b9abb4afe3e05e85910ed57bdab235f2b8ab1d33
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-01-29 23:42:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+"""
+This main.py module contains three function. Each
+of them takes a list of instances of players (both
+elves and dwarves), and performs some of their methods.
+Methods can print or return string, or return an integer,
+to provide some information about a player.
+"""
+
+
+def calculate_team_total_rating(players):
+    return sum(
+        [
+            player.get_rating() for player in players
+        ]
+    )
+
+
+def elves_concert(elves):
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves):
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,29 @@
+"""
+This is specific class for dwarf-blacksmith.
+It inherits all the methods from Player and
+Dwarf classes, and adds one additional method
+particularly for the dwarf-blacksmith. All of player`s
+instances should be created from this class, if
+dwarf-blacksmith type is choosed to play with.
+"""
+
+
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(
+            self,
+            nickname: str,
+            favourite_dish: str,
+            skill_level: int
+    ):
+        super().__init__(nickname, favourite_dish)
+        self._skill_level = skill_level
+
+    def get_rating(self):
+        return self._skill_level
+
+    def player_info(self):
+        return f"Dwarf blacksmith {self.nickname} with" \
+               f" skill of the {self._skill_level} level"
