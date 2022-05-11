0b2fe25d91f129e177656d297ce4a4fe8b17cbff
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-11 22:29:38 +0300

    cleaned up file main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
-# from app.players.elves.elf_ranger import ElfRanger
-# from app.players.elves.druid import Druid
-# from app.players.dwarves.dwarf_warrior import DwarfWarrior
-#
-
 def calculate_team_total_rating(heros: list):
     return sum([hero.get_rating() for hero in heros])
 
