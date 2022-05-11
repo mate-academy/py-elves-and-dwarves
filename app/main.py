33e558e7fa146aced3ded3f40aa840298555d996
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-11 22:26:27 +0300

    Soution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+# from app.players.elves.elf_ranger import ElfRanger
+# from app.players.elves.druid import Druid
+# from app.players.dwarves.dwarf_warrior import DwarfWarrior
+#
+
+def calculate_team_total_rating(heros: list):
+    return sum([hero.get_rating() for hero in heros])
+
+
+def elves_concert(elfs: list):
+    for elf in elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarfs: list):
+    for dwarf in dwarfs:
+        dwarf.eat_favourite_dish()
---
+++ app/players/elves/druid.py
@@ -0,0 +1,16 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+
+    def __init__(self, nickname, musical_instrument, favourite_spell):
+        super().__init__(nickname, musical_instrument)
+        self.__favourite_spell = favourite_spell
+
+    def player_info(self):
+        return f"Druid {self.nickname}." \
+               f" {self.nickname} has a favourite spell:" \
+               f" {self.__favourite_spell}"
+
+    def get_rating(self):
+        return len(self.__favourite_spell)
