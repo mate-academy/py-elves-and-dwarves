3d7103e904484282a56b8f4528bd400871274396
Taras Nester <tarasnester414@gmail.com>
2022-05-24 14:25:44 +0300

    deleted class atribute

--- app/players/player.py
+++ app/players/player.py
@@ -2,11 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Player(ABC):
-    nickname = []
 
     def __init__(self, nickname: str):
         self.nickname = nickname
-        Player.nickname.append(nickname)
 
     @abstractmethod
     def get_rating(self):
