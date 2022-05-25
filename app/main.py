9826e33019c588c4cb799cd96a4147d56e0fb6ca
Daria <darizhur.vol2@gmail.com>
2022-05-25 19:46:22 +0300

    'New_solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def calculate_team_total_rating(team):
-    return sum([i.get_rating() for i in team])
+    return sum([member.get_rating() for member in team])
 
 
 def elves_concert(elves):
-    for i in elves:
-        i.play_elf_song()
+    for elf in elves:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves):
-    for i in dwarves:
-        i.eat_favourite_dish()
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
