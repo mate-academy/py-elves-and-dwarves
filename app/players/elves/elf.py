3a08ebcccd326a66ef0626e46d959d8783ee58c8
Dorius <kornelydorius@gmail.com>
2022-05-25 09:13:07 +0300

    'Solution'

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -3,7 +3,7 @@ from app.players.player import Player
 
 
 class Elf(Player, ABC):
-    def __init__(self, nickname, musical_instrument):
+    def __init__(self, nickname: str, musical_instrument: str):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
