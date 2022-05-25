43ff157bbeaa522094ad550314666a108eff263f
bohdan <bodyan1108@gmail.com>
2022-05-25 13:41:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def calculate_team_total_rating(list_of_players: list) -> int:
+    result = 0
+    for player in list_of_players:
+        result += player.get_rating()
+
+    return result
+
+
+def elves_concert(list_of_elfs: list):
+    for elf in list_of_elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(list_of_dwarves: list):
+    for dwarf in list_of_dwarves:
+        dwarf.eat_favourite_dish()
