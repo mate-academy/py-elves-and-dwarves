e8f0d98fd3e32b57d1f0d531d59d1abee59230b9
kamilla-boiko <kamilla.boyko@gmail.com>
2022-05-26 16:00:38 +0300

    deleted unnecessary modules

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
-# from abc import ABC, abstractmethod
-# from app.players.dwarves.dwarf import Dwarf
-# from app.players.dwarves.dwarf_warrior import DwarfWarrior
-# from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-#
-# from app.players.elves.elf import Elf
-# from app.players.elves.elf_ranger import ElfRanger
-# from app.players.elves.druid import Druid
-
-
 def calculate_team_total_rating(players: list):
     return sum([player.get_rating() for player in players])
 
