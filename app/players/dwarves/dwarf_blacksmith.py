8260cb10a7a935741c1f015ba2c59d502ccd3dec
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-05-26 21:11:26 +0300

    Solution

---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,16 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, nickname, favourite_dish, skill_level: int):
+        super().__init__(nickname, favourite_dish)
+        self._skill_level = skill_level
+
+    def player_info(self):
+        return f"Dwarf blacksmith " \
+               f"{self.nickname} " \
+               f"with skill of the " \
+               f"{self._skill_level} level"
+
+    def get_rating(self):
+        return self._skill_level
