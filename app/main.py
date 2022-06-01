b746e4ed050a3f41c31638dfed1734ef380c584f
Denis <sairustv228@gmail.com>
2022-06-01 22:51:17 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+
+
+def calculate_team_total_rating(total_rating: list):
+    return sum(player.get_rating() for player in total_rating)
+
+
+def elves_concert(elves: list):
+    for music in elves:
+        music.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list):
+    for eat in dwarves:
+        eat.eat_favourite_dish()
