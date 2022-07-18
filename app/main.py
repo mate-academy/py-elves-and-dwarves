977f1a2141c1467e2307a5edd45a39b06c22413f
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-18 15:26:39 +0300

    Solution corrected

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 def calculate_team_total_rating(team):
-
     return sum(player.get_rating() for player in team)
 
 
 def elves_concert(elves):
-
     for elf in elves:
         elf.play_elf_song()
 
