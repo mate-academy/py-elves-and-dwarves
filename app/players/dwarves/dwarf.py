02861aeceb05014959e00334ad2b8a429e5f74b3
Yan Purdenko <purdenko22@gmail.com>
2022-08-15 18:56:23 +0200

    My Solution2

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,8 +1,7 @@
-from abc import ABC
 from app.players.player import Player
 
 
-class Dwarf(Player, ABC):
+class Dwarf(Player):
 
     def __init__(self, nickname: str, favourite_dish: str):
         super(Dwarf, self).__init__(nickname)
