f30abc35bd2a3952bae799bc7a1c6ab67c85b506
Victor <ruravictor@gmail.com>
2022-08-30 15:13:42 +0200

    Solution

---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf, ABC):
+    def __init__(self, nickname, favourite_dish, hummer_level):
+        super().__init__(nickname, favourite_dish)
+        self._hummer_level = hummer_level
+
+    def get_rating(self):
+        return self._hummer_level + 4
+
+    def player_info(self):
+        return f"Dwarf warrior {self.nickname}. " \
+               f"{self.nickname} has a hummer of the " \
+               f"{self._hummer_level} level"
