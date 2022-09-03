2ca188e68109c233b598d90d2582495a85be2f2b
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-03 23:56:50 +0300

    Solution for WoW

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team):
+    all_ratings = 0
+    for player in team:
+        all_ratings += player.get_rating()
+    return all_ratings
+
+
+def elves_concert(elves):
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves):
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
