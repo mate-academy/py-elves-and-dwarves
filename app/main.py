7c0b5d8c57b632c0c0f11ff2fea43b92182f3bab
Diana <daria.blessed18@gmail.com>
2022-09-10 16:41:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team):
+    all_ratings = []
+    for player in team:
+        all_ratings.append(player.get_rating())
+    return sum(all_ratings)
+
+
+def elves_concert(elves):
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves):
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
