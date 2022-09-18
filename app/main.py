b0043cb5bfcdf32abfbb1a9b0a8bbff2293c8562
Oleksandr Mamchur <irex1988@gmail.com>
2022-09-18 21:34:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team):
+    team_rating = 0
+    for player in team:
+        team_rating += player.get_rating()
+    return team_rating
+
+
+def elves_concert(elves):
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves):
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
