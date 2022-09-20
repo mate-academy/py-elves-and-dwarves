a3351d8a4caa87f34bc677c820d1005b06402a8a
mltdll <ilya.vyplavin@gmail.com>
2022-09-20 17:11:00 +0300

    Solution

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+
+from app.players.player import Player
+
+
+class Dwarf(Player, metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
