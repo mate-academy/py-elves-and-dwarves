0b6bea4dbb232ec03bee6a596deae68e5feadc7e
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-20 18:00:24 +0300

    fixing syntax problem

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ def calculate_team_total_rating(list_of_players: list) -> int:
     return sum([player.get_rating() for player in list_of_players])
 
 
-def elves_concert(list_of_elfs: list) -> None:
-    for i in range(len(list_of_elfs)):
-        list_of_elfs[i].play_elf_song()
+def elves_concert(list_of_elves: list) -> None:
+    for elv in list_of_elves:
+        elv.play_elf_song()
 
 
-def feast_of_the_dwarves(list_of_dwarfes: list) -> None:
-    for i in range(len(list_of_dwarfes)):
-        list_of_dwarfes[i].eat_favourite_dish()
+def feast_of_the_dwarves(list_of_dwarves: list) -> None:
+    for dwarf in list_of_dwarves:
+        dwarf.eat_favourite_dish()
