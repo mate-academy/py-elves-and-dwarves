fa6a26cabe672642ddc82ec5232f5c3a97a34d09
igor kosianenko <funkcionalkos@gmail.com>
2022-09-26 17:39:46 +0300

    ls changed to list

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ from app.players.dwarves.dwarf import Dwarf
 from app.players.elves.elf import Elf
 
 
-def calculate_team_total_rating(players_ls: list[Player]) -> int:
+def calculate_team_total_rating(players_list: list[Player]) -> int:
 
-    return sum(player.get_rating() for player in players_ls)
+    return sum(player.get_rating() for player in players_list)
 
 
-def elves_concert(elf_ls: list[Elf]) -> None:
+def elves_concert(elf_list: list[Elf]) -> None:
 
-    for elf in elf_ls:
+    for elf in elf_list:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf_ls: list[Dwarf]) -> None:
+def feast_of_the_dwarves(dwarf_list: list[Dwarf]) -> None:
 
-    for dwarf in dwarf_ls:
+    for dwarf in dwarf_list:
         dwarf.eat_favourite_dish()
