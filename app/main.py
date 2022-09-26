d64ff985cb62d4981ad4911cd3dcaff9b31c6f4b
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-09-26 14:42:39 +0300

    Comments deleted, list comprehension applied

--- app/main.py
+++ app/main.py
@@ -1,19 +1,5 @@
-# from app.players.player import Player
-# from app.players.dwarves.dwarf import Dwarf
-# from app.players.dwarves.dwarf_warrior import DwarfWarrior
-# from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-# from app.players.elves.elf import Elf
-# from app.players.elves.elf_ranger import ElfRanger
-# from app.players.elves.druid import Druid
-
-
 def calculate_team_total_rating(players: list) -> int:
-    result = 0
-
-    for player in players:
-        result += player.get_rating()
-
-    return result
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elfes: list) -> None:
