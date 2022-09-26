ee24f30f87849777f941107cdbc275c69e798dc4
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-09-26 17:17:19 +0300

    Got rid of []

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(list_of_players: list) -> int:
-    return sum([player.get_rating() for player in list_of_players])
+    return sum(player.get_rating() for player in list_of_players)
 
 
 def elves_concert(list_of_elves: list[Elf]) -> None:
