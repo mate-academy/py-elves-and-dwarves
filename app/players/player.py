c61617fe094bc70477dbb7f86bfa97a55be6a8cc
mltdll <ilya.vyplavin@gmail.com>
2022-09-27 17:48:08 +0300

    Revert "Remove __init__() method from Player class"
    This reverts commit c3f44d635937f6f9c79ae5c42d79cad47b252343.

--- app/players/player.py
+++ app/players/player.py
@@ -3,6 +3,10 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
     @abstractmethod
+    def __init__(self, nickname: str) -> None:
+        self.nickname = nickname
+
+    @abstractmethod
     def get_rating(self):
         pass
 
