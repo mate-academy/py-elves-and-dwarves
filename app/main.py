338e55632ee6fb809caa10f3a2dad60e6c9092a8
Alexander <alexzubkoff123@gmail.com>
2022-09-27 19:06:52 +0300

    Changed var names according plural form and delete extra brackets

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players_list: list[Player]) -> int:
-    return sum([player.get_rating() for player in players_list])
+    return sum(player.get_rating() for player in players_list)
 
 
-def elves_concert(elf_list: list[Elf]) -> None:
-    for elf in elf_list:
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf_list: list[Dwarf]) -> None:
-    for dwarf in dwarf_list:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
         dwarf.eat_favourite_dish()
