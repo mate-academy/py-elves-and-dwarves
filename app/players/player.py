c3f44d635937f6f9c79ae5c42d79cad47b252343
mltdll <ilya.vyplavin@gmail.com>
2022-09-27 15:13:39 +0300

    Remove __init__() method from Player class

--- app/players/player.py
+++ app/players/player.py
@@ -3,10 +3,6 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
     @abstractmethod
-    def __init__(self, nickname: str) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
     def get_rating(self):
         pass
 
