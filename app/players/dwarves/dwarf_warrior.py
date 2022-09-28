bed7783d3cc736ae4f2b0455cf8d47ddf1428e12
Roman Kolmakov <cerne313@gmail.com>
2022-09-28 15:49:59 +0300

    changed single- to double-quote string notation everywhere

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -16,5 +16,5 @@ class DwarfWarrior(Dwarf):
         return self._hummer_level + 4
 
     def player_info(self) -> str:
-        return (f'Dwarf warrior {self.nickname}. {self.nickname} has a hummer '
-                f'of the {self._hummer_level} level')
+        return (f"Dwarf warrior {self.nickname}. {self.nickname} has a hummer "
+                f"of the {self._hummer_level} level")
