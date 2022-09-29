cda9b30fdac2dafe030d8a562716ca3615540d37
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-09-29 13:21:57 +0300

    Solution

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -12,8 +12,8 @@ class DwarfWarrior(Dwarf):
     def player_info(self) -> str:
         nick = self.nickname
         hummer_lvl = self._hummer_level
-        return f"Dwarf warrior {nick}. " \
-               f"{nick} has a hummer of the {hummer_lvl} level"
+        return (f"Dwarf warrior {nick}. "
+                f"{nick} has a hummer of the {hummer_lvl} level")
 
     def get_rating(self) -> int:
         return self._hummer_level + 4
