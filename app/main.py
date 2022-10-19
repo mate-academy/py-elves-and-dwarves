a4a8f778d0ce4d942c5394e1bc1389cf1e40e0ab
Marina Bidenko <bidenko.bmw@gmail.com>
2022-10-19 21:13:38 +0300

    fixed2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-def calculate_team_total_rating(players: list):
+def calculate_team_total_rating(players: list) -> int:
     return sum([player.get_rating() for player in players])
 
 
-def elves_concert(elfs: list):
+def elves_concert(elfs: list) -> None:
     for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list):
+def feast_of_the_dwarves(dwarfs: list) -> None:
     for dwarf in dwarfs:
         dwarf.eat_favourite_dish()
