ed675dd33fc003598a5a867eb8553cb4d3054f54
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-11-05 15:28:25 +0200

    added inh abc

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -2,7 +2,7 @@ from abc import ABC
 from app.players.player import Player
 
 
-class Elf(Player):
+class Elf(Player, ABC):
 
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
