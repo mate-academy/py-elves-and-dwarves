b78a9fe37ae241c6fb4170d057681c0efdd6f0b1
Admin <winofkill@gmail.com>
2022-11-10 13:34:28 +0400

    Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+
+
+def calculate_team_total_rating(teams: list) -> int:
+    summary = 0
+    for player in teams:
+        summary += int(player.get_rating())
+    return summary
+
+
+def elves_concert(actors: list) -> None:
+    for actor in actors:
+        actor.play_elf_song()
+
+
+def feast_of_the_dwarves(dishes: list) -> None:
+    for dish in dishes:
+        dish.eat_favourite_dish()
