7b3deab21a204b6d36ef28effe60e4eeb6b42a33
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2022-11-23 07:05:21 +0200

    Res

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+
+
+def calculate_team_total_rating(team: list) -> int:
+    total = 0
+    for character in team:
+        total += character.get_rating()
+    return total
+
+
+def elves_concert(elves: list) -> None:
+    for character in elves:
+        character.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for character in dwarves:
+        character.eat_favourite_dish()
