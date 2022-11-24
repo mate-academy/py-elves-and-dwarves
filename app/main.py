0b25b8170b447aabef88c13db73a74fc54676043
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2022-11-24 16:56:35 +0200

    Res2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(team: list) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     total = 0
     for character in team:
         total += character.get_rating()
     return total
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     for character in elves:
         character.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for character in dwarves:
         character.eat_favourite_dish()
