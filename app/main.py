39dcaf644af171b65482b25a515036e71a5903dc
Peter Abramov <sebshe3@gmail.com>
2022-11-26 20:48:50 +0200

    'list_annotation'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
-def calculate_team_total_rating(players: list) -> int:
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+
+
+def calculate_team_total_rating(
+        players: list[DwarfBlacksmith | DwarfWarrior | Druid | ElfRanger]
+) -> int:
     total_rating = 0
 
     for player in players:
@@ -7,11 +15,15 @@ def calculate_team_total_rating(players: list) -> int:
     return total_rating
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(
+        elves: list[Druid | ElfRanger]
+) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(
+        dwarves: list[DwarfBlacksmith | DwarfWarrior]
+) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
