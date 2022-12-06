89a94fdd3727c099daa0610eca920425cb897e7c
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-06 21:32:48 +0200

    Made corrections: create list comprehension and  fix indentation

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from typing import Any
-
-
 def calculate_team_total_rating(team: list) -> int:
-    sum_rating = 0
-    for player in team:
-        sum_rating += player.get_rating()
-    return sum_rating
+    return sum([player.get_rating() for player in team])
 
 
-def elves_concert(list_elves: list) -> list[Any]:
-    elves = []
-    for elf in list_elves:
-        elves.append(elf.play_elf_song())
-    return elves
+def elves_concert(list_elves: list) -> list:
+    return [elf.play_elf_song() for elf in list_elves]
 
 
-def feast_of_the_dwarves(list_dwarves: list) -> None:
-    dwarves = []
-    for dwarf in list_dwarves:
-        dwarves.append(dwarf.eat_favourite_dish())
+def feast_of_the_dwarves(list_dwarves: list) -> list:
+    return [dwarf.eat_favourite_dish() for dwarf in list_dwarves]
