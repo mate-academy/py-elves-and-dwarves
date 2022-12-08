1f1c5828a384e6db2bb3a63a46051ff9871c8a13
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-08 20:14:59 +0200

    Made corrections regarding the annotations

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum([player.get_rating() for player in team])
 
 
-def elves_concert(list_elves: list) -> list:
+def elves_concert(list_elves: list[Elf]) -> list:
     return [elf.play_elf_song() for elf in list_elves]
 
 
-def feast_of_the_dwarves(list_dwarves: list) -> list:
+def feast_of_the_dwarves(list_dwarves: list[Dwarf]) -> list:
     return [dwarf.eat_favourite_dish() for dwarf in list_dwarves]
