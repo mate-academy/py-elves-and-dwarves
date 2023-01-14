88ac85092ee7482fa6ee3ab5dd60ce42f40f274e
Dmytro <omelchenko.dm@gmail.com>
2023-01-14 21:52:07 +0200

    Elves_Dwarves_Fixed_v02

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -3,10 +3,11 @@ from app.players.player import Player
 
 class Elf(Player):
 
-    def __init__(self,
-                 nickname: str,
-                 musical_instrument: str
-                 ) -> None:
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str
+    ) -> None:
         super().__init__(nickname)
         self.musical_instrument = musical_instrument
 
