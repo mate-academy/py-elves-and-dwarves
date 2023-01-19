efcfe4ab70a3bcb774b8695f1a1c21af208ef26b
yura.vataschuk <yura.vataschuk@gmail.com>
2023-01-19 22:15:28 +0200

    Fixed

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -15,6 +15,8 @@ class Druid(Elf):
         return len(self._favourite_spell)
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. " \
-               f"{self.nickname} has a favourite spell: " \
-               f"{self._favourite_spell}"
+        return (
+            f"Druid {self.nickname}. "
+            f"{self.nickname} has a favourite spell: "
+            f"{self._favourite_spell}"
+        )
