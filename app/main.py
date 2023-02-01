d2ad3095f96e8f320c32bf3e582d3a9e4932221a
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-02-01 13:20:21 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,31 +1,9 @@
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
-
-
-elves_list = []
-dwarves_list = []
 
 
 def calculate_team_total_rating(players: list) -> int:
-    total_rating = 0
-    for player in players:
-        if isinstance(player, DwarfBlacksmith):
-            dwarves_list.append(player)
-            total_rating += DwarfBlacksmith.get_rating(player)
-        if isinstance(player, DwarfWarrior):
-            dwarves_list.append(player)
-            total_rating += DwarfWarrior.get_rating(player)
-        if isinstance(player, Druid):
-            elves_list.append(player)
-            total_rating += Druid.get_rating(player)
-        if isinstance(player, ElfRanger):
-            elves_list.append(player)
-            total_rating += ElfRanger.get_rating(player)
-    return total_rating
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list) -> None:
