03f813a7f07037d0d20c70bb02fda47e5e88613e
Zoriana Dvulit <zdvulit@ukr.net>
2023-02-02 16:21:45 +0200

    New New Solution

--- app/players/player.py
+++ app/players/player.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
     @abstractmethod
-    def __init__(self, nickname: str) -> str:
+    def __init__(self, nickname: str) -> None:
         self.nickname = nickname
 
     @abstractmethod
