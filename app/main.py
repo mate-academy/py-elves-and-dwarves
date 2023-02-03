e23ca7595ea8f6a4a5960d0cd615b8bdb0e28f28
leetwinoff <leetwinoff92@gmail.com>
2023-02-02 21:39:30 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from __future__ import annotations
+
+
+def calculate_team_total_rating(players: list) -> int:
+    total_rating = 0
+    for player in players:
+        total_rating += player.get_rating()
+    return total_rating
+
+
+def elves_concert(players: list) -> None:
+    for player in players:
+        player.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
