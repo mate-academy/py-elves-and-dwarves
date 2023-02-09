210dda399aca082ff7fa4f719c0d8a48c5c578d5
Andrii Fedoryshyn <warvar2016wgmail.com>
2023-02-09 15:20:52 +0200

    Solution_for_elves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    return sum(player_of_team.get_rating() for player_of_team in team)
+
+
+def elves_concert(team: list) -> None:
+    for elf in team:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(team: list) -> None:
+    for dwarv in team:
+        dwarv.eat_favourite_dish()
