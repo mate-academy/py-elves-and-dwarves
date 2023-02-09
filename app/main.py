163ca520b62bad405bcb40cc1d21dca04e9252b8
Andrii Fedoryshyn <warvar2016wgmail.com>
2023-02-09 16:53:04 +0200

    Solution_for_elves

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-def calculate_team_total_rating(team_of_elves: list) -> int:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team_of_elves: list[Player]) -> int:
     return sum(player_of_team.get_rating() for player_of_team in team_of_elves)
 
 
-def elves_concert(choir_of_elves: list) -> None:
+def elves_concert(choir_of_elves: list[Player]) -> None:
     for elf in choir_of_elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(team_of_dwarves: list) -> None:
+def feast_of_the_dwarves(team_of_dwarves: list[Player]) -> None:
     for dwarv in team_of_dwarves:
         dwarv.eat_favourite_dish()
