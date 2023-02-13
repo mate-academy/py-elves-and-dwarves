23afdaef2883d949a2b945972e4f20bc1b0fbb2b
Max Bilokur <bilmaxim@gmail.com>
2023-02-13 19:56:37 +0200

    update

--- app/main.py
+++ app/main.py
@@ -8,10 +8,8 @@ def calculate_team_total_rating(player_list: list[Player]) -> int:
 
 
 def elves_concert(elves: list[Elf]) -> None:
-    for elf in elves:
-        elf.play_elf_song()
+    [elf.play_elf_song() for elf in elves]
 
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
-    for dwarf in dwarves:
-        dwarf.eat_favourite_dish()
+    [dwarf.eat_favourite_dish() for dwarf in dwarves]
