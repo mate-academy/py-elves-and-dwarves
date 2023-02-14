9ed2a05bcc39f99034b75520b3e3fd07021e8c09
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-14 22:22:34 +0200

    4 commit

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def calculate_team_total_rating(team: list[Player]) -> int:
 
 
 def elves_concert(elves_list: list[Elf]) -> None:
-    [elf.play_elf_song() for elf in elves_list]
+    for elf in elves_list:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarf_list: list[Dwarf]) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in dwarf_list]
+    for dwarf in dwarf_list:
+        dwarf.eat_favourite_dish()
