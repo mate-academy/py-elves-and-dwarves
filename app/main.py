78563e4b3fa125ecd8b7f312c1685976740ebc23
Bohdan Kovalenko <bog7807@gmail.com>
2023-02-14 20:08:54 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(players: dict) -> int:
+    total_rate = 0
+    for hero in players:
+        total_rate += hero.get_rating()
+    return total_rate
+
+
+def elves_concert(elves: list) -> None:
+    for hero in elves:
+        hero.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for hero in dwarves:
+        hero.eat_favourite_dish()
