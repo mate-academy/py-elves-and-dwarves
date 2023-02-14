3725884a9ec54d66d8edfabe1922455ab760289e
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-14 16:51:27 +0200

    Solution

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -13,6 +13,6 @@ class Druid(Elf):
         return len(self._favourite_spell)
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. " \
-               f"{self.nickname} has a favourite " \
-               f"spell: {self._favourite_spell}"
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a favourite "
+                f"spell: {self._favourite_spell}")
