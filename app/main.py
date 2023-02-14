08c5f344b0861a30961f9ed26e9da1c2bd881d5b
Ruslan <noezyboy1@gmail.com>
2023-02-14 16:47:32 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+
+
 def calculate_team_total_rating(team: list) -> int:
     return sum([player.get_rating() for player in team])
 
