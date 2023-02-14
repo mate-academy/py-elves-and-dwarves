dd71781a1e1f222bb46d3f77e0d7e0db2f1d52e0
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-14 18:48:40 +0200

    All logic had been implemented

--- app/main.py
+++ app/main.py
@@ -92,3 +92,14 @@ class DwarfBlacksmith(Dwarf):
     def get_rating(self):
         return self._skill_level
 
+
+def calculate_team_total_rating(players_list: list[Elf, Dwarf]) -> int:
+    return sum(player.get_rating() for player in players_list)
+
+
+def elves_concert(elves_list: list[Elf]) -> None:
+    [elf.play_elf_song() for elf in elves_list]
+
+
+def feast_of_the_dwarves(dwarves_list: list[Dwarf]) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in dwarves_list]
