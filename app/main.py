0a2afc71416a7a50823aeebce5a5902aeb6197d7
dmytro <manjustice1998@gmail.com>
2023-02-14 19:03:26 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def calculate_team_total_rating(players: List[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elfs: List[Elf]) -> None:
-    for elf in elfs:
+def elves_concert(elves: List[Elf]) -> None:
+    for elf in elves:
         elf.play_elf_song()
 
 
