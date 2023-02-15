3f1b3f81e136d11e2607f7a8d67df070b0381d34
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-15 20:15:35 +0200

    More descriptive type annotation and f string

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-def calculate_team_total_rating(list_players: list) -> int | float:
+from __future__ import annotations
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+
+
+def calculate_team_total_rating(
+        list_players: list[Druid | ElfRanger]
+) -> int | float:
     return sum(player.get_rating() for player in list_players)
 
 
-def elves_concert(list_elf: list) -> None:
-    [elf.play_elf_song() for elf in list_elf]
+def elves_concert(list_elf: list[Druid | ElfRanger]) -> None:
+    for elf in list_elf:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(list_dwarf: list) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in list_dwarf]
+def feast_of_the_dwarves(
+        list_dwarf: list[DwarfWarrior | DwarfBlacksmith]
+) -> None:
+    for dwarf in list_dwarf:
+        dwarf.eat_favourite_dish()
--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -15,5 +15,5 @@ class DwarfWarrior(Dwarf):
         return self._hummer_level + 4
 
     def player_info(self) -> str:
-        return f"Dwarf warrior {self.nickname}. {self.nickname} " \
-               f"has a hummer of the {self._hummer_level} level"
+        return (f"Dwarf warrior {self.nickname}. {self.nickname}"
+                f" has a hummer of the {self._hummer_level} level")
