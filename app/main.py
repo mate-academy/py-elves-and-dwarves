1a806a9f76be754a46802194ff879a7cced569c7
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-15 13:16:45 +0200

    Removed list comprehension from sum calculating

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def calculate_team_total_rating(players: list) -> float:
-    return sum([player.get_rating() for player in players])
+    return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elfs: list) -> list:
-    return [elf.play_elf_song() for elf in elfs]
+def elves_concert(elfs: list) -> None:
+    [elf.play_elf_song() for elf in elfs]
 
 
-def feast_of_the_dwarves(dwarves: list) -> list:
-    return [dwarf.eat_favourite_dish() for dwarf in dwarves]
+def feast_of_the_dwarves(dwarves: list) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in dwarves]
