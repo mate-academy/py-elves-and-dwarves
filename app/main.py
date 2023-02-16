c2ec6caa7349dd8cb3b9930a6d130fb75e8336cd
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-16 18:51:31 +0200

    Implemented inheritance from ABC for Elf and Dwarf classes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-def calculate_team_total_rating(players: list) -> float:
+from typing import List
+
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: List[Player]) -> float:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elfs: list) -> None:
+def elves_concert(elfs: List[Elf]) -> None:
     [elf.play_elf_song() for elf in elfs]
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in dwarves]
+def feast_of_the_dwarves(dwarves: List[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
