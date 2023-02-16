8f6ac15a0b3e6109042fc153a80cb6e1e2201533
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-16 15:41:44 +0200

    Type annotationation for team

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 from __future__ import annotations
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(
-        list_players: list[Druid | ElfRanger]
+        list_players: list[Elf | Dwarf]
 ) -> int | float:
     return sum(player.get_rating() for player in list_players)
 
 
-def elves_concert(list_elf: list[Druid | ElfRanger]) -> None:
+def elves_concert(list_elf: list[Elf]) -> None:
     for elf in list_elf:
         elf.play_elf_song()
 
 
 def feast_of_the_dwarves(
-        list_dwarf: list[DwarfWarrior | DwarfBlacksmith]
+        list_dwarf: list[Dwarf]
 ) -> None:
     for dwarf in list_dwarf:
         dwarf.eat_favourite_dish()
