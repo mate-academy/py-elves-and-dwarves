1e3aa0a1a23f91ffc2c7217293efa4555dc2de9b
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-18 17:39:46 +0200

    Add changing - v2

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,12 +1,10 @@
-from abc import ABC
-
 from app.players.player import Player
 
 
-class Dwarf(Player, ABC):
+class Dwarf(Player):
     def __init__(self, favourite_dish: str, nickname: str) -> None:
         super().__init__(nickname)
         self._favourite_dish = favourite_dish
 
-    def eat_favourite_dish(self) -> str:
+    def eat_favourite_dish(self) -> None:
         print(f"{self.nickname} is eating {self._favourite_dish}")
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,14 +1,12 @@
-from abc import ABC
-
 from app.players.player import Player
 
 
-class Elf(Player, ABC):
+class Elf(Player):
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
-    def play_elf_song(self) -> str:
+    def play_elf_song(self) -> None:
         print(f"{self.nickname} is "
               f"playing a song on "
               f"the {self._musical_instrument}")
