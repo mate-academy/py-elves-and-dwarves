832d6662469a864cd0b2364a37464b754c215f0a
Jorjio <Jorjio22@gmail.com>
2023-03-09 14:43:52 +0000

    'Corrected'

--- app/main.py
+++ app/main.py
@@ -7,23 +7,15 @@ from app.players.elves.elf import Elf
 from app.players.elves.elf_ranger import ElfRanger
 
 
-def calculate_team_total_rating(players: list) -> int:
-    return sum([player.get_rating() for player in players])
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: list) -> None:
-    [elf.play_elf_song() for elf in elves]
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in dwarves]
-
-
-if __name__ == "__main__":
-    player = Player("Thiddeal")
-    dwarf = Dwarf("Thiddeal", "French Fries")
-    dwarf_blacksmith = DwarfBlacksmith("Thiddeal", "French Fries", 3)
-    dwarf_warrior = DwarfWarrior("Thiddeal", "French Fries", 3)
-    druid = Druid("Thiddeal", "French Fries", "aaa")
-    elf = Elf("Thiddeal", "French Fries")
-    elf_ranger = ElfRanger("Thiddeal", "French Fries")
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
