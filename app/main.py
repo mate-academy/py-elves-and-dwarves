88ef8fd6c0bf69fda3bac3133d1615756bb2abf2
Jorjio <Jorjio22@gmail.com>
2023-03-10 16:54:31 +0000

    'Finali'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from app.players.player import Player
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
