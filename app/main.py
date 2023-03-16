58dcc72f166bd468121b5d6c0a4109406074f24f
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-03-17 00:10:06 +0200

    Select

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from __future__ import annotations
+from app.players.player import Player
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+
+
+def calculate_team_total_rating(rating: list[Player]) -> int:
+    return sum(player.get_rating() for player in rating)
+
+
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
+    for dwarf in dwarfs:
+        dwarf.eat_favourite_dish()
