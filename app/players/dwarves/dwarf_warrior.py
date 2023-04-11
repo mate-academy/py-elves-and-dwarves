eca0ffbea7cae53bc3d3e7a5f60682cbbb75cce3
Volodymyr Yerokhov <kavbian@gmail.com>
2023-04-11 17:33:58 +0300

    '()'

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -12,8 +12,8 @@ class DwarfWarrior(Dwarf):
         self._hummer_lever = hummer_level
 
     def player_info(self) -> str:
-        return f"Dwarf warrior {self.nickname}. {self.nickname} has " \
-               f"a hummer of the {self._hummer_lever} level"
+        return (f"Dwarf warrior {self.nickname}. {self.nickname} has "
+                f"a hummer of the {self._hummer_lever} level")
 
     def get_rating(self) -> int:
         return self._hummer_lever + 4
