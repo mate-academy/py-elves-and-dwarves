03ebe29a797975fd06a7ed5f6c7be052acb940aa
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-20 19:55:50 +0300

    Solution of Elves and Dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    rating_count = 0
+    for player in players:
+        rating_count += player.get_rating()
+    return rating_count
+
+
+def elves_concert(elf_players: list[Elf]) -> None:
+    for elf in elf_players:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarf_players: list[Dwarf]) -> None:
+    for dwarf in dwarf_players:
+        dwarf.eat_favourite_dish()
