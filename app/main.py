fb3cdcda8d3abfb3f3d8d4c323b6c6463e04a88d
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-04-27 09:20:55 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
-from app.players.elves.elf import Elf
-from app.players.dwarves.dwarf import Dwarf
-
-
-def calculate_team_total_rating(team):
+def calculate_team_total_rating(team: list) -> int:
+    """Calculate the total rating of a team."""
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves):
+def elves_concert(elves: list) -> None:
+    """Play an elf song for each elf in the given list."""
     for elf in elves:
         elf.play_elf_song()
 
-def feast_of_the_dwarves(dwarves):
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    """Have each dwarf in the given list eat their favourite dish."""
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
-
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -2,13 +2,18 @@ from app.players.elves.elf import Elf
 
 
 class ElfRanger(Elf):
-    def __init__(self, nickname, musical_instrument, bow_level: int):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            bow_level: int
+    ) -> None:
         super().__init__(nickname, musical_instrument)
         self._bow_level = bow_level
 
-    def get_rating(self):
+    def get_rating(self) -> int:
         return self._bow_level * 3
 
-    def player_info(self):
-        return (f"Elf ranger {self.nickname}. {self.nickname} has bow "
-                f"of the {self._bow_level} level")
+    def player_info(self) -> str:
+        return (f"Elf ranger {self.nickname}. {self.nickname} "
+                f"has bow of the {self._bow_level} level")
