7eadbaeab3571929a983da6bbc19b7aa2dacac26
Petro Havrysh <petro.havrysh@gmail.com>
2023-04-28 20:08:16 +0300

    'Solution'

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -12,8 +12,8 @@ class DwarfBlacksmith(Dwarf):
         self._skill_level = skill_level
 
     def player_info(self) -> str:
-        return f"Dwarf blacksmith {self.nickname} with skill of the " \
-               f"{self._skill_level} level"
+        return (f"Dwarf blacksmith {self.nickname} with skill of the "
+                f"{self._skill_level} level")
 
     def get_rating(self) -> int:
         return self._skill_level
