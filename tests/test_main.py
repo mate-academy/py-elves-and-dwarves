4bc624b1c6725bffbddf38947f95f0877920e377
Petro Havrysh <petro.havrysh@gmail.com>
2023-04-28 18:15:28 +0300

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,13 +6,13 @@ import pytest
 
 from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
-from app.main import Dwarf
-from app.main import DwarfBlacksmith
-from app.main import DwarfWarrior
-from app.main import Druid
-from app.main import Elf
-from app.main import ElfRanger
-from app.main import Player
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.druid import Druid
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.player import Player
 
 
 @pytest.mark.parametrize(
