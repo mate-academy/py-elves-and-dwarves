1e1487185d9433a6137122a63c0b85591572e040
Olexandr <maxbovling23432@gmail.com>
2023-05-14 15:16:46 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/main.py
    #	app/players/elves/druid.py
    #	app/players/elves/elf_ranger.py

--- app/main.py
+++ app/main.py
@@ -1,36 +1,38 @@
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+
+
 def calculate_team_total_rating(team: list) -> int:
     calculate_total_rating = 0
-    for yy in team:
-        if type(yy).__name__ == "Druid":
-            calculate_total_rating += len(yy.favourite_spell)
-        elif type(yy).__name__ == "ElfRanger":
-            calculate_total_rating += 3 * yy.bow_level
-        elif type(yy).__name__ == "DwarfWarrior":
-            calculate_total_rating += yy.hummer_level + 4
-        elif type(yy).__name__ == "DwarfBlacksmith":
-            calculate_total_rating += yy.skill_level
-        elif yy == []:
+    for races in team:
+        if type(races).__name__ == "Druid":
+            calculate_total_rating += len(races.favourite_spell)
+        elif type(races).__name__ == "ElfRanger":
+            calculate_total_rating += 3 * races.bow_level
+        elif type(races).__name__ == "DwarfWarrior":
+            calculate_total_rating += races.hummer_level + 4
+        elif type(races).__name__ == "DwarfBlacksmith":
+            calculate_total_rating += races.skill_level
+        elif races == []:
             calculate_total_rating = 0
     return calculate_total_rating
 
 
-def elves_concert(elves: list) -> int:
-    for yy in elves:
-        print(f"{yy.nickname} is playing a song on the "
-              f"{yy.musical_instrument}")
+def elves_concert(elves: list[Elf]) -> int:
+    for races in elves:
+        print(f"{races.nickname} is playing a song on the "
+              f"{races.musical_instrument}")
 
 
 def play_elf_song(elves: list) -> int:
-    for yy in elves:
-        print(f"{yy.nickname} is playing a song on the "
-              f"{yy.musical_instrument}")
+    print(f"{elves.nickname} is playing a song on the "
+          f"{elves.musical_instrument}")
 
 
-def feast_of_the_dwarves(dwarves: list) -> int:
-    for yy in dwarves:
-        print(f"{yy.nickname} is eating {yy.favourite_dish}")
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> int:
+    for races in dwarves:
+        print(f"{races.nickname} is eating {races.favourite_dish}")
 
 
 def eat_favourite_dish(dwarves: list) -> int:
-    for yy in dwarves:
-        print(f"{yy.nickname} is eating {yy.favourite_dish}")
+    print(f"{dwarves.nickname} is eating {dwarves.favourite_dish}")
