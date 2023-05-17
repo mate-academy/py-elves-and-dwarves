b6d763154c89480d411c77b37de32f0642ba5c86
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-17 14:15:48 +0300

    solution

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -6,14 +6,13 @@ class Druid(Elf):
                  nickname: str,
                  musical_instrument: str,
                  favourite_spell: str) -> None:
-
         super().__init__(nickname=nickname,
                          musical_instrument=musical_instrument)
         self._favourite_spell = favourite_spell
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. {self.nickname} " \
-               f"has a favourite spell: {self._favourite_spell}"
+        return (f"Druid {self.nickname}. {self.nickname} "
+                f"has a favourite spell: {self._favourite_spell}")
 
     def get_rating(self) -> int:
         return len(self._favourite_spell)
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -6,14 +6,13 @@ class ElfRanger(Elf):
                  nickname: str,
                  musical_instrument: str,
                  bow_level: int) -> None:
-
         super().__init__(nickname=nickname,
                          musical_instrument=musical_instrument)
         self._bow_level = bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. " \
-               f"{self.nickname} has bow of the {self._bow_level} level"
+        return (f"Elf ranger {self.nickname}. "
+                f"{self.nickname} has bow of the {self._bow_level} level")
 
     def get_rating(self) -> int:
         return 3 * self._bow_level
