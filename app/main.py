cc67262b5beb8fa3f4bf0fc2e4fd4cc84cbd0272
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2023-05-24 21:12:34 +0300

    Fix2

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.players.dwarves.dwarf import Dwarf
 from app.players.elves.elf import Elf
 
 
-def calculate_team_total_rating(team: list["Player"]) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum([member.get_rating() for member in team])
 
 
-def elves_concert(band: list["Elf"]) -> None:
+def elves_concert(band: list[Elf]) -> None:
     for member in band:
         member.play_elf_song()
 
 
-def feast_of_the_dwarves(group: list["Dwarf"]) -> None:
+def feast_of_the_dwarves(group: list[Dwarf]) -> None:
     for member in group:
         member.eat_favourite_dish()
