77719fd6037d9920aa0d931c81b3df52eefdba90
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-05-31 15:30:10 +0300

    player constructor update

--- app/players/player.py
+++ app/players/player.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
     def __init__(self, nickname: str) -> None:
-        super().__init__()
         self._nickname: str = nickname
 
     @abstractmethod
