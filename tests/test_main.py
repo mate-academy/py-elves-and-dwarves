0603f34a4b3207c1a47a8310b813cd0545754ebb
Denys Raboshchuk <kontroler444@gmail.com>
2023-05-31 17:03:44 +0300

    Done!

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
 from app.players.dwarves.dwarf import Dwarf
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_warior import DwarfWarrior
 from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
 from app.players.elves.elf_ranger import ElfRanger
