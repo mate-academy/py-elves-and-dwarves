e87b546b936f2086522cb4d0fa05ed736cef08c0
Andrew Sulym <a.sulymm@gmail.com>
2023-06-02 15:43:17 +0100

    Fix

--- app/main.py
+++ app/main.py
@@ -4,17 +4,14 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players_list: list[Player]) -> int:
-    count = 0
-    for i in players_list:
-        count += i.get_rating()
-    return count
+    return sum([player.get_rating() for player in players_list])
 
 
 def elves_concert(players_list: list[Elf]) -> None:
-    for i in players_list:
-        i.play_elf_song()
+    for player in players_list:
+        player.play_elf_song()
 
 
 def feast_of_the_dwarves(players_list: list[Dwarf]) -> None:
-    for i in players_list:
-        i.eat_favourite_dish()
+    for player in players_list:
+        player.eat_favourite_dish()
