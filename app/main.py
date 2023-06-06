3c142dd9d513df25c58e05b29aee872f96f04183
Rostyslav <rostyslav.furman@gmail.com>
2023-06-06 17:32:10 +0300

    my_commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
+
+    return sum(hero.get_rating() for hero in team)
+
+
+def elves_concert(team: list[Elf]) -> None:
+    [hero.play_elf_song() for hero in team]
+
+
+def feast_of_the_dwarves(team: list[Dwarf]) -> None:
+    [hero.eat_favourite_dish() for hero in team]
