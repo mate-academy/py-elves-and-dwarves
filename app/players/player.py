5cd3cdbd5ac30be93d43c13b86f382844520f7cc
Marina <marishka28023@gmail.com>
2023-06-09 18:32:10 +0200

    'Solution2'

---
+++ app/players/player.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Player(ABC):
+    def __init__(self, nickname: str) -> None:
+        self.nickname = nickname
+
+    @abstractmethod
+    def get_rating(self) -> Any:
+        pass
+
+    def player_info(self) -> Any:
+        pass
