321c16971f790097da149e427723dc6471f9d226
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-06-18 14:58:55 +0300

    solution2

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -12,8 +12,9 @@ class DwarfBlacksmith(Dwarf):
         self.skill_level = skill_level
 
     def player_info(self) -> str:
-        return f"Dwarf blacksmith {self.nickname} " \
-               f"with skill of the {self.skill_level} level"
+        return (f"Dwarf blacksmith {self.nickname} "
+                f"with skill of the {self.skill_level} level"
+                )
 
     def get_rating(self) -> int:
         return self.skill_level
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -11,8 +11,9 @@ class ElfRanger(Elf):
         self.bow_level = bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. " \
-               f"{self.nickname} has bow of the {self.bow_level} level"
+        return (f"Elf ranger {self.nickname}. "
+                f"{self.nickname} has bow of the {self.bow_level} level"
+                )
 
     def get_rating(self) -> int:
         return 3 * self.bow_level
