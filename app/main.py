36fadb50257f5ef4b07468d28aea27a4981c030c
stepband <vladshvik@gmail.com>
2023-06-20 11:58:12 +0300

    All errors have been fixed, and the program is now working properly.

--- app/main.py
+++ app/main.py
@@ -1,21 +1,15 @@
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.elf_druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-
-
-def calculate_team_total_rating(players):
+def calculate_team_total_rating(players: list) -> int:
     total_rating = 0
     for player in players:
         total_rating += player.get_rating()
     return total_rating
 
 
-def elves_concert(elves):
+def elves_concert(elves: list) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves):
+def feast_of_the_dwarves(dwarves: list) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
