073ed663c170d391d35635b786fb3153c0fb0861
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-06-27 13:47:37 +0300

    Changed typehints v.2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
-from typing import Optional
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
 
 
-def calculate_team_total_rating(players: Optional[list]) -> None:
+def calculate_team_total_rating(
+        players: list[
+            DwarfWarrior,
+            DwarfBlacksmith,
+            ElfRanger,
+            Druid
+        ]
+) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: Optional[list]) -> None:
+def elves_concert(elves: [list[Elf]]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: Optional[list]) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
