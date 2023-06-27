a01aae33b5bb1b2998fdbb05e52addc23763f56f
Alek Ilchenko <yaichenko87@gmail.com>
2023-06-27 09:10:12 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
-from typing import Optional
 
 from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: Optional[list[Player]]) -> int:
-    return sum([player.get_rating() for player in players])
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elfs: Optional[list[Elf]]) -> None:
+def elves_concert(elfs: list[Elf]) -> None:
     for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: Optional[list[Dwarf]]) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
