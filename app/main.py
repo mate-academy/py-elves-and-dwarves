48be77824b04ea335233793eacce01a787dec521
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-06-27 13:13:53 +0300

    Changed typehints

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-def calculate_team_total_rating(players: list) -> None:
+from typing import Optional
+
+
+def calculate_team_total_rating(players: Optional[list]) -> None:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: Optional[list]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: Optional[list]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
