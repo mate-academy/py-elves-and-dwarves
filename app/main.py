2e983be710acc205ea4e672e536faacab005e1e9
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-06-29 20:11:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from __future__ import annotations
+
+from app.players.dwarves import Dwarf
+from app.players.elves import Elf
+from app.players import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    """Returns the sum of the ratings for all team members"""
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
