510b4d23169edc34873366862808479e6ba26f05
Maksym Hovorunenko <govorunenko@gmail.com>
2023-06-30 16:44:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
-# from players.elves.elf_ranger import ElfRanger
-# from players.elves.druid import Druid
-# from players.dwarves.dwarf_warrior import DwarfWarrior
-# from players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
