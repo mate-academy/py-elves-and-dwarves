d754500940798774c03332112dc5b59cc6de9d01
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-07-05 14:23:09 +0300

    - fixed length of line

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,4 +7,5 @@ class Elf(Player):
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+        print(f"{self.nickname} is playing "
+              f"a song on the {self._musical_instrument}")
