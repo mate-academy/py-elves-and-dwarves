9d580b6d7445b27c93c544b5c5cd31e26130d43e
Vladyslav Shepilov <shepsterya@gmail.com>
2023-07-10 22:46:01 +0300

    with __init__.py -es

---
+++ app/players/__init__.py
@@ -0,0 +1,3 @@
+# from .player import Player
+# from .elves.elf import Elf
+# from .dwarves.dwarf import Dwarf
---
+++ app/players/dwarves/__init__.py
@@ -0,0 +1,3 @@
+# from .dwarf import Dwarf
+# from .dwarf_warrior import DwarfWarrior
+# from .dwarf_blacksmith import DwarfBlacksmith
