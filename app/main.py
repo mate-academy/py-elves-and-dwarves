a2b9558b6edc636e10dac4ea8179bc336889a2f9
Maksym Pavliuchenko <maksym.pavliuchenko@scalors.com>
2023-07-18 19:19:39 +0300

    fix syntax issues

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 
 
 def calculate_team_total_rating(players: list) -> int:
-    return sum((player.get_rating() for player in players))
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list) -> None:
-    [elf.play_elf_song() for elf in elves]
+    for elf in elves:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves: list) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in dwarves]
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
