287d3f10103812a026eca824de6fb49600321b0d
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-07-21 20:05:29 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -4,18 +4,19 @@ from app.players.elves.druid import Druid
 from app.players.dwarves.dwarf import Dwarf
 from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from typing import Union
 
 
-def calculate_team_total_rating(players: list[Elf, Dwarf]) -> int:
+def calculate_team_total_rating(players: list[Union[Elf, Dwarf]]) -> int:
     return sum(elem.get_rating() for elem in players)
 
 
-def elves_concert(list_elfs: list[ElfRanger, Druid]) -> None:
+def elves_concert(list_elfs: list[Union[ElfRanger, Druid]]) -> None:
     for elem in list_elfs:
         elem.play_elf_song()
 
 
-def feast_of_the_dwarves(list_dwarves: list[DwarfWarrior,
-                                            DwarfBlacksmith]) -> None:
+def feast_of_the_dwarves(list_dwarves: list[Union[DwarfWarrior,
+                                            DwarfBlacksmith]]) -> None:
     for elem in list_dwarves:
         elem.eat_favourite_dish()
