87aa3e0ceea983436bf0076e7e1d5801c956497d
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-25 22:50:24 +0300

    Update druid.py

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -11,8 +11,9 @@ class Druid(Elf):
         self.favourite_spell = favourite_spell
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. {self.nickname} " \
-               f"has a favourite spell: {self._favourite_spell}"
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a "
+                f"favourite spell: {self._favourite_spell}")
 
     def get_rating(self) -> int:
-        return len(self.favourite_spell)
+        return len(self._favourite_spell)
