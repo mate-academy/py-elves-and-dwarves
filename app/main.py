786647afdf65c49e29f6ddfdfc6ed347bd825e27
H1kki <vetalkuz102@gmail.com>
2023-07-28 15:11:01 +0300

    added more precise annotations

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
+from .players.elves.elf import Elf
+from .players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list) -> int | float:
+def calculate_team_total_rating(
+        players: list[Elf] | list[Dwarf]
+) -> int | float:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
