31b697806ffd454d0f13612e6e3398a3abf29caa
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-31 15:43:21 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def calculate_team_total_rating(persons: list[Player]) -> int:
     return sum(person.get_rating() for person in persons)
 
 
-def elves_concert(persons: list[Elf]) -> list[None]:
-    return [person.play_elf_song() for person in persons]
+def elves_concert(persons: list[Elf]) -> None:
+    for person in persons:
+        person.play_elf_song()
 
 
-def feast_of_the_dwarves(persons: list[Dwarf]) -> list[None]:
-    return [person.eat_favourite_dish() for person in persons]
+def feast_of_the_dwarves(persons: list[Dwarf]) -> None:
+    for person in persons:
+        person.eat_favourite_dish()
