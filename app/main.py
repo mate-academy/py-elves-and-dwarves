9797d356812d57063555cb1a6cf953515f6b3eb6
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-31 14:20:50 +0300

    Annotations fixed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-def calculate_team_total_rating(persons: list) -> int:
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(persons: list[Player]) -> int:
     return sum(person.get_rating() for person in persons)
 
 
-def elves_concert(persons: list) -> list[str]:
+def elves_concert(persons: list[Elf]) -> list[None]:
     return [person.play_elf_song() for person in persons]
 
 
-def feast_of_the_dwarves(persons: list) -> list[str]:
+def feast_of_the_dwarves(persons: list[Dwarf]) -> list[None]:
     return [person.eat_favourite_dish() for person in persons]
