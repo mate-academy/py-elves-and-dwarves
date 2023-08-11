02a6d69cfc5986aee4982dd9cc30721e2c9a80db
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-11 18:12:00 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from players.elves.elf_ranger import ElfRanger
-from players.elves.druid import Druid
-from players.dwarves.dwarf_warrior import DwarfWarrior
-from players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.elves.elf_ranger import ElfRanger
+from app.elves.druid import Druid
+from app.dwarves.dwarf_warrior import DwarfWarrior
+from app.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
 elf_ranger = ElfRanger
@@ -11,17 +11,15 @@ dwarf_blacksmith = DwarfBlacksmith
 
 
 def calculate_team_total_rating(players: list) -> int:
-    total = 0
-    for player in players:
-        total += player.rating
+    total = sum(player.rating for player in players)
     return total
 
 
-def elves_concert(Elf: list, play_elf_song: list) -> None:
-    for elf in Elf:
-        elf.play_song(play_elf_song)
+def elves_concert(elf: list, play_elf_song: list) -> None:
+    for elfs in elf:
+        elfs.play_song(play_elf_song)
 
 
-def feast_of_the_dwarves(Dwarf: list, eat_favourite_dish: list) -> None:
-    for dwarf in Dwarf:
-        dwarf.favourite_dish(eat_favourite_dish)
+def feast_of_the_dwarves(dwarf: list, eat_favourite_dish: list) -> None:
+    for dwarfs in dwarf:
+        dwarfs.favourite_dish(eat_favourite_dish)
