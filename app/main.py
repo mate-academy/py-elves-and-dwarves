d770f949805c58062ada5bfcbb1f53764a88ccac
Vitalii Zhabskii <vitalik15031996@gmail.com>
2023-08-14 17:38:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,25 +1,35 @@
-from app.elves.elf_ranger import ElfRanger
-from app.elves.druid import Druid
-from app.dwarves.dwarf_warrior import DwarfWarrior
-from app.dwarves.dwarf_blacksmith import DwarfBlacksmith
-
-
-elf_ranger = ElfRanger
-druid = Druid
-dwarf_warrior = DwarfWarrior
-dwarf_blacksmith = DwarfBlacksmith
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
 def calculate_team_total_rating(players: list) -> int:
-    total = sum(player.rating for player in players)
-    return total
+    total_ratings = sum(player.get_rating() for player in players)
+    return total_ratings
 
 
-def elves_concert(elf: list, play_elf_song: list) -> None:
-    for elfs in elf:
-        elfs.play_song(play_elf_song)
+def elves_concert(elf_player: list) -> None:
+    for elf in elf_player:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf: list, eat_favourite_dish: list) -> None:
-    for dwarfs in dwarf:
-        dwarfs.favourite_dish(eat_favourite_dish)
+def feast_of_the_dwarves(dwarfs_players: list) -> None:
+    for dwarf in dwarfs_players:
+        dwarf.eat_favourite_dish()
+
+# Example usage
+
+    elf_ranger = ElfRanger("Lego las", "Harp", 5)
+    druid = Druid("Malfunction", "Flute", "Nature's Wrath")
+    dwarf_warrior = DwarfWarrior("Gimli", "Roast Lamb", 8)
+    dwarf_blacksmith = DwarfBlacksmith("Baling", "Dwarves Ale", 7)
+
+    elf_players = [elf_ranger, druid]
+    dwarf_players = [dwarf_warrior, dwarf_blacksmith]
+
+    elves_concert(elf_players)
+    feast_of_the_dwarves(dwarf_players)
+
+    total_rating = calculate_team_total_rating(elf_players + dwarf_players)
+    print("Total Team Rating:", total_rating)
---
+++ app/players/elves/elf.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+
+from app.players.player import Player
+
+
+class Elf(Player):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the "
+              f"{self._musical_instrument}")
+
+    @abstractmethod
+    def get_rating(self) -> int:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> str:
+        pass
