0ffc52a8d4303c6346f6eba94eb12c2acd863ac8
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-08-14 21:16:48 +0300

    Solution

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -3,7 +3,10 @@ from app.players.dwarves.dwarf import Dwarf
 
 class DwarfWarrior(Dwarf):
     def __init__(
-        self, nickname: str, favourite_dish: str, hummer_level: int
+            self,
+            nickname: str,
+            favourite_dish: str,
+            hummer_level: int
     ) -> None:
         super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
