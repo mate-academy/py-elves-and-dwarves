b234c02df7a360c50e6beb07948d72375ae5ff29
Inna Isareva <inna8110302@gmail.com>
2023-08-15 20:19:17 +0300

    updates - version 3

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -12,8 +12,8 @@ class Druid(Elf):
         self._favourite_spell = favourite_spell
 
     def player_info(self) -> str:
-        return (f"Druid {self.nickname}. " 
-                f"{self.nickname} has a favourite spell: " 
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a favourite spell: "
                 f"{self._favourite_spell}")
 
     def get_rating(self) -> int:
