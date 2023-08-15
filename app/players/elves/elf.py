7822c751dcbecee8bc3d2bcc5b702ff98ca458d4
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-15 20:31:08 +0200

    Update elf.py

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,7 +7,6 @@ class Elf(Player):
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
-
     
     @abstractmethod
     def get_rating(self) -> int:
