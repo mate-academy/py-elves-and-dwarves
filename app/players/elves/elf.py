b22671a8328ee923ca717a5c45cb3c6b90e0eb21
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-15 20:21:27 +0200

    Update elf.py

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,10 +7,7 @@ class Elf(Player):
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the "
-              f"{self._musical_instrument}")
+        
 
     @abstractmethod
     def get_rating(self) -> int:
@@ -18,4 +15,4 @@ class Elf(Player):
 
     @abstractmethod
     def player_info(self) -> str:
-        pass
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
