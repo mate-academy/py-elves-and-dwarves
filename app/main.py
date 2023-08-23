e49052019daa7a5662c9d915c85c73577cd5f23a
Maksim Stahovskij <maksss196@gmail.com>
2023-08-23 23:27:49 +0300

    py-elves-and-dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.Player import Player
+
+
+def calculate_team_total_rating(players: list) -> int:
+    team_rating = 0
+    for player in players:
+        if isinstance(player, Player):
+            player_rating = player.get_rating()
+            team_rating += player_rating
+    return team_rating
+
+
+def elves_concert(elves: list) -> str:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> str:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,13 +6,13 @@ import pytest
 
 from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
-from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
-from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.player import Player
+from app.players.dwarves.Dwarf import Dwarf
+from app.players.dwarves.Dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.Dwarf_warrior import DwarfWarrior
+from app.players.elves.Druid import Druid
+from app.players.elves.Elf import Elf
+from app.players.elves.Elf_ranger import ElfRanger
+from app.players.Player import Player
 
 
 @pytest.mark.parametrize(
