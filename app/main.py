971e1d4fa3784a122280bd2d6773c48833e9b5a7
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-08-23 16:11:30 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def calculate_team_total_rating(
         list_of_players: list[Player]
 ) -> int:
     if list_of_players:
-        return sum([player.get_rating() for
-                    player in list_of_players])
+        return sum(
+            [player.get_rating() for player in list_of_players]
+        )
     return 0
 
 
