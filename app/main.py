081258bd7406c67a1d35ffd2ea815798c926441a
Nadiia <nadiia.developer777@gmail.com>
2023-09-05 16:15:04 +0300

    Solution-py-elves-and-dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> None:
+    total_rating = sum(player.get_rating() for player in team)
+    return total_rating
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
