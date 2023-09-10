da24e017aad09a9cc944d79208ecb059db0351ab
Anton <anton380969658858@gmail.com>
2023-09-10 14:12:23 +0300

    changed type annotation

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: [Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: [Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: [Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
