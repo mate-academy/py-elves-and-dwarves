328c32f95555b75d3ca32f4f97e8e7b9c264c776
Anton <tkacuk2291@gmail.com>
2023-09-12 21:33:31 +0300

    Solution_edit

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -2,15 +2,13 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 class DwarfWarrior(Dwarf):
-    def __init__(self, nickname: str,
-                 favourite_dish: str, hummer_level: int) -> None:
+    def __init__(self, nickname: str, favourite_dish: str, hummer_level: int) -> None:
+        super().__init__(nickname=nickname, favourite_dish=favourite_dish)
         self._hummer_level: int = hummer_level
-        super().__init__(nickname, favourite_dish)
 
     def player_info(self) -> str:
-        return f"Dwarf warrior {self._nickname}" \
-               f". {self._nickname} " \
-               f"has a hummer of the {self._hummer_level} level"
+        return (f"Dwarf warrior {self._nickname}. {self._nickname} "
+                f"has a hummer of the {self._hummer_level} level")
 
     def get_rating(self) -> int:
         return self._hummer_level + 4
