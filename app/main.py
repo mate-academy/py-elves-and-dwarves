2c9da641afa2865221c3815d756699e60e0b7b63
Depaton <you@example.com>
2023-09-22 16:43:06 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+
+
+def calculate_team_total_rating(person_list: list) -> int:
+    return sum(name.get_rating() for name in person_list)
+
+
+def elves_concert(elves_list: list[Elf]) -> None:
+    for name in elves_list:
+        name.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves_list: list[Dwarf]) -> None:
+    for name in dwarves_list:
+        name.eat_favourite_dish()
