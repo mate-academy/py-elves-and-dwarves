b3a856eec5f350224980d56b8470a6533f5b9d9a
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-09-24 11:36:54 +0300

    'Solution_elves-and-dwarves'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
+    team_rating = 0
+    for hero in team:
+        team_rating += hero.get_rating()
+    return team_rating
+
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
