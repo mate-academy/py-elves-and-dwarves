f570ffedaa7e88775ce68f8551a54ec1859ca24e
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-03 10:53:37 +0300

    Completed the task "Elves and Dwarves".

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def calculate_team_total_rating(players: list) -> int:
+    return sum(rating.get_rating() for rating in players)
+
+
+def elves_concert(elves: list) -> None:
+    [elf.play_elf_song() for elf in elves]
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in dwarves]
