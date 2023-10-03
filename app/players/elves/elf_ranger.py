9f37488f3c314984649b8daffc5c884d69cb65dc
Marina <marina.ua13@gmail.com>
2023-10-03 15:36:49 +0300

    'Solution1'

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -15,6 +15,6 @@ class ElfRanger(Elf):
         return 3 * self._bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. {self.nickname} " \
-               f"has bow of the {self._bow_level} " \
-               f"level"
+        return (f"Elf ranger {self.nickname}. {self.nickname} "
+                f"has bow of the {self._bow_level} "
+                f"level")
