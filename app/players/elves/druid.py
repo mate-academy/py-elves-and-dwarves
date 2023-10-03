77a49d0cb4db2af9e5c6b3bdbe196922cebaad6c
Marina <marina.ua13@gmail.com>
2023-10-03 13:34:44 +0300

    'Solution'

---
+++ app/players/elves/druid.py
@@ -0,0 +1,14 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(self, musical_instrument: str, nickname: str, favourite_spell: str) -> None:
+        super().__init__(nickname=nickname, musical_instrument=musical_instrument)
+        self._favourite_spell = favourite_spell
+        pass
+
+    def get_rating(self) -> int:
+        return len(self._favourite_spell)
+
+    def player_info(self) -> str:
+        return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self._favourite_spell}"
