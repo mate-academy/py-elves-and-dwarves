79b714694a2c954ce2647fc59bd1d25b3200562c
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-10-06 16:19:59 +0300

    feat: almost done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    sum = 0
+    for player in players:
+        sum += player.get_rating()
+
+
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarws: list[Dwarf]) -> None:
+    for dwarw in dwarws:
+        dwarw.eat_favourite_dish()
