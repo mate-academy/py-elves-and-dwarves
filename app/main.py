5e5c152d00e4203a5116c08569d99a84af3d97a5
Andrey Britov <abritov82@gmail.com>
2023-10-07 15:45:31 +0300

    Solusion

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(players: list) -> int:
+    return sum(player_one.get_rating() for player_one in players)
+
+
+def elves_concert(players: list) -> None:
+    for player_one in players:
+        if player_one is ElfRanger or player_one is Druid:
+            player_one.play_elf_song
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf_one in dwarves:
+        dwarf_one.eat_favorite_dish
+
+
+# dwarves = [
+#     DwarfWarrior(nickname="Thiddeal", favourite_dish="French Fries", hummer_level=3),
+#     DwarfWarrior(nickname="Dwarf", favourite_dish="Caesar Salad", hummer_level=3),
+# ]
+# feast_of_the_dwarves(dwarves)
+# # "Thiddeal is eating French Fries"
+# # "Dwarf is eating Caesar Salad"
+
+
+
+
+
+
+
+
+
---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,17 @@
+from abc import abstractmethod, ABC
+from app.players.player import Player
+
+
+class Dwarf(Player, ABC):
+
+    def __init__(
+            self,
+            nickname: str,
+            favourite_dish: str,
+    ) -> None:
+        self._favourite_dish = favourite_dish
+        Dwarf.nickname = nickname
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname}"
+              f" is eating {self._favourite_dish}")
