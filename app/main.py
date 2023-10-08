9ee658bc8ceccb67e0d871d3013444150dcf09f2
Your_Name Your_Surname <your@email>
2023-10-08 15:53:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(list_players: list["Player"]) -> int:
+    total_rating = 0
+    for player in list_players:
+        total_rating += player.get_rating()
+    return total_rating
+
+
+def elves_concert(list_elves: list["Elf"]) -> None:
+    for elves in list_elves:
+        elves.play_elf_song()
+
+
+def feast_of_the_dwarves(list_dwarves: list["Dwarf"]) -> None:
+    for dwarf in list_dwarves:
+        dwarf.eat_favourite_dish()
