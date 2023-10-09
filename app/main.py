795b7da41faec6228dc4d4597c2f19718531d556
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-10-09 16:26:26 +0300

    Solution for py_elves_and_dwarves

--- app/main.py
+++ app/main.py
@@ -1,65 +1,17 @@
-from abc import ABC, abstractmethod
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-class Player(ABC):
-    @abstractmethod
-    def __init__(self, nickname) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self) -> str:
-        if self is ElfRanger:
-            return 3 * self.bow_level
-
-    @abstractmethod
-    def player_info(self) -> str:
-            if self is ElfRanger:
-                return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self.favourite_spell}"
-            if self is Druid:
-                return f"Dwarf warrior {self.nickname}. {self.nickname} has a hummer of the {self.hummer_level} level"
-            if self is DwarfWarrior:
-                return f"Dwarf blacksmith {self.nickname} with skill of the {self.skill_level} level"
-            if self is DwarfBlacksmith:
-                return f"Dwarf blacksmith {self.nickname} with skill of the {self.skill_level} level"
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
 
 
-class Elf(Player):
-    def __init__(self, nickname, musical_instrument) -> None:
-        super().__init__(nickname)
-        self._musical_instrument = musical_instrument
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
 
 
-class Dwarf(Player):
-    def __init__(self, favourite_dish) -> None:
-        super().__init__(favourite_dish)
-        self._favourite_dish = favourite_dish
-
-    def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} is eating {self._favourite_dish}")
-
-
-class ElfRanger(Elf):
-    def __init__(self, name, musical_instrument, bow_level: int) -> None:
-        super().__init__(name, musical_instrument)
-        self._bow_level = bow_level
-
-
-class Druid(Elf):
-    def __init__(self, name, musical_instrument, favourite_spell) -> None:
-        super().__init__(name, musical_instrument)
-        self._favourite_spell = favourite_spell
-
-
-class DwarfWarrior(Dwarf):
-    def __init__(self, name, favourite_dish, hummer_level) -> None:
-        super().__init__(name, favourite_dish)
-        self._hummer_level = hummer_level
-
-
-class DwarfBlacksmith(Dwarf):
-    def __init__(self, name, favourite_dish, skill_level: int) -> None:
-        super().__init__(name, favourite_dish)
-        self._skill_level = skill_level
+def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
+    for dwarf in dwarfs:
+        dwarf.eat_favourite_dish()
