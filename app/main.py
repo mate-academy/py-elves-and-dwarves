15b93cdecffa32d3e03fa22c0d3fc03926f68d68
Denys <den.prislipskyi@gmail.com>
2023-10-11 18:26:51 +0200

    fix i inside for

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
-    return sum(i.get_rating() for i in team)
+    return sum(level.get_rating() for level in team)
 
 
 def elves_concert(elves: list[Elf]) -> None:
-    for i in elves:
-        i.play_elf_song()
+    for elf in elves:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
-    for i in dwarves:
-        i.eat_favourite_dish()
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
