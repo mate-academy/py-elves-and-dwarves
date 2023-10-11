352c2104065ef07ebd3f0256d04f7b83d470720d
Denis Komandyr <sabsdaid@gmail.com>
2023-10-12 02:34:50 +0300

    fixed annotations

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-def calculate_team_total_rating(players_list: list) -> int:
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+def calculate_team_total_rating(players_list: list[Player]) -> int:
     return sum([player.get_rating() for player in players_list])
 
 
-def elves_concert(elf_list: list) -> None:
+def elves_concert(elf_list: list[Elf]) -> None:
     for elf in elf_list:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf_list: list) -> None:
+def feast_of_the_dwarves(dwarf_list: list[Dwarf]) -> None:
     for dwarf in dwarf_list:
         dwarf.eat_favourite_dish()
