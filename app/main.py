3332087c59a6719610260b160ea2a29ff9e945eb
Max Muhanuk <ytmaksik425@gmail.com>
2023-10-16 14:22:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    return sum(i.get_rating() for i in team)
+
+
+def elves_concert(elf: list) -> None:
+    for i in elf:
+        i.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for i in dwarves:
+        i.eat_favourite_dish()
---
+++ app/players/elves/druid.py
@@ -0,0 +1,13 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(self, nickname: str, musical_instrument: str, favourite_spell: str) -> None:
+        super().__init__(nickname, musical_instrument)
+        self.__favourite_spell = favourite_spell
+
+    def player_info(self):
+        return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self.__favourite_spell}"
+
+    def get_rating(self) -> int:
+        return len(self.__favourite_spell)
