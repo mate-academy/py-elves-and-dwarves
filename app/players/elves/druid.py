26312def9edafa6ca2901ba5e728fdf729679003
Danylo Sheremeta <danylo3002@gmail.com>
2023-10-17 20:52:19 +0300

    Solution

---
+++ app/players/elves/druid.py
@@ -0,0 +1,20 @@
+from ..elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            favourite_spell: str
+    ) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._favourite_spell = favourite_spell
+
+    def player_info(self) -> str:
+        name = self.nickname
+        spell = self._favourite_spell
+        return f"Druid {name}. {name} has a favourite spell: {spell}"
+
+    def get_rating(self) -> int:
+        return len(self._favourite_spell)
---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,20 @@
+from ..elves.elf import Elf
+
+
+class ElfRanger(Elf):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            bow_level: int
+    ) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def player_info(self) -> str:
+        name = self.nickname
+        lvl = self._bow_level
+        return f"Elf ranger {name}. {name} has bow of the {lvl} level"
+
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
