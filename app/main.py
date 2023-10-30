323067cb79f81b4cd9629d427ae9bfd63479be1c
Denysov Viacheslav <wdenisov113@gmail.com>
2023-10-30 13:53:17 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 
 
 def calculate_team_total_rating(team: list) -> int:
-    return sum([teammate.get_rating() for teammate in team])
+    return sum(teammate.get_rating() for teammate in team)
 
 
 def elves_concert(elves: list) -> None:
