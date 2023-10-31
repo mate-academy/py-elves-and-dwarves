48c02defe75c1b55b89eb325bcfcdb05c6014323
Yuliia Dudych <juliadudych@gmail.com>
2023-10-31 22:42:27 +0200

    Add function to 'main' to have dwarves' feast

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from app.players.player import Player
 from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
@@ -12,3 +13,8 @@ def calculate_team_total_rating(players: list[Player]) -> int:
 def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
