7d4dd0d6e00ffb6e794f4fbc9005e00436a09413
Yuliia Dudych <juliadudych@gmail.com>
2023-10-31 22:41:35 +0200

    Add function to 'main' to play elves concert

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from app.players.player import Player
+from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
@@ -6,3 +7,8 @@ def calculate_team_total_rating(players: list[Player]) -> int:
     for player in players:
         total_rating += player.get_rating()
     return total_rating
+
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
