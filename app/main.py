802505418a5f894802623b0d7026ee1608bb68d2
Yuliia Dudych <juliadudych@gmail.com>
2023-10-31 22:40:39 +0200

    Add function to 'main' to calculate total team rating

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    total_rating = 0
+    for player in players:
+        total_rating += player.get_rating()
+    return total_rating
