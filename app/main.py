56971027463c648443609e176ab81f2ca34cbe43
lyaskov <lyaskov1@gmail.com>
2023-11-08 22:03:22 +0200

    fixed Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
 from typing import List
 
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
+from app.players.player import Player
 
 
-def calculate_team_total_rating(
-        players: List[ElfRanger | Druid | DwarfWarrior | DwarfBlacksmith]
-) -> int:
+def calculate_team_total_rating(players: List[Player]) -> int:
     return sum([player.get_rating() for player in players])
 
 
