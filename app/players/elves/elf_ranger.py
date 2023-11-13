a3b67b90beae1b0536ae618cda8dd8f0b78357b0
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2023-11-13 14:17:53 +0200

    'Add()'

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -14,6 +14,8 @@ class ElfRanger(Elf):
         return 3 * self._bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. " \
-               f"{self.nickname} has bow of the " \
-               f"{self._bow_level} level"
+        return (
+            f"Elf ranger {self.nickname}. "
+            f"{self.nickname} has bow of the "
+            f"{self._bow_level} level"
+        )
