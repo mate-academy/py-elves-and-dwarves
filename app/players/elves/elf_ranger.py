c704fabf23b1d217b3cf8b91291a6f67cfaa1862
Ostap <ostapbojko04@gmail.com>
2023-11-15 11:37:14 +0200

    py-elves(fixed 3 month)

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -15,5 +15,7 @@ class ElfRanger(Elf):
         return 3 * self._bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. {self.nickname}" \
-               f" has bow of the {self._bow_level} level"
+        return (
+            f"Elf ranger {self.nickname}. {self.nickname}"
+            f" has bow of the {self._bow_level} level"
+        )
