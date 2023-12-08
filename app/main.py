49325973a6351a700430e17deb3724e1479ce83a
Hermesvlad <hermesvlad@gmail.com>
2023-12-08 18:41:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def calculate_team_total_rating(team: list) -> int:
     return total_rating
 
 
-def elves_concert(elves: list[Elf]) -> list[Elf]:
-    return [elf.play_elf_song() for elf in elves]
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        print(elf.play_elf_song())
 
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]) -> list[Dwarf]:
-    return[dwarf.eat_favourite_dish() for dwarf in dwarves]
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        print(dwarf.eat_favourite_dish())
