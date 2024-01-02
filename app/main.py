09054db43ef5613a206955eadf8b0ae5ac63f237
angelzagniy <angelzagniy@gmail.com>
2024-01-02 13:50:43 +0200

    change importing file to import class for Player and remove extra blank lines

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-from app.players import player
-
+from app.players.player import Player
 from app.players.elves.elf import Elf
-
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list[player.Player]) -> int:
+def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(each_player.get_rating() for each_player in players)
 
 
