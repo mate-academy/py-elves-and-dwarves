430730d9a024c3e399f4cc31f5185b6fb27d3324
Valeriy Semenik <valeriy.semenik@gmail.com>
2024-01-04 16:32:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(team: [Player]) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: [Elf]) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
