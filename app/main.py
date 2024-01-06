8ce25556c438a436f72f53f85e4385f91e520123
Arsen Rudnytskyy <acikz.33@google.com>
2024-01-06 15:09:52 +0100

    Remove import and fix total rating calculation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
-from __future__ import annotations
-
 from app.players.player import Player
 from app.players.dwarves.dwarf import Dwarf
 from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
-    rating = 0
-    for player in team:
-        rating += player.get_rating()
-    return rating
+    return sum(player.get_rating() for player in team)
 
 
 def elves_concert(elves: list[Elf]) -> None:
