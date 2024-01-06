e2e51dbcc07efc1e8ba85ab368bd3e3265b03556
Arsen Rudnytskyy <acikz.33@google.com>
2024-01-06 14:03:55 +0100

    Type fixing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
 from app.players.player import Player
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
-from app.players.elves.elf_ranger import ElfRanger
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
@@ -14,13 +12,13 @@ def calculate_team_total_rating(team: list[Player]) -> int:
     return rating
 
 
-def elves_concert(elves: list[Druid | ElfRanger]) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
 def feast_of_the_dwarves(
-        dwarves: list[DwarfWarrior | DwarfBlacksmith]
+        dwarves: list[Dwarf]
 ) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
