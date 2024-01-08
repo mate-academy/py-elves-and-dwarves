cd9dfacbed917b88158cb1b692c33c78108991f7
andriy-py <andriy.omelya@gmail.com>
2024-01-08 13:51:09 +0200

    task complite

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,7 @@
 import inspect
 import io
 from contextlib import redirect_stdout
-
 import pytest
-
 from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
 from app.players.dwarves.dwarf import Dwarf
@@ -11,7 +9,7 @@ from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves import ElfRanger
+from app.players.elves.elf_ranger import ElfRanger
 from app.players.player import Player
 
 
