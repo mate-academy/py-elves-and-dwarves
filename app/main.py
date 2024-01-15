82d290b052aadc9a50d466dbf64e2e88f0bf7626
Ded <71114713+DenZatolokin@users.noreply.github.com>
2024-01-15 12:02:12 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,10 +5,6 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    # sum_rating = 0
-    # for player in players:
-    #     player_ = player
-    #     sum_rating += player_.get_rating()
     return sum(player.get_rating() for player in players)
 
 
