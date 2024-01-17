7be55556b30faacee74c3bc61d63b4d4c67da376
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-17 21:32:21 +0200

    Solution with fixed annotations

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elfs: list) -> list:
+def elves_concert(elfs: list[Elf]) -> list:
     return [elf.play_elf_song() for elf in elfs]
 
 
-def feast_of_the_dwarves(dwarves: list) -> list:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> list:
     return [dwarf.eat_favourite_dish() for dwarf in dwarves]
