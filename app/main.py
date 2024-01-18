b26ae269f911b4b2a7d360b1d0e2085763b6086c
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-01-18 15:12:49 +0200

    Changed oneliners into loops, fixed type annotation for functions to None

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elfs: list[Elf]) -> list:
-    [elf.play_elf_song() for elf in elfs]
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]) -> list:
-    [dwarf.eat_favourite_dish() for dwarf in dwarves]
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
