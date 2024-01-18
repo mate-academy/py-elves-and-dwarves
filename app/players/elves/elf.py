3614f3c45483bbf60248ddcdfbae755615e986d9
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-01-18 15:29:48 +0200

    Fixed Solution

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,6 +7,5 @@ class Elf(Player):
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} "
-              f"is playing a song on the "
-              f"{self._musical_instrument}")
+        print(f"{self.nickname} is playing a song "
+              f"on the {self._musical_instrument}")
