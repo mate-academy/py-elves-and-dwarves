2774c2c30142ab4bc2fbeeddeed11e385e8c5595
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-19 18:17:04 +0200

    fix position of brackets

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -5,8 +5,7 @@ class DwarfWarrior(Dwarf):
     def __init__(self,
                  nickname: str,
                  favourite_dish: str,
-                 hummer_level: int
-                 ) -> None:
+                 hummer_level: int) -> None:
         super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -5,8 +5,7 @@ class Druid(Elf):
     def __init__(self,
                  nickname: str,
                  musical_instrument: str,
-                 favourite_spell: str
-                 ) -> None:
+                 favourite_spell: str) -> None:
         super().__init__(nickname, musical_instrument)
         self._favourite_spell = favourite_spell
 
