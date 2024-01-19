4088848b0e264a5d81ba919ee805e623f0dcae83
Nick Illich <nickillich96@gmail.com>
2024-01-19 18:24:14 +0200

    'Solution'

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -13,10 +13,12 @@ class ElfRanger(Elf):
         super().__init__(nickname, musical_instrument)
         self._bow_level = bow_level
 
-    def player_info(self) -> None:
-        return f"Elf ranger {self.nickname}." \
-               f" {self.nickname} has bow of the " \
-               f"{self._bow_level} level"
+    def player_info(self) -> str:
+        return (
+            f"Elf ranger {self.nickname}."
+            f" {self.nickname} has bow of the "
+            f"{self._bow_level} level"
+        )
 
     def get_rating(self) -> int:
         return 3 * self._bow_level
