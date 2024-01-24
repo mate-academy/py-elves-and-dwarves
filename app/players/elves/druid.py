d6c7118189e765ebc2ff04a67998b957dfe18b86
D Petetskyi <4477617@gmail.com>
2024-01-24 10:37:51 +0200

    Solution

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -2,12 +2,18 @@ from app.players.elves.elf import Elf
 
 
 class Druid(Elf):
-    def __init__(self, nickname: str, musical_instrument: str, favourite_spell: str) -> None:
+    def __init__(
+            self, nickname: str, musical_instrument: str, favourite_spell: str
+    ) -> None:
         super().__init__(nickname, musical_instrument)
         self._favourite_spell = favourite_spell
 
-    def player_info(self) -> None:
-        print(f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self._favourite_spell}")
+    def player_info(self) -> str:
+        info = (
+            f"Druid {self.nickname}. {self.nickname} "
+            f"has a favourite spell: {self._favourite_spell}"
+        )
+        return info
 
     def get_rating(self) -> int:
         return len(self._favourite_spell)
