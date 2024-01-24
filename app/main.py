33b14eb70046f4425be9479e8a02d1adef001d4d
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-01-24 15:44:47 +0200

    made edits

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: list[Player]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Player]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
