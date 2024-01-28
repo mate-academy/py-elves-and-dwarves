692221732d933b87d3e5b4b14d74129f4a5435fb
Alex Grytsai <a.grytsai1987@gmail.com>
2024-01-28 14:04:06 +0200

    Fixed def calculate_team_total_rating

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.players.player import Player
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    return sum([player.get_rating() for player in players])
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(players: list[Elf]) -> None:
