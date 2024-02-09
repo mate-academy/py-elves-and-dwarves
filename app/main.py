6e34b7a8ea2081e0a8d5dd8788ff5466a184ce37
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-02-09 12:41:42 +0200

    add annotation type for class in list

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-def calculate_team_total_rating(elves: list) -> int:
-    return sum(elf.get_rating() for elf in elves)
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
 
 
-def elves_concert(elves: list) -> None:
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
