98a84f60c63f5c23985774287bcd8e64573e27a4
Mariia Sydoruk <marisydoruk12@gmail.com>
2024-02-09 16:20:19 +0200

    First commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+
+
 def calculate_team_total_rating(players: list) -> int:
     return sum(player_rating.get_rating() for player_rating in players)
 
