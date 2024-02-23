a3cd2bd32cff03a58dc4cde5a17f0bc8725172f1
Maksim <sma4no.7@gmail.com>
2024-02-23 14:37:34 +0100

    Solution

---
+++ app/players/player.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+
+
+class Player(ABC):
+    nickname = []
+
+    @abstractmethod
+    def get_rating(self) -> None:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> None:
+        pass
