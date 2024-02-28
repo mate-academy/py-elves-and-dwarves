1f585a5b1a17ffec096e189fcbd5b658d04d6956
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-02-28 18:50:50 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,11 +3,9 @@ def calculate_team_total_rating(team: list) -> int:
 
 
 def elves_concert(elves: list) -> None:
-    for elf in elves:
-        elf.play_elf_song()
+    [elf.play_elf_song() for elf in elves]
 
 
 def feast_of_the_dwarves(dwarves: list) -> None:
-    # for dwarf in dwarves:
-    print("\n".join([f"{dwarf.nickname} is eating "
-                     f"{dwarf._favourite_dish}" for dwarf in dwarves]))
+    for dwarf in dwarves:
+        print(f"{dwarf.nickname} is eating {dwarf._favourite_dish}")
