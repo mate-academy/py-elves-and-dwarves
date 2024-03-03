18eb1e2d5b1660a58c2b467ab9e1a54eabbfbc3f
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-03 17:50:06 +0200

    'new_Line'

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 from app.players.player import Player
 
 
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -3,11 +3,11 @@ from app.players.elves.elf import Elf
 
 class ElfRanger(Elf):
     def __init__(
-                 self,
-                 nickname: str,
-                 musical_instrument: str,
-                 bow_level: int
-                 ) -> None:
+        self,
+        nickname: str,
+        musical_instrument: str,
+        bow_level: int
+    ) -> None:
         super().__init__(nickname, musical_instrument)
         self.bow_level = bow_level
 
