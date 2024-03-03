8bf6fb4062360096b7d1726c1d8aa3768390e258
Maksym Hekalo <maximgekalo@gmail.com>
2024-03-03 17:32:35 +0200

    implement Elves and Dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+
+def calculate_team_total_rating(players_group: list) -> int:
+    return sum(teammate.get_rafting() for teammate in players_group)
+
+
+def elves_concert(elfs_group: list) -> None:
+    for elf in elfs_group:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarfs_group: list) -> None:
+    for dwarf in dwarfs_group:
+        dwarf.eat_favourite_dish()
