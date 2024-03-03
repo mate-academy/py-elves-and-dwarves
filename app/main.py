5e675032d99d8eeab6f64e2bf0795db5529ed4b5
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-03-03 18:44:20 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ def calculate_team_total_rating(team: [Player]) -> int:
     return sum(member.get_rating() for member in team)
 
 
-def elves_concert(elves: Elf) -> [Elf]:
+def elves_concert(elves: [Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: Dwarf) -> [Dwarf]:
+def feast_of_the_dwarves(dwarves: [Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
