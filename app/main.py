68d166b0242f520cddd970bd74f38eb408d01b64
skeletronys <mykytcey@gmail.com>
2024-03-05 01:25:13 +0100

    change import

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 from typing import List
 from app.players.player import Player
-from app.players.dwarves import dwarf
-from app.players.elves import elf
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team: List[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elfs: List[elf]) -> None:
+def elves_concert(elfs: List[Elf]) -> None:
     for player_elf in elfs:
         player_elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: List[dwarf]) -> None:
+def feast_of_the_dwarves(dwarves: List[Dwarf]) -> None:
     for player_dwarf in dwarves:
         player_dwarf.eat_favourite_dish()
