6bc462315bb0d7429dd81c453845c64a926c87fe
Mikola Rozhansky <bymikola@gmail.com>
2024-03-07 21:29:22 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from abc import abstractmethod, ABC
 
 
-class Player(ABC):
 
-    def __init__(self, nickname: str) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self) -> None:
-        pass
-
-    @abstractmethod
-    def player_info(self) -> None:
-        pass
 
 
 class Elf(Player):
@@ -93,4 +82,23 @@ class DwarfBlacksmith(Dwarf):
 
     def get_rating(self) -> int:
         return self._skill_level
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    suma = 0
+    for player in players:
+        suma += player.get_rating()
+    return suma
+
+
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
+    for dwarf in dwarfs:
+        dwarf.eat_favourite_dish()
+
+
 
