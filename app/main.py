9799194fcf555771ce4b9dbcf7f8ad6d4268bdb0
Mikola Rozhansky <bymikola@gmail.com>
2024-03-07 20:10:55 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+from abc import abstractmethod, ABC
+
+
+class Player(ABC):
+
+    def __init__(self, nickname: str) -> None:
+        self.nickname = nickname
+
+    @abstractmethod
+    def get_rating(self) -> None:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> None:
+        pass
+
+
+class Elf(Player):
+    def __init__(self, musical_instrument: str, nickname: str) -> None:
+        super().__init__(nickname)
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+
+class Dwarf(Player):
+    pass
+
