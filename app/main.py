17a48f1b68a5bf890964b9eaef1b081aee09fae7
Mikola Rozhansky <bymikola@gmail.com>
2024-03-07 22:11:42 +0200

    all_ok

--- app/main.py
+++ app/main.py
@@ -8,10 +8,8 @@ def calculate_team_total_rating(players: list[Player]) -> int:
 
 
 def elves_concert(elfs: list[Elf]) -> None:
-    for elf in elfs:
-        elf.play_elf_song()
+    [elf.play_elf_song() for elf in elfs]
 
 
 def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
-    for dwarf in dwarfs:
-        dwarf.eat_favourite_dish()
+    [dwarf.eat_favourite_dish() for dwarf in dwarfs]
