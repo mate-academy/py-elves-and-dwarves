a415c11054d7c311aef6b9f4bf9745a7da57101e
antonver <anton.verbovyi@gmail.com>
2024-03-09 16:00:18 +0100

    Players classe are implemented

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
 def calculate_team_total_rating(players: list) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     [elf.play_elf_song() for elf in elves]
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     [dwarf.eat_favourite_dish() for dwarf in dwarves]
