ac4d56c01af54b477739150ed6c6697f4f6ef2b7
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-03-24 21:06:01 +0100

    Elves and Dworves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from typing import List
+
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(players_list: List[Player]) -> int:
+    return sum(player.get_rating() for player in players_list)
+
+
+def elves_concert(elfs_list: List[Elf]) -> None:
+    [elf.play_elf_song() for elf in elfs_list]
+
+
+def feast_of_the_dwarves(dwarfs_list: List[Dwarf]) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in dwarfs_list]
