5397bb7bc1995f76eb4a290a45b5fc131ad08f88
bujhm <Bujhm99@gmail.com>
2024-03-25 19:24:27 +0200

    types in list, use compreh, correct names

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
 def calculate_team_total_rating(team: list) -> int:
-    total_rating = 0
-    for player in team:
-        total_rating += player.get_rating()
-    return total_rating
+    return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list) -> None:
-    for elve in elves:
-        elve.play_elf_song()
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
-    for dwarv in dwarves:
-        dwarv.eat_favourite_dish()
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
