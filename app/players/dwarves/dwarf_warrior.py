8ccaa8f1dcac3a0d83de9a978e7a22d614a3422e
Serhii Marynin <serhii.marynin@gmail.com>
2024-03-26 00:40:12 +0200

    change private attributes to protected

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -7,11 +7,11 @@ class DwarfWarrior(Dwarf):
                  favourite_dish: str,
                  hummer_level: int) -> None:
         super().__init__(nickname, favourite_dish)
-        self.__hummer_level = hummer_level
+        self._hummer_level = hummer_level
 
     def player_info(self) -> str:
         return (f"Dwarf warrior {self.nickname}. {self.nickname} "
-                f"has a hummer of the {self.__hummer_level} level")
+                f"has a hummer of the {self._hummer_level} level")
 
     def get_rating(self) -> int:
-        return self.__hummer_level + 4
+        return self._hummer_level + 4
