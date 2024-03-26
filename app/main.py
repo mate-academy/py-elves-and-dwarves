498a3fcaaea93fc7d6ce52cb8a9f97ad217ecfce
bujhm <Bujhm99@gmail.com>
2024-03-26 08:29:27 +0200

    missing list, types abstract

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(team: list) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
