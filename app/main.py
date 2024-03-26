c32950a1d5a3464c56f34e1b7d3ff7bcff11fdaa
Vitalii <vitalikbashkiser@gmail.com>
2024-03-26 23:12:47 +0200

    Solution#3

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ def calculate_team_total_rating(players: List[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: List[Union[Elf, Dwarf]]) -> None:
+def elves_concert(elves: List[Union[Elf]]) -> None:
     for elf in elves:
         if isinstance(elf, Elf):
             elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: List[Union[Elf, Dwarf]]) -> None:
+def feast_of_the_dwarves(dwarves: List[Union[Dwarf]]) -> None:
     for dwarf in dwarves:
         if isinstance(dwarf, Dwarf):
             dwarf.eat_favourite_dish()
