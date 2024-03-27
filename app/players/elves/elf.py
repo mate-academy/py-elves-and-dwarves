cc21fa554ff275d1543b1453af3164cf548667d2
semermaks <semermaks@mail.com>
2024-03-27 10:18:59 +0200

    Elf __init__ ( _musical_instrument to musical_instrument)

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -4,9 +4,9 @@ from app.players.player import Player
 class Elf(Player):
 
     def __init__(self, nickname: str,
-                 _musical_instrument: str) -> None:
+                 musical_instrument: str) -> None:
         super().__init__(nickname)
-        self._musical_instrument = _musical_instrument
+        self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
         print(f"{self.nickname}"
