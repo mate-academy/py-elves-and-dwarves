532e9ceb4616011af8bfc0a3127d2d86af06f99b
serhii_chaban <boetz1995@gmail.com>
2024-03-31 17:30:48 +0300

    fixed3

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
-from app.players.dwarves.dwarf import Dwarf
-from app.players.elves.elf import Elf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(players: list) -> int:
+def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(players: list) -> None:
+def elves_concert(players: list[Player]) -> None:
     for player in players:
-        if issubclass(player.__class__, Elf):
-            player.play_elf_song()
+        player.play_elf_song()
 
 
-def feast_of_the_dwarves(players: list) -> None:
+def feast_of_the_dwarves(players: list[Player]) -> None:
     for player in players:
-        if issubclass(player.__class__, Dwarf):
-            player.eat_favourite_dish()
+        player.eat_favourite_dish()
