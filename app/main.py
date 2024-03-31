01edc4cfa0ebb913b33d56676bf94b1d35710622
Maryna Barannik <sober2night@gmail.com>
2024-03-31 12:14:19 +0200

    'Fix'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
