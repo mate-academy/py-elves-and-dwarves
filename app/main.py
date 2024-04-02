bb907488e6807e3613b05d7f687229740150ba6e
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-02 13:00:07 +0300

    fixing List comprehensions

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def calculate_team_total_rating(player_list: list[Player]) -> int:
 
 
 def elves_concert(list_elfs: list[Elf]) -> None:
-    [elf.play_elf_song() for elf in list_elfs]
+    for elf in list_elfs:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dw_list: list[Dwarf]) -> None:
-    [dw.eat_favourite_dish() for dw in dw_list]
+    for dw in dw_list:
+        dw.eat_favourite_dish()
