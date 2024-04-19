6ce08068c9a80306e3fcd20db36ebb7d99b2fbcc
Oleh <musiychuk09@gmail.com>
2024-04-19 11:54:43 +0300

    () was added instead of /

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -9,5 +9,7 @@ class Elf(Player, ABC):
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the"
-              f" {self._musical_instrument}")
+        print(
+            f"{self.nickname} is playing a song on the "
+            f"{self._musical_instrument}"
+        )
