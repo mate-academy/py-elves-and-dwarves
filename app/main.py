1763aff46c386fdec7110be982352587bbfcfb0f
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-04-21 14:07:41 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 def calculate_team_total_rating(players: list) -> float:
-    total_rating = 0.0
-    for player in players:
-        total_rating += player.get_rating()
+    total_rating = sum(player.get_rating() for player in players)
     return int(total_rating)
 
 
