d14ba61ee959b3ac0cf91a9242fe169a9381c711
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-04-21 17:08:53 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from typing import List
+
 from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: List[Player]) -> float:
-    return int(sum(player.get_rating() for player in players))
+def calculate_team_total_rating(players: List[Player]) -> float | int:
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: List[Elf]) -> None:
