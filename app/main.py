d38b608ca85c77ea0ea008037fb640310ccf4860
Fibonacci <volokitin.vitaliy@icloud.com>
2024-05-19 15:50:06 +0300

    Complated

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from typing import List
+
+
+def calculate_team_total_rating(team: List) -> int:
+    total_rating = 0
+    for player in team:
+        total_rating += player.get_rating()
+    return total_rating
+
+
+def elves_concert(elves: List) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: List) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
