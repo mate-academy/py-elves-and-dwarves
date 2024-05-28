df5ee9b19086904b4c22d0d3180a7ad7f2a9b5f1
Іван Білейчук <ivan.bileichuk@gmail.com>
2024-05-28 18:23:19 +0300

    Solution_v1

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,11 @@
+from app.players.player import Player
+from abc import ABC
+
+
+class Dwarf(Player, ABC):
+    def __init__(self, nickname: str, favorite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favorite_dish = favorite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favorite_dish}")
