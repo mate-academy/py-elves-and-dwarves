6903de6752208fa3943c5d7e7354dbc38def701d
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-01 16:49:08 +0200

    add fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team: list[Elf, Dwarf]) -> int:
-    return sum([player.get_rating() for player in team])
+    return sum(player.get_rating() for player in team)
 
 
 def elves_concert(elves: list[Elf]) -> None:
