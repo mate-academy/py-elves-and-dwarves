5b4d27fb7f5ffdbedae7e62681f9f3e8f1c0f78c
Ivan <ivan.seeker6@gmail.com>
2024-06-07 14:11:42 +0300

    sol 4

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ def elves_concert(elves: list) -> None:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list["DwarfBlacksmith", "DwarfWarrior"]) -> None:
+def feast_of_the_dwarves(dwarves: list["DwarfBlacksmith", "DwarfWarrior"]) \
+        -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
