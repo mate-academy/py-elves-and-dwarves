24e4cf979e638a934f460e4b0d3e2ec6d0c4ef52
Yevheniy Dolinskiy <zkorvoz02@gmail.com>
2024-06-12 15:44:05 +0200

    fix

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -6,7 +6,7 @@ class Elf(Player, ABC):
     def __init__(
             self,
             nickname: str,
-            musical_instrument
+            musical_instrument: str
     ) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
