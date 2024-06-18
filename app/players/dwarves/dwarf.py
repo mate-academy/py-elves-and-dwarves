e5e95226112dcd769b2be67501891325731a10d3
Liftoff <vladislav70707@gmail.com>
2024-06-18 13:13:50 +0200

    solution py-elves-and-dwarves

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from app.players.player import Player
+
+
+class Dwarf(Player, ABC):
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self.__favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        return print(f"{self.nickname} is eating {self.__favourite_dish}")
---
+++ app/players/elves/elf.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from app.players.player import Player
+
+
+class Elf(Player, ABC):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self.__musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} "
+              f"is playing a song on the "
+              f"{self.__musical_instrument}")
