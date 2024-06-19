05e3111e4f9e339cc91c1a64a7813b174601fe4a
Andriy Zhuk <andri195595@gmail.com>
2024-06-19 14:41:50 +0300

    +

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
-def calculate_team_total_rating(teams: list) -> int:
-    result = [sum(team.get_rating() for team in teams)]
-    return result[0]
+from typing import List
 
 
-def elves_concert(elves: list) -> None:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(teams: List[Player]) -> int:
+    return sum(player.get_rating() for player in teams)
+
+
+def elves_concert(elves: List[Player]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list) -> None:
+def feast_of_the_dwarves(dwarfs: List[Player]) -> None:
     for dwarf in dwarfs:
         dwarf.eat_favourite_dish()
