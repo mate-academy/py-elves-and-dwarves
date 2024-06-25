c47068370b6c580c3e1fbf70f094e8274077bb26
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-25 12:59:15 +0300

    elf and dwarf are ABC

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from app.players.dwarves.dwarf import Dwarf
 from app.players.elves.elf import Elf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(players: list) -> int:
+def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
