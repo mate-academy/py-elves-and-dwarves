a677238aa30f38e1a79fdf54b0dc6a275fed9050
Hennadii <bzpg545256@gmail.com>
2024-06-30 09:36:20 +0100

    need help

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from players.elves.elf_ranger import ElfRanger
+from players.elves.druid import Druid
+from players.dwarves.dwarf_warrior import DwarfWarrior
+from players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from typing import List
+from players.player import Player
+
+
+def calculate_team_total_rating(team: List[Player]) -> int:
+    total_rating = 0
+    for player in team:
+        total_rating += player.get_rating()
+    return total_rating
+
+
+def elves_concert(elves: List[Druid]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: List[DwarfWarrior]) -> None:
+    for dish in dwarves:
+        dish.eat_favourite_dish()
