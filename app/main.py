4e3406d375ecd501348f803dd0b8a02680fb9d03
Vladyslav Yurchenko <vladyu89@gmail.com>
2024-07-10 14:50:19 +0200

    Solution for py-elves-and-dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: list) -> int:
+    return sum(teammate.get_rating() for teammate in team
+               if isinstance(teammate, Player))
+
+
+def elves_concert(elves: list) -> None:
+    [elf.play_elf_song() for elf in elves
+     if isinstance(elf, Elf)]
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in dwarves
+     if isinstance(dwarf, Dwarf)]
