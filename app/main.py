49f94919c50f969a4d5934d281e34bc77c2dff41
Yuliia Yedynak <eeddiinnaakk@gmail.com>
2024-07-14 14:53:52 +0000

    fixed?

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(
-        players: list[ElfRanger, Druid, DwarfWarrior, DwarfBlacksmith]
-) -> int:
+def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elfs: list) -> None:
+def elves_concert(elfs: list[Elf]) -> None:
     for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
