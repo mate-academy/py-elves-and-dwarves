d7ce44df1b91d1d149400333e265cf64105cb95a
Anton Bilous <anton.bilosus@gmail.com>
2024-07-16 17:18:04 +0300

    Add __init__ class

---
+++ app/players/dwarves/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "Dwarf",
+    "DwarfWarrior",
+    "DwarfBlacksmith",
+)
+
+from .dwarf import Dwarf
+from .dwarf_warrior import DwarfWarrior
+from .dwarf_blacksmith import DwarfBlacksmith
