5bdb20d2b69bdefbb549bdb269e5ba4115fdb6a9
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-07-17 16:43:37 +0300

    Implemented solution

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -5,10 +5,10 @@ from abc import ABC
 class Elf(Player, ABC):
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
-        self.musical_instrument = musical_instrument
+        self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
         print(
             f"{self.nickname} is playing "
-            f"a song on the {self.musical_instrument}"
+            f"a song on the {self._musical_instrument}"
         )
