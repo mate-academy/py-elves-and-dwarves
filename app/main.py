9e47af69ea5f986377145e824fd0cd550f92f183
Illia Dubina <armahanz.music@gmail.com>
2024-07-22 14:19:51 +0300

    change imports and list annotations

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
-from app.players.dwarves.dwarf_warrior import DwarfWarrior, Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.elves.druid import Elf, Druid
-from app.players.elves.elf_ranger import ElfRanger
+from app.players.player import Player
+from app.players.dwarves.dwarf_warrior import Dwarf
+from app.players.elves.druid import Elf
 
 
-def calculate_team_total_rating(team: list[Dwarf | Elf]) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list[Druid | ElfRanger]) -> None:
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
 def feast_of_the_dwarves(
-        dwarves: list[DwarfWarrior | DwarfBlacksmith]
+        dwarves: list[Dwarf]
 ) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
