ccb55ffcc40189f7670c0f7904dd441ff8f809d5
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-07-25 14:53:47 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
-from typing import List
-
 from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(player_list: List[Player]) -> int:
-    return sum(player.get_rating() for player in player_list)
+def calculate_team_total_rating(list_players: list[Player]) -> int:
+    return sum(player.get_rating() for player in list_players)
 
 
-def elves_concert(elf_list: List[Elf]) -> None:
-    for elf in elf_list:
+def elves_concert(list_elf: list[Elf]) -> None:
+    for elf in list_elf:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf_list: List[Dwarf]) -> None:
-    for dwarf in dwarf_list:
+def feast_of_the_dwarves(list_dwarf: list[Dwarf]) -> None:
+    for dwarf in list_dwarf:
         dwarf.eat_favourite_dish()
--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -1,19 +1,19 @@
-from .dwarf import Dwarf
+from app.players.dwarves.dwarf import Dwarf
 
 
 class DwarfWarrior(Dwarf):
-    def __init__(self,
-                 nickname: str,
-                 favourite_dish: str,
-                 hummer_level: int,
-                 ) -> None:
-        super().__init__(nickname,
-                         favourite_dish)
-        self.hummer_level = hummer_level
+    def __init__(
+            self, nickname: str,
+            favourite_dish: str,
+            hummer_level: int
+    ) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._hummer_level = hummer_level
 
     def player_info(self) -> str:
-        return (f"Dwarf warrior {self.nickname}. {self.nickname} "
-                f"has a hummer of the {self.hummer_level} level")
+        return (f"Dwarf warrior {self.nickname}. "
+                f"{self.nickname} has a hummer of the "
+                f"{self._hummer_level} level")
 
     def get_rating(self) -> int:
-        return self.hummer_level + 4
+        return self._hummer_level + 4
