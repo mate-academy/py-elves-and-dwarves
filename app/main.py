46360e1dad51cc446295a6414389100b2c2825bc
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-07-29 21:55:01 +0200

    implement py-elves-and-dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    return sum(per.get_rating() for per in team)
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarv in dwarves:
+        dwarv.eat_favourite_dish()
