3a32669e0979c4f6822f99a75aa7431ecca295fd
Ihor Utkin <igorutkin2002@gmail.com>
2024-07-31 20:26:32 +0200

    Resolve .2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(person.get_rating() for person in team)
 
 
