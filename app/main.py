c8cc50adbbebcf672ed7baca262f53cb141754be
Denys Piuro <denys.pyuro@gmail.com>
2024-07-31 18:27:56 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
-# from app.players.elves.elf_ranger import ElfRanger
-# from app.players.elves.druid import Druid
-# from app.players.dwarves.dwarf_warrior import DwarfWarrior
-# from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-# Comented because of test flake8 app, or it is possible to add examples
+from typing import List
 
-def calculate_team_total_rating(team: list) -> int:
+from app.players.player import Player
+
+
+def calculate_team_total_rating(team: List[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: List[Player]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: List[Player]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
