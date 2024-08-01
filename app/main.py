ca7a79d05305635cc6ac1c2557783b4d918a04c4
Marharyta Zazulia <zazmargo@gmail.com>
2024-07-31 22:13:34 -0300

    Changed list comprehension to loop

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def calculate_team_total_rating(team_players: list[Player]) -> int:
 
 
 def elves_concert(team_elves: list[Elf]) -> None:
-    [player.play_elf_song() for player in team_elves]
+    for player in team_elves:
+        player.play_elf_song()
 
 
 def feast_of_the_dwarves(team_dwarves: list[Dwarf]) -> None:
-    [player.eat_favourite_dish() for player in team_dwarves]
+    for player in team_dwarves:
+        player.eat_favourite_dish()
