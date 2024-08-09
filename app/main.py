d49ebaf2f9af21cbdcd22f1203248c0df6655327
user <graban2253@gmail.com>
2024-08-10 01:39:12 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def calculate_team_total_rating(players):
+    total_rating = sum(player.get_rating() for player in players)
+    return total_rating
+
+
+def elves_concert(elves) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
