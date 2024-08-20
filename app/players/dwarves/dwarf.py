751a44c284448f28a7c4cfd0d5a742ccbcc9a593
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-20 11:36:45 +0300

    add dwarf class

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,9 @@
+from app.players.player import Player
+
+
+class Dwarf(Player):
+    def __init__(self, favourite_dish) -> None:
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
