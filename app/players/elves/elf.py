26d6ae3f53a65a3f1ba2c7d9aa04fb6f8dbe18bc
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-20 11:34:44 +0300

    add elf class

---
+++ app/players/elves/elf.py
@@ -0,0 +1,9 @@
+from app.players.player import Player
+
+
+class Elf(Player):
+    def __init__(self, musical_instrument) -> None:
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
