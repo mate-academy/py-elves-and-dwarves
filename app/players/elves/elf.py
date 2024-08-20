64f6acddce5fd8ecb39e5b1779bc968d72b977f1
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-20 14:10:26 +0300

    code refactor according to flake8

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -3,9 +3,10 @@ from abc import ABC
 
 
 class Elf(Player, ABC):
-    def __init__(self, nickname, musical_instrument: str) -> None:
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+        print(f"{self.nickname} is playing a song on the "
+              f"{self._musical_instrument}")
