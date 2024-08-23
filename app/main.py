7a9a0635b8e4da51c8123447384504540d327096
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-23 09:31:38 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+from app.players.dwarves.dwarf import Dwarf
 from app.players.player import Player
 from app.players.elves.elf import Elf
-from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players_list: list[Player]) -> int:
