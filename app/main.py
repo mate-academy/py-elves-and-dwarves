57b5f236d8dd2ebc0e5bd29d08baa96350d21dd9
Kostiantin Pastukh <kostyapastukh@gmail.com>
2024-09-02 09:38:22 +0300

    Fix list_comprehansion

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-
 from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(players_list: list[Player]) -> int:
-    return sum([player.get_rating() for player in players_list])
+    return sum(player.get_rating() for player in players_list)
 
 
 def elves_concert(elfs_list: list[Elf]) -> None:
