8a36c701c4d500c2db9de64889016afc16ee37c7
Nikita Ponomarov <wertyciti29@gmail.com>
2024-09-08 18:46:11 +0300

    Unnecessary code removed

--- app/main.py
+++ app/main.py
@@ -1,25 +1,21 @@
 from app.players.player import Player
-from app.players.elves.druid import Druid
-from app.players.elves.elf_ranger import ElfRanger
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    return sum([player.get_rating() for player in players])
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(
-        elves: list[Elf | ElfRanger | Druid]
+        elves: list[Elf]
 ) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
 def feast_of_the_dwarves(
-        dwarves: list[Dwarf | DwarfWarrior | DwarfBlacksmith]
+        dwarves: list[Dwarf]
 ) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
