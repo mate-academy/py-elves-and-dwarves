811442944078af8ae231e2915f12573b7b70941b
Bohdan Poliienko <mignatov756@gmail.com>
2024-10-05 08:24:47 +0300

    fix flake8

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -17,4 +17,5 @@ class Elf(Player, ABC):
         )
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+        print(f"{self.nickname} is playing a "
+              f"song on the {self._musical_instrument}")
