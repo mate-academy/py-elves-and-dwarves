de97cd339f729a9536e3b3d51dab8bfeb9591383
roman <skyfoxwork@gmail.com>
2024-10-06 14:41:55 +0300

    some_refactoring_1

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -9,9 +9,3 @@ class Dwarf(Player):
 
     def eat_favourite_dish(self) -> None:
         print(f"{self.nickname} is eating {self._favourite_dish}")
-
-    def player_info(self):
-        pass
-
-    def get_rating(self):
-        pass
