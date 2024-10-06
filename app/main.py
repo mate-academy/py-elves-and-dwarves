26f0a1608a8b39cf4bab5deb4c75c62b4891b7d6
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-10-06 19:01:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(team_lst: list[object]) -> int:
+    return sum(character.get_rating() for character in team_lst)
+
+
+def elves_concert(elf_list: list[object]) -> None:
+    for elf in elf_list:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarf_lst: list[object]) -> None:
+    for dwarf in dwarf_lst:
+        dwarf.eat_favourite_dish()
