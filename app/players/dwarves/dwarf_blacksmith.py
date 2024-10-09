74bc10b761a505910a2c4826a51ec7949e496810
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-09 09:16:01 +0300

    type annotations

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -10,9 +10,9 @@ class DwarfBlacksmith(Dwarf):
                          favourite_dish=favourite_dish)
         self._skill_level = skill_level
 
-    def get_rating(self):
+    def get_rating(self) -> int:
         return self._skill_level
 
-    def player_info(self):
+    def player_info(self) -> str:
         return (f"Dwarf blacksmith {self.nickname} with skill of the "
                 f"{self._skill_level} level")
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -10,10 +10,10 @@ class ElfRanger(Elf):
                          musical_instrument=musical_instrument)
         self._bow_level = bow_level
 
-    def get_rating(self):
+    def get_rating(self) -> int:
         return 3 * self._bow_level
 
-    def player_info(self):
+    def player_info(self) -> str:
         return (f"Elf ranger {self.nickname}. "
                 f"{self.nickname} has bow of the "
                 f"{self._bow_level} level")
