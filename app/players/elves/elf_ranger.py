cd0a9ffa9c72639b4109b23e2308b15e898a5bff
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2024-10-10 10:49:39 +0300

    not to use \, inherit from ABC

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -16,6 +16,6 @@ class ElfRanger(Elf):
         return 3 * self._bow_level
 
     def player_info(self) -> str:
-        return f"Elf ranger {self.nickname}. " \
-               f"{self.nickname} has bow of " \
-               f"the {self._bow_level} level"
+        return (f"Elf ranger {self.nickname}. "
+                f"{self.nickname} has bow of "
+                f"the {self._bow_level} level")
