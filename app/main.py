2c71d792d262f761a3e03a9fdb54cadb46ecffae
Maxim Khrypunov <khrypunovmax@gmail.com>
2024-10-24 16:20:05 +0300

    solution_3

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ def calculate_team_total_rating(players: List[Player]) -> int:
     return sum_rating
 
 
-def elves_concert(elves: list[Elf]) -> None:
+def elves_concert(elves: List[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+def feast_of_the_dwarves(dwarves: List[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
