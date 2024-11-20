10bd1f04cce13d7ca83f67b88557747f45250760
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-11-20 10:36:22 +0100

    fixed 2

--- app/main.py
+++ app/main.py
@@ -6,16 +6,16 @@ from app.players.elves.elf_ranger import ElfRanger
 from app.players.player import Player
 
 
-def calculate_team_total_rating(team: list[Player]):
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: list[Elf]):
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves):
+def feast_of_the_dwarves(dwarves: list[DwarfWarrior]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
 
