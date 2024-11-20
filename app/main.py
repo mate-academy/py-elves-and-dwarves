073e142f2e68ffe95ccec89bba151aeb9778521c
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-11-20 10:30:36 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.elves.druid import Druid
+from app.players.elves.elf import Elf
 from app.players.elves.elf_ranger import ElfRanger
+from app.players.player import Player
 
 
-def calculate_team_total_rating(team):
+def calculate_team_total_rating(team: list[Player]):
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves):
+def elves_concert(elves: list[Elf]):
     for elf in elves:
         elf.play_elf_song()
 
@@ -20,10 +22,22 @@ def feast_of_the_dwarves(dwarves):
 
 if __name__ == "__main__":
     team = [
-        Druid(nickname="Nardual", musical_instrument="флейта", favourite_spell="aaa"),
-        ElfRanger(nickname="Rothilion", musical_instrument="труба", bow_level=33),
-        DwarfWarrior(nickname="Thiddeal", favourite_dish="Картопля фрі", hummer_level=3),
-        DwarfBlacksmith(nickname="Balin", favourite_dish="Цезар Салат", skill_level=5),
+        Druid(nickname="Nardual",
+              musical_instrument="флейта",
+              favourite_spell="aaa"
+              ),
+        ElfRanger(nickname="Rothilion",
+                  musical_instrument="труба",
+                  bow_level=33
+                  ),
+        DwarfWarrior(nickname="Thiddeal",
+                     favourite_dish="Картопля фрі",
+                     hummer_level=3
+                     ),
+        DwarfBlacksmith(nickname="Balin",
+                        favourite_dish="Цезар Салат",
+                        skill_level=5
+                        ),
     ]
 
     print(f"Total team rating: {calculate_team_total_rating(team)}")
