377fa6f90ea0b4d823c2e89d090b24965b799d64
Olha Shumska <olgashumska65@gmail.com>
2024-11-27 16:12:51 +0100

    new commit

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    total_rating = sum(player.get_rating() for player in players)
-    return total_rating
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list[Elf]) -> None:
