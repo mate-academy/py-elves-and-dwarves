28f7a7a83ca667ebdc2bcfa80196926117bf594d
shafran <shafran.andriy@gmail.com>
2024-11-30 14:01:44 +0200

    Created all packeges, modules, classes and methods

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    sum_raiting = 0
+    for player in players:
+        sum_raiting += player.get_rating()
+    return sum_raiting
+
+
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
+    for dwarf in dwarfs:
+        dwarf.eat_favourite_dish()
---
+++ app/players/elves/elf.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from app.players.player import Player
+
+
+class Elf(Player, ABC):
+    def __init__(self, musical_instrument: str, nickname: str) -> None:
+        super().__init__(nickname)
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
