ac895bf581717b5cd1ca40dc8a68c2c236ab6153
Olexander Lypovenko <fantomasdeft@gmail.com>
2024-12-06 18:09:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(list_player: list) -> int:
+    return sum(player.get_rating() for player in list_player)
+
+
+def elves_concert(list_elf: list) -> None:
+    for elf in list_elf:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(list_dwarf: list) -> None:
+    for dwarf in list_dwarf:
+        dwarf.eat_favourite_dish()
