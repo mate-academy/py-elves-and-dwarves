9022e91b52e3ec12f5064521c711ab2546368230
Iryna Mishchenko <djrobentab@gmail.com>
2024-12-06 13:59:11 +0100

    Fixed flake8

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -1,5 +1,6 @@
 from app.players.elves.elf import Elf
 
+
 class Druid(Elf):
     def __init__(
             self,
@@ -11,7 +12,8 @@ class Druid(Elf):
         self._favorite_spell = favourite_spell
 
     def player_info(self) -> str:
-        return f"Druid {self.nickname}. {self.nickname} has a favourite spell: {self._favorite_spell}"
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a favourite spell: {self._favorite_spell}")
 
     def get_rating(self) -> int:
         return len(self._favorite_spell)
