25f5b6268102b99f5d060884f3b6543ff24e48c9
Tymofii Khlistik <tima0323199@gmail.com>
2024-12-07 13:43:48 +0200

    in progress

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+
+
+class Elf(Player):
+    def __init__(self, musical_instrument:str) -> None:
+        self.musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self.musical_instrument}")
+
+
+class Dwarf(Player):
+    def __init__(self, favourite_dish: str) -> None:
+        self.favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self.favourite_dish}")
