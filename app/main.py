6adae671d2afe2ea6ec8efb8694f67b1d4a9e8d7
Tymofii Khlistik <tima0323199@gmail.com>
2024-12-07 19:28:12 +0200

    New Solution1

--- app/main.py
+++ app/main.py
@@ -4,11 +4,6 @@ from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
 
 # ----------------------------------------------------------------
 def calculate_team_total_rating(players: List[Player]) -> int:
