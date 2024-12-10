3b0e9a1c179211232ef6bd4a0d5c4cba0db54fe2
Nazar Slavych <slavuch232355@gmail.com>
2024-12-10 22:32:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(teams: list) -> int:
+    rating = 0
+    for player in teams:
+        rating += player.get_rating()
+    return rating
+
+
+def elves_concert(teams: list) -> None:
+    for player in teams:
+        player.play_elf_song()
+
+
+def feast_of_the_dwarves(teams: list) -> None:
+    for player in teams:
+        player.eat_favourite_dish()
