c74aaf0c7aee038e4474891c84bf9b8cf7969aa8
Maksym <maximuschampion2002@gmail.com>
2024-12-10 20:44:56 +0200

    elves-and-dwarves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,97 @@
-# write your code here
+from abc import ABC, abstractmethod
+
+
+class Player(ABC):
+    def __init__(self, nickname: str) -> None:
+        self.nickname: str = nickname
+
+    @abstractmethod
+    def get_rating(self) -> int:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> str:
+        pass
+
+
+class Elf(Player, ABC):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self._musical_instrument: str = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song "
+              f"on the {self._musical_instrument}")
+
+
+class Dwarf(Player, ABC):
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish: str = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating "
+              f"{self._favourite_dish}")
+
+
+class ElfRanger(Elf):
+    def __init__(self, nickname: str,
+                 musical_instrument:
+                 str, bow_level: int) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level: int = bow_level
+
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
+
+    def player_info(self) -> str:
+        return (f"Elf ranger {self.nickname}. "
+                f"{self.nickname} has bow of the "
+                f"{self._bow_level} level")
+
+
+class Druid(Elf):
+    def __init__(self, nickname: str,
+                 musical_instrument: str,
+                 favourite_spell: str) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._favourite_spell: str = favourite_spell
+
+    def get_rating(self) -> int:
+        return len(self._favourite_spell)
+
+    def player_info(self) -> str:
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a favourite spell: "
+                f"{self._favourite_spell}")
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(self, nickname: str,
+                 favourite_dish: str,
+                 hummer_level: int) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._hummer_level: int = hummer_level
+
+    def get_rating(self) -> int:
+        return self._hummer_level + 4
+
+    def player_info(self) -> str:
+        return (f"Dwarf warrior {self.nickname}. "
+                f"{self.nickname} has a hummer of the "
+                f"{self._hummer_level} level")
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, nickname: str,
+                 favourite_dish: str,
+                 skill_level: int) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._skill_level: int = skill_level
+
+    def get_rating(self) -> int:
+        return self._skill_level
+
+    def player_info(self) -> str:
+        return (f"Dwarf blacksmith {self.nickname} with "
+                f"skill of the {self._skill_level} level")
