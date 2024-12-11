e86bcd69f0147e26b8af3de87332611928d3c93b
Admin <mr.artem450@gmail.com>
2024-12-11 21:55:23 +0200

    Task

--- app/main.py
+++ app/main.py
@@ -1,40 +1,17 @@
-from players.elves import ElfRanger, Druid
-from players.dwarves import DwarfWarrior, DwarfBlacksmith
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(players):
-    """Calculates the sum of ratings for all players in the team."""
-    total_rating = 0
-    for player in players:
-        total_rating += player.get_rating()
-    return total_rating
+def calculate_team_total_rating(team_list: list[Player]) -> int:
+    return sum([player.get_rating() for player in team_list])
 
 
-def elves_concert(elves):
-    """Calls play_elf_song for each Elf in the list."""
-    for elf in elves:
+def elves_concert(elves_list: list[Elf]) -> None:
+    for elf in elves_list:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves):
-    """Calls eat_favourite_dish for each Dwarf in the list."""
-    for dwarf in dwarves:
+def feast_of_the_dwarves(dwarfs_list: list[Dwarf]) -> None:
+    for dwarf in dwarfs_list:
         dwarf.eat_favourite_dish()
-
-
-# Example usage
-ranger = ElfRanger(
-    nickname="Nardual Chaekian", musical_instrument="flute", bow_level=7
-)
-warrior = DwarfWarrior(nickname="Thiddeal", favourite_dish="French Fries", hummer_level=7)
-
-team = [Druid(nickname="Druid", musical_instrument="flute", favourite_spell="ABC"), ranger]
-print(calculate_team_total_rating(team))  # Output: 102
-
-elves = [
-    Druid(nickname="Nardual", musical_instrument="flute", favourite_spell="aaa"), ranger
-]
-elves_concert(elves)
-
-dwarves = [warrior, DwarfBlacksmith(nickname="Dwarf", favourite_dish="Salad", skill_level=5)]
-feast_of_the_dwarves(dwarves)
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -1,13 +1,21 @@
-from players.elves.elf import Elf
+from app.players.elves.elf import Elf
 
 
 class ElfRanger(Elf):
-    def __init__(self, nickname, musical_instrument, bow_level):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            bow_level: int
+    ) -> None:
         super().__init__(nickname, musical_instrument)
-        self.__bow_level = bow_level
+        self._bow_level = bow_level
 
-    def get_rating(self):
-        return self.__bow_level * 3
+    def player_info(self) -> str:
+        return (
+            f"Elf ranger {self.nickname}. {self.nickname} "
+            f"has bow of the {self._bow_level} level"
+        )
 
-    def player_info(self):
-        return f"Elf ranger {self.nickname}. {self.nickname} has bow of the {self.__bow_level} level"
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
