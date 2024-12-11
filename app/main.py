b0f0b4d5568b0113b09063265e27b995f84d84ce
Aleksey Pastukhov <mindflux99@gmail.com>
2024-12-11 16:47:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from collections.abc import Iterable
+
+
+def calculate_team_total_rating(team: Iterable) -> int:
+    return sum(player.get_rating() for player in team)
+
+
+def elves_concert(elves: Iterable) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: Iterable) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
