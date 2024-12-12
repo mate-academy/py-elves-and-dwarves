b96b1d9d8e80e04149c0ed6399fe9b216ff5681a
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-12 15:12:50 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def calculate_team_total_rating(player: list) -> int:
+    return sum([player.get_rating() for player in player])
+
+
+def elves_concert(elf: list) -> None:
+    [el.play_elf_song() for el in elf]
+
+
+def feast_of_the_dwarves(dwarf: list) -> None:
+    [dwa.eat_favourite_dish() for dwa in dwarf]
