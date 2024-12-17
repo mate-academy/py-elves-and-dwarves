40b933f1d8c2458dae4e46ac8934486914b7cc70
D Petetskyi <4477617@gmail.com>
2024-12-17 19:44:14 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from abc import ABC, abstractmethod
+
+
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
+
+def elves_concert(list_of_elf: list[Elf]) -> None:
+    [elf.play_elf_song() for elf in list_of_elf]
+
+def feast_of_the_dwarves(list_of_dwarves: list[Dwarf]) -> None:
+    [dwarf.eat_favourite_dish() for dwarf in list_of_dwarves]
