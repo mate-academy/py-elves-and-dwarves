6c56ac52e47e8c784c2bc55716033af5cad27f77
bc2024-4 <bzabrodskij20@gmail.com>
2025-01-14 08:57:17 +0200

    add program

--- app/main.py
+++ app/main.py
@@ -1,1 +1,43 @@
-# write your code here
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+
+
+def calculate_team_total_rating(team: list) -> float:
+    """
+    Обчислює загальний рейтинг команди.
+    """
+    return sum(player.get_rating() for player in team)
+
+
+def elves_concert(elves: list) -> None:
+    """
+    Організовує концерт для ельфів.
+    """
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
+
+
+if __name__ == "__main__":
+    team = [
+        Druid(nickname="Druid", musical_instrument="flute", favourite_spell="ABC"),
+        ElfRanger(nickname="Ranger", musical_instrument="trumpet", bow_level=33),
+    ]
+    print(calculate_team_total_rating(team))  # Очікуваний результат: 102
+
+    elves = [
+        Druid(nickname="Nardual", musical_instrument="flute", favourite_spell="aaa"),
+        ElfRanger(nickname="Rothilion", musical_instrument="trumpet", bow_level=33),
+    ]
+    elves_concert(elves)
+
+    dwarves = [
+        DwarfWarrior(nickname="Thiddeal", favourite_dish="French Fries", hummer_level=3),
+        DwarfWarrior(nickname="Dwarf", favourite_dish="Caesar Salad", hummer_level=3),
+    ]
+    feast_of_the_dwarves(dwarves)
