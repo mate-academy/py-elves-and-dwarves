b53e8bfb98f9f142040997a47470fe85e55999a5
Svitlana <svetapruta@gmail.com>
2025-01-15 11:01:59 +0100

    add project structure

---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,14 @@
+from abc import ABC
+from app.players.elves.elf import Elf
+
+
+class ElfRanger(Elf, ABC):
+    def __init__(self, nickname: str, musical_instrument: str, bow_level: int) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def player_info(self) -> None:
+        print(f"Elf ranger {self.nickname}. {self.nickname} has bow of the {self._bow_level} level")
+
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
