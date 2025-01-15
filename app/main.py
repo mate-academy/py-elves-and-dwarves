bd212f8b42ee84ba328d09ac8854b079d528f29e
Svitlana <svetapruta@gmail.com>
2025-01-15 09:49:59 +0100

    add class Elf and Dwarf

--- app/main.py
+++ app/main.py
@@ -14,8 +14,18 @@ class Player(ABC):
         pass
 
 
-class Elf(Player):
+class Elf(Player, ABC):
+    def __init__(self, musical_instrument: str) -> None:
+        super().__init__()
+        self._musical_instrument = musical_instrument
 
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
 
-class Dwarf(Player):
+class Dwarf(Player, ABC):
+    def __init__(self, favourite_dish: str) -> None:
+        super().__init__()
+        self._favourite_dish = favourite_dish
 
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
