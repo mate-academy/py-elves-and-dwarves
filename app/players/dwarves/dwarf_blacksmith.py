b757d91f9cbea664ab62fea55d1d2d0654d7269a
Volodymir Shlikhta <shlikhta77@gmail.com>
2025-01-19 09:06:00 +0200

    Implement Player, Elf, Dwarf Classes and Utility Functions

---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,12 @@
+from app.players.dwarves.dwarf import Dwarf
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, nickname: str, favourite_dish: str, skill_level: int):
+        super().__init__(nickname, favourite_dish)
+        self._skill_level = skill_level
+
+    def get_rating(self):
+        return self._skill_level
+
+    def player_info(self):
+        return f"Dwarf blacksmith {self.nickname} with skill of the {self._skill_level} level"
