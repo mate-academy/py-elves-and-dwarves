29362d742f53dfbae2bf0e4bae7dc59933455190
Vik <viktoria.om@gmail.com>
2025-02-03 16:05:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    total_rating = 0
+    for player in team:
+        total_rating += player.get_rating()
+    return total_rating
+
+
+def elves_concert(elves: list) -> None:
+    for one in elves:
+        one.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for one in dwarves:
+        one.eat_favourite_dish()
