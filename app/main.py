57c0184c5e9b15cba656dfd212f34eea2cd5ef4d
Viktor Stadnik <stadhik.victor@gmail.com>
2025-02-13 19:26:27 +0200

    implemented "py-elves-and-dwarves"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(lists: list) -> int:
+    return sum(player.get_rating() for player in lists)
+
+
+def elves_concert(lists: list) -> None:
+    for player in lists:
+        player.play_elf_song()
+
+
+def feast_of_the_dwarves(lists: list) -> None:
+    for player in lists:
+        player.eat_favourite_dish()
