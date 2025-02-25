290e5d19feb63957297a44412cad7b6ac0a0bfc8
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-02-26 00:03:52 +0100

    'elves_and_dwarves_v1'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+
+
+def calculate_team_total_rating(gamers: list) -> int:
+    rating = 0
+    for gamer in gamers:
+        rating += gamer.get_rating()
+    return rating
+
+
+def elves_concert(singers: list) -> None:
+    for singer in singers:
+        singer.play_elf_song()
+
+
+def feast_of_the_dwarves(eaters: list) -> None:
+    for eater in eaters:
+        eater.eat_favourite_dish()
