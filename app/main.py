7dae5cfb40d0b275ba39811aab8b969bde886ee2
Anton Vynychenko <kinid@ukr.net>
2025-02-28 19:32:40 +0100

    Ask

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def calculate_team_total_rating(team: list) -> None:
+    total = 0
+    for player in team:
+        one_player = player.get_rating()
+        total += one_player
+    return total
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
