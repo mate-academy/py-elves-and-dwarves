328f90154f5857f66a3e77263f4430d86a79aa77
Iryna <irakirvas78@gmail.com>
2025-03-05 21:36:54 +0300

    Implement py-elves-and-dwares

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(ls: list) -> int:
+    return sum(play.get_rating() for play in ls)
+
+
+def elves_concert(ls: list) -> None:
+    for play in ls:
+        play.play_elf_song()
+
+
+def feast_of_the_dwarves(ls: list) -> None:
+    for play in ls:
+        play.eat_favourite_dish()
