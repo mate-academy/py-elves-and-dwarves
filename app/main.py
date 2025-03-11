dc4b70e37ffeefb1229d9ada12d6797df8e5f360
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-03-11 20:34:29 +0100

    feat: create meta universe

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from typing import List
+
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: List[Player]) -> int:
+    return sum(player.get_rating() for player in team)
+
+
+def elves_concert(team: List[Elf]) -> None:
+    for player in team:
+        player.play_elf_song()
+
+
+def feast_of_the_dwarves(team: List[Dwarf]) -> None:
+    for player in team:
+        player.eat_favourite_dish()
---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,22 @@
+from .elf import Elf
+
+
+class ElfRanger(Elf):
+    def __init__(
+        self,
+        nickname: str,
+        bow_level: int,
+        musical_instrument: str,
+    ) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def player_info(self) -> str:
+        return (
+            f"Elf ranger {self.nickname}. "
+            f"{self.nickname} has bow of the {self._bow_level} level"
+        )
+
+    def get_rating(self) -> int:
+        multiply_to = 3
+        return self._bow_level * multiply_to
