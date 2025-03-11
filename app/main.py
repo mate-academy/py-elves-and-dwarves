f0b81f51502d9035387f15eb075f09accc134b0a
Max <smolinskijmaksim1@gmail.com>
2025-03-11 18:22:51 +0100

    Fix error4

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(people_list: list) -> int:
+    result = 0
+    for person in people_list:
+        result += person.get_rating()
+    return result
+
+
+def elves_concert(elf_list: list) -> None:
+    for elf in elf_list:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves_list: list) -> None:
+    for dwarf in dwarves_list:
+        dwarf.eat_favourite_dish()
