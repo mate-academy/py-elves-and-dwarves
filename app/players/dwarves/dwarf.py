54c2c6016607660cad8eaa41dfd9301bcb616e0f
Max <smolinskijmaksim1@gmail.com>
2025-03-11 18:46:27 +0100

    Solution

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -7,7 +7,7 @@ class Dwarf(Player, ABC):
 
     def __init__(self, nick_name: str, favourite_dish: str) -> None:
         super().__init__(nick_name)
-        self.favourite_dish = favourite_dish
+        self._favourite_dish = favourite_dish
 
     def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} is eating {self.favourite_dish}")
+        print(f"{self.nickname} is eating {self._favourite_dish}")
