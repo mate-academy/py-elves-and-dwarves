bc1dab0f50d9cddf2cea377b70133c4830cc55b1
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-13 20:56:13 +0200

    innitial commit

---
+++ app/players/__init__.py
@@ -0,0 +1,4 @@
+# This file is used to import all the player classes in the package
+from .player import Player  # noqa: F401
+from .elves.elf import Elf  # noqa: F401
+from .dwarves.dwarf import Dwarf  # noqa: F401
