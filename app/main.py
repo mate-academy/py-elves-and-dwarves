f3f8f647b4dfdb01e00e564015ead7c7861e5718
Danya Teruk <danyakris123@gmail.com>
2025-03-16 17:00:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: list) -> (int, float):
+    total_rating = sum(player.get_rating() for player in team)
+    return total_rating
+
+
+def elves_concert(elves: list[(Elf,
+                               ElfRanger,
+                               Druid)]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[(Dwarf,
+                                        DwarfWarrior,
+                                        DwarfBlacksmith)]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
