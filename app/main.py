cab3832b9759c2fa941270723b00adcd5c1c406d
Danya Teruk <danyakris123@gmail.com>
2025-03-17 00:28:12 +0100

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
