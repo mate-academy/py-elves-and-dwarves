8e50401f3dddbd491c2aabcf6e2a3eb487f4d10b
Alexandr <oleksandrglushen2005@gmail.com>
2025-03-19 21:33:05 +0200

    fixed code 2

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list) -> int:
-    return sum(players.get_rating() for player in players if isinstance(player, Player))
+def calculate_team_total_rating(player: list) -> int:
+    return sum(player.get_rating() for player in player if isinstance(player, Player))
 
 def elves_concert(elf_list: list) -> None:
     for elf in elf_list:
