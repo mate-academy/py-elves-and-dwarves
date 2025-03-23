cb57d4cc3481bbd31c0e85e1d049220d620fb2b0
Windings-Lab <janton20001@gmail.com>
2025-03-23 19:16:47 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ def calculate_team_total_rating(team: list[Player]) -> int:
 
 
 def elves_concert(team: list[Elf]) -> None:
-    [elf.play_elf_song() for elf in team]
+    (elf.play_elf_song() for elf in team)
 
 
 def feast_of_the_dwarves(team: list[Dwarf]) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in team]
+    (dwarf.eat_favourite_dish() for dwarf in team)
