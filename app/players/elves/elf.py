7a44a96bd0938566f388f9772661665206f6968a
Vadym <strauuch@gmail.com>
2025-03-26 13:49:42 +0100

    fix flake8

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -2,10 +2,10 @@ from app.players.player import Player
 
 
 class Elf(Player):
-    def __init__(self, nickname: str, musical_instrument: str):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
-    def play_elf_song(self):
+    def play_elf_song(self) -> str:
         print(f"{self.nickname} is playing a song on the"
               f" {self._musical_instrument}")
--- app/players/player.py
+++ app/players/player.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class Player(ABC):
-    def __init__(self, nickname: str):
+    def __init__(self, nickname: str) -> None:
         self.nickname = nickname
 
     @abstractmethod
-    def get_rating(self):
+    def get_rating(self) -> None:
         pass
 
     @abstractmethod
-    def player_info(self):
+    def player_info(self) -> None:
         pass
