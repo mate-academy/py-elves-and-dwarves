911826aaeaccbab508d2d6aede28ad77315e4168
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-26 21:48:59 +0200

    updated annotations, improved using sum method

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.players import Player, Elf, Dwarf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    return sum([player.get_rating() for player in players])
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elfs: list[Elf]) -> None:
