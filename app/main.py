00cf1cc534618add39f232ec8d74b20c6cb21972
Leonov <leonovaviktoria291@gmail.com>
2025-03-27 03:00:36 +0200

    Solutio1n

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-
-
 def calculate_team_total_rating(team: list) -> int:
-    return sum(player.get_rating() for player in team)
+    total_rating = 0
+    for player in team:
+        total_rating += player.get_rating()
+    return total_rating
 
 
 def elves_concert(elves: list) -> None:
@@ -15,38 +13,3 @@ def elves_concert(elves: list) -> None:
 def feast_of_the_dwarves(dwarves: list) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
-
-
-ranger = ElfRanger(
-    nickname="Nardual Chaekian", musical_instrument="flute", bow_level=7
-)
-print(ranger.get_rating())
-print(ranger.player_info())
-ranger.play_elf_song()
-
-warrior = DwarfWarrior(
-    nickname="Thiddeal", favourite_dish="French Fries", hummer_level=7
-)
-print(warrior.get_rating())
-print(warrior.player_info())
-warrior.eat_favourite_dish()
-
-team = [
-    Druid(
-        nickname="Druid", musical_instrument="flute", favourite_spell="ABC"
-    ),
-    ElfRanger(
-        nickname="Ranger", musical_instrument="trumpet", bow_level=33
-    ),
-]
-print(calculate_team_total_rating(team))
-
-elves = [
-    Druid(
-        nickname="Nardual", musical_instrument="flute", favourite_spell="aaa"
-    ),
-    ElfRanger(
-        nickname="Rothilion", musical_instrument="trumpet", bow_level=33
-    ),
-]
-elves_concert(elves)
