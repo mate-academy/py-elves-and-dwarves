39bb35afa9717dcb4074f05feecf0847caee2445
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-03-31 16:09:20 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,15 @@
-from abc import ABC, abstractmethod
 from typing import List
-
-class Player(ABC):
-    def __init__(self, nickname: str):
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self):
-        pass
-
-    @abstractmethod
-    def player_info(self):
-        pass
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 def calculate_team_total_rating(players: List[Player]) -> int:
     return sum(player.get_rating() for player in players)
 
-def elves_concert(elves: List['Elf']):
+def elves_concert(elves: List[Elf]):
     for elf in elves:
         elf.play_elf_song()
 
-def feast_of_the_dwarves(dwarves: List['Dwarf']):
+def feast_of_the_dwarves(dwarves: List[Dwarf]):
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
