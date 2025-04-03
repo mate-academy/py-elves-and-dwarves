9efde488d0fe1776a75dd9ff4bfcc201808af3dd
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 15:40:32 +0300

    elf.py

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,10 +1,13 @@
 from abc import ABC
+
 from app.players.player import Player
 
+
 class Elf(Player, ABC):
-    def __init__(self, nickname: str, musical_instrument: str):
-        super().__init__(nickname)
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        self.nickname += nickname
         self._musical_instrument = musical_instrument
 
-    def play_elf_song(self):
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing "
+              f"a song on the {self._musical_instrument}")
