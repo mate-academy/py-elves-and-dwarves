c5f32b4508430f144fafb32e42cd73747b849a72
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-09 22:07:08 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from oop.players.player import Player
-from oop.players.elves.elf import Elf
-from oop.players.elves.elf_ranger import ElfRanger
-from oop.players.elves.druid import Druid
-from oop.players.dwarves.dwarf import Dwarf
-from oop.players.dwarves.dwarf_warrior import DwarfWarrior
-from oop.players.dwarves.dwarf_blacksmith import DwarfBlackSmith
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlackSmith
 
 
 def calculate_team_total_rating(team_list: list[Player]):
