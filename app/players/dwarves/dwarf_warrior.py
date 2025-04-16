067514e4bd466ff3f3cce688a9ad2b7930189d0f
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2025-04-16 15:33:48 +0300

    'Solution'

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -5,12 +5,12 @@ class DwarfWarrior(Dwarf):
     def __init__(self, nickname: str,
                  favourite_dish: str, hummer_level: int) -> None:
         super().__init__(nickname, favourite_dish)
-        self.__hummer_level = hummer_level
+        self._hummer_level = hummer_level
 
     def player_info(self) -> str:
         return (f"Dwarf warrior {self.nickname}. "
                 f"{self.nickname} has a hummer of the "
-                f"{self.__hummer_level} level")
+                f"{self._hummer_level} level")
 
     def get_rating(self) -> int:
-        return self.__hummer_level + 4
+        return self._hummer_level + 4
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -6,12 +6,12 @@ class ElfRanger(Elf):
                  musical_instrument: str,
                  bow_level: int) -> None:
         super().__init__(nickname, musical_instrument)
-        self.__bow_level = bow_level
+        self._bow_level = bow_level
 
     def player_info(self) -> str:
         return (f"Elf ranger {self.nickname}. "
                 f"{self.nickname} has bow of the "
-                f"{self.__bow_level} level")
+                f"{self._bow_level} level")
 
     def get_rating(self) -> int:
-        return 3 * self.__bow_level
+        return 3 * self._bow_level
