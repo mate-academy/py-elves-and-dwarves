deefbc8bcab85bbba282fb9b0e47d246b55f3047
Elisiane Quadros <elisiane.quadros@hotmail.com>
2025-04-23 21:16:37 -0300

    fix: use and print returned strings from play_elf_song and eat_favourite_dish

--- app/main.py
+++ app/main.py
@@ -9,9 +9,11 @@ def calculate_team_total_rating(team: list[Player]) -> int:
 
 def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
-        elf.play_elf_song()
+        song = elf.play_elf_song()
+        print(song)
 
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
-        dwarf.eat_favourite_dish()
+        dish = dwarf.eat_favourite_dish()
+        print(dish)
