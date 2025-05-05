b608e5334f4a2daf80040291a903b45ef87c95b6
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-05 23:43:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,36 +1,12 @@
-from players.dwarves.dwarf_warrior import DwarfWarrior
-from players.elves.druid import Druid
-from players.elves.elf_ranger import ElfRanger
+def calculate_team_total_rating(list_of_players: list) -> int:
+    return sum((player.get_rating() for player in list_of_players))
 
 
-def calculate_team_total_rating() -> int:
-    team = [
-        Druid(nickname="Druid",
-              musical_instrument="flute", favourite_spell="ABC"),
-        ElfRanger(nickname="Ranger",
-                  musical_instrument="trumpet", bow_level=33),
-    ]
-
-    return sum(player.get_rating() for player in team)
-
-
-def elves_concert() -> None:
-    elves = [
-        Druid(nickname="Nardual",
-              musical_instrument="flute", favourite_spell="aaa"),
-        ElfRanger(nickname="Rothilion",
-                  musical_instrument="trumpet", bow_level=33),
-    ]
-    for elf in elves:
+def elves_concert(elfs: list) -> None:
+    for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves() -> None:
-    dwarves = [
-        DwarfWarrior(nickname="Thiddeal",
-                     favourite_dish="French Fries", hummer_level=3),
-        DwarfWarrior(nickname="Dwarf",
-                     favourite_dish="Caesar Salad", hummer_level=3),
-    ]
-    for dwarf in dwarves:
+def feast_of_the_dwarves(dwarfs: list) -> None:
+    for dwarf in dwarfs:
         dwarf.eat_favourite_dish()
