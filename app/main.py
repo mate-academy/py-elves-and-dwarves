1311f3cde2306a254adcf7c733c3fb3b85779359
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-05 18:38:54 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def calculate_team_total_rating(team: list) -> int:
     return sum(team_ratings)
 
 
-def elves_concert(elves: list) -> str:
+def elves_concert(elves: list) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> str:
+def feast_of_the_dwarves(dwarves: list) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
