022ce8406137e5facde6ec96369e27fa481ed42c
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-05 23:28:52 +0200

    Solution

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,4 +1,4 @@
-from .players.player import Player
+from players.player import Player
 
 
 class Elves(Player):
@@ -9,3 +9,9 @@ class Elves(Player):
     def play_elf_song(self) -> None:
         print(f"{self.nickname} "
               f"is playing a song on the {self._musical_instrument}")
+
+    def get_rating(self) -> None:
+        pass
+
+    def player_info(self) -> None:
+        pass
