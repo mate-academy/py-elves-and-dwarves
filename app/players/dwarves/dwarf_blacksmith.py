ffbda9982ca8dcd674c1469531d4f53545df0c55
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-05 18:19:48 -0300

    Add files via upload

---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,16 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, _skill_level: str) -> None:
+        super().__init__("")
+        self._skill_level = _skill_level
+
+    def get_rating(self) -> int:
+        return self._skill_level
+
+    def player_info(self) -> str:
+        return (
+            f"Dwarf blacksmith {self.nickname}."
+            f"{self.nickname} with skill of the {self._skill_level}"
+        )
---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,16 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(self, _hummer_level: str) -> None:
+        super().__init__("")
+        self._hummer_level = _hummer_level
+
+    def get_rating(self) -> int:
+        return (self._hummer_level + 4)
+
+    def player_info(self) -> str:
+        return (
+            f"Dwarf warrior {self.nickname}."
+            f"{self.nickname} has a hummer of the {self._hummer_level} level"
+        )
