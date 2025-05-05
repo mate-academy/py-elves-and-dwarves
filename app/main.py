306ec53c7bb44ec45f8648fcbaa2fc6cd423c527
Ustym <Ustym.hav@gmail.com>
2025-05-06 01:18:47 +0300

    aliance

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ def calculate_team_total_rating(team: List[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves_only: List[Elf]) -> str:
+def elves_concert(elves_only: List[Elf]) -> None:
     for elf in elves_only:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(gnomy: List[Dwarf]) -> str:
+def feast_of_the_dwarves(gnomy: List[Dwarf]) -> None:
     for dwarf in gnomy:
         dwarf.eat_favourite_dish()
