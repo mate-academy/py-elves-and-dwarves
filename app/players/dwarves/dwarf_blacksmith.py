604e922450df5256a797a60ae18a0319a50739a7
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-06 04:40:08 -0300

    Update dwarf_blacksmith.py

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -1,18 +1,18 @@
 from app.players.dwarves.dwarf import Dwarf
 
 
-class DwarfWarrior(Dwarf):
+class DwarfBlacksmith(Dwarf):
     def __init__(
-        self, nickname: str, favourite_dish: str, hummer_level: int
+        self, nickname: str, favourite_dish: str, skill_level: int
     ) -> None:
         super().__init__(nickname, favourite_dish)
-        self._hummer_level = hummer_level
+        self._skill_level = skill_level
 
     def get_rating(self) -> int:
-        return (self._hummer_level + 4)
+        return self._skill_level
 
     def player_info(self) -> str:
         return (
-            f"Dwarf warrior {self.nickname}. "
-            f"{self.nickname} has a hummer of the {self._hummer_level} level"
+            f"Dwarf blacksmith {self.nickname}. "
+            f"with skill of the {self._skill_level} level"
         )
