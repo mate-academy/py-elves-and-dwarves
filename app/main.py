4c2882b7e1a4a1cdb5e09fe421727fe71c31900b
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-06 04:53:39 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    team_ratings = []
+    for team_player in team:
+        team_ratings.append(team_player.get_rating())
+    return sum(team_ratings)
+
+
+def elves_concert(elves: list) -> str:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> str:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
---
+++ app/players/elves/test.py
@@ -0,0 +1,7 @@
+nickname = "Friend"
+musical_instrument = "flute"
+
+print(
+    f"{nickname} is playing a song on the "
+    f"{musical_instrument}"
+)
