2b9d65e055c777b90b192ce4bfff7030a95e6d8f
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-06 04:40:22 -0300

    Update dwarf_warrior.py

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -3,9 +3,9 @@ from app.players.dwarves.dwarf import Dwarf
 
 class DwarfWarrior(Dwarf):
     def __init__(
-        self, nickname: str, favourite_dish: str, hummer_level: str
+        self, nickname: str, favourite_dish: str, hummer_level: int
     ) -> None:
-        super().__init__(nickname, hummer_level)
+        super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
     def get_rating(self) -> int:
