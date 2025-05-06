c1f5a96e0bcd519746bc42035a358bdcc3e29338
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-06 04:25:05 -0300

    Update dwarf_blacksmith.py

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -13,6 +13,6 @@ class DwarfBlacksmith(Dwarf):
 
     def player_info(self) -> str:
         return (
-            f"Dwarf blacksmith {self.nickname}."
+            f"Dwarf blacksmith {self.nickname}. "
             f"{self.nickname} with skill of the {self._skill_level}"
         )
