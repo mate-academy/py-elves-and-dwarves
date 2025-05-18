c28e6ee44c12d73075ebbee125b184790b1dccbf
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-18 11:27:23 +0200

    players skills, rate

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from players.elves.elf import Elf
+from players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(team: list) -> int:
+    return sum(player.get_rating() for player in team)
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
