5bca05ae4dacfc65f90b9745c4627fb8ae3ba61f
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-18 11:43:46 +0200

    players skills, rate

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -3,9 +3,11 @@ from abc import ABC
 
 
 class Elf(Player, ABC):
-    def __init__(self, nickname: str, musical_instrument: str) -> None:
+    def __init__(self, nickname: str,
+                 musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+        print(f"{self.nickname} "
+              f"is playing a song on the {self._musical_instrument}")
