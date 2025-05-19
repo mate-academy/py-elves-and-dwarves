10f959d3074f18b6a20b376451a6f533a8200b8c
Hunach <sasha2001sek@gmail.com>
2025-05-19 03:00:37 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
+from typing import List
+
 from app.players.player import Player
 from app.players.dwarves.dwarf import Dwarf
 from app.players.elves.elf import Elf
 
 
-def calculate_team_total_rating(pl: [Player]) -> int:
+def calculate_team_total_rating(pl: List[Player]) -> int:
     suma = 0
     for put in pl:
         suma += put.get_rating()
     return suma
 
 
-def elves_concert(elfs: [Elf]) -> None:
+def elves_concert(elfs: List[Elf]) -> None:
     for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: [Dwarf]) -> None:
+def feast_of_the_dwarves(dwarfs: List[Dwarf]) -> None:
     for dwarf in dwarfs:
         dwarf.eat_favourite_dish()
