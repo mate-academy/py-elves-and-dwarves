e775111df242bd2e1749ff7dce0068ca11979a67
thomasdouglasam <thomasdouglasam@gmail.com>
2025-05-21 20:08:26 -0300

    Fix Elf class: use correct naming for musical_instrument parameter

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -3,9 +3,9 @@ from app.players.player import Player
 
 
 class Elf(Player):
-    def __init__(self, nickname: str, _musical_instrument: str) -> None:
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
-        self._musical_instrument = _musical_instrument
+        self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
         print(f"{self.nickname} is playing a song "
