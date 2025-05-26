bd7679d6aee8f46c02fdcb075348f051f0bb4722
Dmitrii Kozoriz <diluck2002@gmail.com>
2025-05-26 12:46:30 +0300

    Solution_2

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -4,7 +4,7 @@ from app.players.elves.elf import Elf
 class ElfRanger(Elf):
     def __init__(self, nickname: str,
                  musical_instrument: str,
-                 bow_level: str
+                 bow_level: int
                  ) -> None:
         super().__init__(nickname, musical_instrument)
         self._bow_level = bow_level
