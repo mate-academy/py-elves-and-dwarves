49af6af456efeaee846c4b565fc81a272abbc268
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-05-30 06:57:04 -0300

    'Solution'

--- app/players/player.py
+++ app/players/player.py
@@ -6,9 +6,9 @@ class Player(ABC):
         self.nickname = nickname
 
     @abstractmethod
-    def get_rating(self) -> int:
+    def player_info(self) -> str:
         pass
 
     @abstractmethod
-    def player_info(self) -> str:
+    def get_rating(self) -> int:
         pass
