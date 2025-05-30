60423c3efb4cc9e6e15f10df3569556a433f32a1
Vladyslav Petrovskyi <vladadn47@gmail.com>
2025-05-30 12:04:53 +0200

    Solution

---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,23 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(
+            self,
+            nickname: str,
+            favourite_dish: str,
+            hummer_level: int
+    ) -> None:
+        super().__init__(nickname=nickname, favourite_dish=favourite_dish)
+        self.__hummer_level = hummer_level
+
+    def player_info(self) -> str:
+
+        return (
+            f"Dwarf warrior {self.nickname}. "
+            f"{self.nickname} has a hummer of the {self.__hummer_level} level"
+        )
+
+    def get_rating(self) -> int:
+
+        return self.__hummer_level + 4
---
+++ app/players/elves/elf.py
@@ -0,0 +1,13 @@
+from app.players.player import Player
+
+
+class Elf(Player):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname=nickname)
+        self.__music_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(
+            f"{self.nickname} is playing a song on the "
+            f"{self.__music_instrument}"
+        )
