167d13d31a80b9ff31928b5c109184ba2e3ea92f
Taras <tarasostapchuk27@gmail.com>
2025-06-12 22:38:49 +0200

    Correct Solution

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -7,7 +7,7 @@ class Druid(Elf):
         self,
         nickname: str,
         musical_instrument: str,
-        favourite_spell: int
+        favourite_spell: str
     ) -> None:
         super().__init__(nickname, musical_instrument)
         self._favourite_spell = favourite_spell
