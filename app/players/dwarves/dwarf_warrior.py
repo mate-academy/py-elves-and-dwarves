c888698c15bda5fb084dce4b6daa2c52737bd2f7
Kaddsi <kirillow.danil@gmail.com>
2025-06-13 17:59:52 +0300

    Solution

---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,14 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(self, hummer_level: str, favourite_dish: str, nickname: str):
+        super().__init__(favourite_dish, nickname)
+        self.hummer_level = hummer_level
+
+    def player_info(self):
+        return (f"Dwarf warrior {self.nickname}. {self.nickname} has "
+                f"a hummer of the {self.hummer_level} level")
+
+    def get_rating(self):
+        return self.hummer_level + 4
---
+++ app/players/elves/druid.py
@@ -0,0 +1,15 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(self, favourite_spell: str,
+                 musical_instrument: str, nickname: str):
+        super().__init__(musical_instrument, nickname)
+        self.favourite_spell = favourite_spell
+
+    def player_info(self):
+        return (f"Druid {self.nickname}. {self.nickname} has a "
+                f"favourite spell: {self.favourite_spell}")
+
+    def get_rating(self):
+        return len(self.favourite_spell)
