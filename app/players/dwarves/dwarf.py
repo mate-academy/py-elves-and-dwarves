7e9db2f16787fc60eab5c9a449936d1cac3cf754
Bohdan Tsapenko <earldoom@Bohdans-MacBook-Air.local>
2025-06-27 16:23:24 +0400

    Solution

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from app.players.player import Player
+
+
+class Dwarf(Player):
+    def __init__(self, favourite_dish: str, nickname: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,14 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, skill_level: int, favourite_dish: str, nickname: str) -> None:
+        super().__init__(favourite_dish=favourite_dish, nickname=nickname)
+        self._skill_level = skill_level
+
+    def player_info(self) -> str:
+        return (f"Dwarf blacksmith {self.nickname} with skill of the "
+                f"{self._skill_level} level")
+
+    def get_rating(self) -> int:
+        return self._skill_level
