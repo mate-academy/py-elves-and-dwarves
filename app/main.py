455570bcd34cdae323d64415e9968ecef4c86641
viannik <ivan.nikitski@gmail.com>
2025-06-29 21:03:10 +0300

    Add player classes for Elf, Dwarf, and their respective functionalities

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.players.player import Player
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(players: list[Elf]) -> None:
+    for player in players:
+        player.play_elf_song()
+
+
+def feast_of_the_dwarves(players: list[Dwarf]) -> None:
+    for player in players:
+        player.eat_favourite_dish()
