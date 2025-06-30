a2745776d88242a5cb77c47e29ca0bcad9f85144
Andrii Novikov <forytube2023@gmail.com>
2025-06-30 14:41:00 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+from typing import List
+from players.player import Player
+from players.elves.elf import Elf
+from players.dwarves.dwarf import Dwarf
+
+def calculate_team_total_rating(team: List[Player]) -> int:
+    return sum(member.get_rating() for member in team)
+
+def elves_concert(elves: List[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+def feast_of_the_dwarves(dwarves: List[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
