4edbc98a1b56d6888345549721c37686db6e558e
Andrii Novikov <forytube2023@gmail.com>
2025-06-30 14:46:31 +0300

    Update elf.py

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,7 +1,12 @@
 from ..player import Player
 
+
 class Elf(Player):
-    def __init__(self, nickname: str, musical_instrument: str) -> None:
+    def __init__(
+        self,
+        nickname: str,
+        musical_instrument: str
+    ) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
