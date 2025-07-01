aed7a118e186ae3da84317939fb1250f6b8687a9
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-07-01 16:06:31 +0300

    Implemented Dwarves and Elves classes

---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,19 @@
+from app.players.elves.elf import Elf
+
+
+class ElfRanger(Elf):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            bow_level: int
+    ) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
+
+    def player_info(self) -> None:
+        return (f"Elf ranger {self._nickname}. {self._nickname} has "
+                f"bow of the {self._bow_level} level")
