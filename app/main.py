93791e24a6756fdafa3a952c71dca63821e4bfbe
Mirosław Kudłacz <mirek.kudlacz@gmail.com>
2025-07-01 21:55:14 +0200

    Fixed for loops

--- app/main.py
+++ app/main.py
@@ -4,12 +4,17 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team_list: list[Player]) -> int:
-    return sum(i.get_rating() for i in team_list)
+    total = 0
+    for i in team_list:
+        total += i.get_rating()
+    return total
 
 
 def elves_concert(team_list: list[Elf]) -> None:
-    [member.play_elf_song() for member in team_list]
+    for i in team_list:
+        i.play_elf_song()
 
 
 def feast_of_the_dwarves(team_list: list[Dwarf]) -> None:
-    [i.eat_favourite_dish() for i in team_list]
+    for i in team_list:
+        i.eat_favourite_dish()
