d95d995374d2d154c67deddfca0b0f5a6217820b
Glego <glego200323@gmail.com>
2025-07-02 12:49:14 +0300

    Solution fix naming of elves and dwarves, type of abstract method

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elfs: list[Elf]) -> None:
-    for elf in elfs:
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
-    for dwarf in dwarfs:
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
         dwarf.eat_favourite_dish()
