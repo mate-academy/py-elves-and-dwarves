9a6465668f51f87cf91c3f83e1e032a29a4ba007
yarik <super.racing@ukr.net>
2025-07-03 13:08:15 +0300

    nicknames fixed

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -2,10 +2,10 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 class DwarfWarrior(Dwarf):
-    def __init__(self, name: str,
+    def __init__(self, nickname: str,
                  favourite_dish: str,
                  hummer_level: int) -> None:
-        super().__init__(name, favourite_dish)
+        super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
     def player_info(self) -> str:
--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -2,10 +2,10 @@ from app.players.elves.elf import Elf
 
 
 class ElfRanger(Elf):
-    def __init__(self, name: str,
+    def __init__(self, nickname: str,
                  musical_instrument: str,
                  bow_level: int) -> None:
-        super().__init__(name, musical_instrument)
+        super().__init__(nickname, musical_instrument)
         self._bow_level = bow_level
 
     def player_info(self) -> str:
