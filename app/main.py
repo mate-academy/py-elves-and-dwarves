50a71ac00715e6df014f727a3b20ab1ead992437
Liliya Kyrylyshena <morsimmortalis05@gmail.com>
2025-07-03 17:32:11 +0300

    fixed something

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from app.players.dwarves.dwarf import Dwarf
+from app.players.elves.elf import Elf
+from app.players.player import Player
+
 def calculate_team_total_rating(players_list: list) -> int:
     total_rating = 0
     for player in players_list:
@@ -6,8 +10,8 @@ def calculate_team_total_rating(players_list: list) -> int:
 
 
 def elves_concert(elves_list: list) -> None:
-    for elve in elves_list:
-        elve.play_elf_song()
+    for elf in elves_list:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves_list: list) -> None:
