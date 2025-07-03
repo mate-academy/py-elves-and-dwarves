bba5504e5d2dee6ed1d33bf637aa465929f51b55
yarik <super.racing@ukr.net>
2025-07-03 12:51:06 +0300

    added blank lines

--- app/main.py
+++ app/main.py
@@ -6,9 +6,11 @@ from players.elves.elf import Elf
 def calculate_team_total_rating(players: list[Player]) -> int:
     return sum([elem.get_rating() for elem in players])
 
+
 def elves_concert(elfs: list[Elf]) -> None:
     for elem in elfs:
         elem.play_elf_song()
+
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for elem in dwarves:
