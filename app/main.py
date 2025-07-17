bdef98b3686d34b52559f9abc72f0cc27cc45867
DiWILove <povilaitusdiana@gmail.com>
2025-07-17 10:27:42 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger
+
+
+def calculate_team_total_rating(list_of_players: list) -> int:
+    total_rating = 0
+    for player in list_of_players:
+        total_rating += player.get_rating()
+    return total_rating
+
+def elves_concert(list_of_elves: list) -> None:
+    for elf in list_of_elves:
+        elf.play_elf_song()
+
+def feast_of_the_dwarves(list_of_dwarves: list) -> None:
+    for dwarf in list_of_dwarves:
+        dwarf.eat_favourite_dish()
---
+++ app/players/elves/elf.py
@@ -0,0 +1,11 @@
+from app.players.player import Player
+
+
+class Elf(Player):
+
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self.__musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self.__musical_instrument}")
