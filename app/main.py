42e4cf5c56a1981451c62d637ee9749f803329c0
Mateusz <szanieckimateusz@gmail.com>
2025-07-21 16:09:37 +0200

    Completed implication of elves_concert and feast_of_the_dwarves functions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def calculate_team_total_rating(players: list) -> float:
+def calculate_team_total_rating(players: list) -> int:
     sum_of_rating = 0
     for player in players:
         sum_of_rating += player.get_rating()
@@ -6,9 +6,11 @@ def calculate_team_total_rating(players: list) -> float:
     return sum_of_rating
 
 
-def elves_concert(elves: list) -> float:
-    pass
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> float:
-    pass
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
