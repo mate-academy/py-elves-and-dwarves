e660af16a315fe67209dd82ee529d8d4048a1ccc
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-22 21:18:33 +0200

    hammer -> hummer

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -3,13 +3,13 @@ from app.players.dwarves.dwarf import Dwarf
 
 class DwarfWarrior(Dwarf):
     def __init__(self, nickname: str,
-                 favourite_dish: str, hammer_level: int) -> None:
+                 favourite_dish: str, hummer_level: int) -> None:
         super().__init__(nickname, favourite_dish)
-        self._hammer_level = hammer_level
+        self._hummer_level = hummer_level
 
     def player_info(self) -> str:
         return (f"Dwarf warrior {self.nickname}."
-                f" {self.nickname} has a hammer of the "
+                f" {self.nickname} has a hummer of the "
                 f"{self._hummer_level} level")
 
     def get_rating(self) -> int:
