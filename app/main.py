be0ef8e6107174a804d16ec544011a7a686d7644
Csillag61 <csillagok@gmail.com>
2025-07-23 16:52:41 -0400

    fixed file structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from players.player import Player
-from players.elves.elf import Elf
-from players.dwarves.dwarf import Dwarf
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
--- players/elves/elf.py
+++
@@ -1,9 +0,0 @@
-from players.player import Player
-
-class Elf(Player):
-    def __init__(self, nickname: str, musical_instrument: str) -> None:
-        super().__init__(nickname)
-        self.musical_instrument = musical_instrument
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self.musical_instrument}")
