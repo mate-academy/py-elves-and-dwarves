ca1b583787ab53d071df839e7eaa6d892ae4d934
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-01 22:19:53 +0200

    2

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,13 +9,15 @@ import pytest
 
 from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
-from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
-from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.player import Player
+from app.main import (
+    Dwarf,
+    DwarfBlacksmith,
+    DwarfWarrior,
+    Druid,
+    Elf,
+    ElfRanger,
+    Player,
+)
 
 
 @pytest.mark.parametrize(
