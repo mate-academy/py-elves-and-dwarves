82e4a6ef8f756edd9f557ad2c1e8af33dfc07943
Oleksandr Bavdyk <sbavdik@gmail.com>
2025-08-11 17:22:02 +0300

    fix

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from app.players.player import Player
 
+
 class Dwarf(Player, ABC):
     def __init__(self, nickname: str, favourite_dish: str) -> None:
         super().__init__(nickname)
