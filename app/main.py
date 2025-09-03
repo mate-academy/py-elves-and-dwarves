725c9f37cf6ee0e9210c5119937c5ac96c937e71
AKupriichuk <alexa05kup@gmail.com>
2025-09-03 18:35:46 +0300

    Error test

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(players: list) -> int:
+    team_total_rating = 0
+    for player in players:
+        team_total_rating += player.get_rating()
+    return team_total_rating
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
