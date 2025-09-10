96f697034c50678ba3e2bf49906c6bf9dc05659a
Oleh Halas <oleggalas8@gmail.com>
2025-09-10 15:47:48 +0300

    Fix errors

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def calculate_team_total_rating(team: list[Player]) -> int:
 
 
 def elves_concert(team: list[Elf]) -> None:
-    [elf.play_elf_song() for elf in team]
+    for elf in team:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(team: list[Dwarf]) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in team]
+    for dwarf in team:
+        dwarf.eat_favourite_dish()
