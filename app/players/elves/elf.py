b3d48b6448a636e163c236210f41af9ffcc74ded
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-09-15 21:51:30 +0100

    created meta universe with 2 races (Elves and Dwarves) created parent abstract class Player with two child classes Elf and Dwarf, both with 2 child classes also. Calculated rating for each player and each team. organised in understandable project structure.

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -8,6 +8,6 @@ class Elf(Player, ABC):
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f'{self.nickname} '
-              f'is playing a song on the '
-              f'{self._musical_instrument}')
+        print(f"{self.nickname} "
+              f"is playing a song on the "
+              f"{self._musical_instrument}")
