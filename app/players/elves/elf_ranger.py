5c7ad328fa9dd2e620e06c731c03479922fd2f65
Kostya <HeineRx2@gmail.com>
2025-09-16 18:45:16 +0300

    Done

---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,16 @@
+# app/players/elves/elf_ranger.py
+from app.players.elves.elf import Elf
+
+
+class ElfRanger(Elf):
+    def __init__(self, nickname: str, musical_instrument: str,
+                 bow_level: int) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def get_rating(self) -> float:
+        return 3 * self._bow_level
+
+    def player_info(self) -> str:
+        return (f"Elf ranger {self.nickname}. {self.nickname} has bow of "
+                f"the {self._bow_level} level")
