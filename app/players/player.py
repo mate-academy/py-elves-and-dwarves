568be0d1eb72281fd33f8caa4b1c1b93bbaa3b24
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-09-16 17:47:08 +0200

    corrected player.py

--- app/players/player.py
+++ app/players/player.py
@@ -2,14 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class Player(ABC):
-    @abstractmethod
     def __init__(self, nickname: str) -> None:
         self.nickname = nickname
 
     @abstractmethod
-    def get_rating(self) -> None:
+    def get_rating(self) -> int:
         pass
 
     @abstractmethod
-    def player_info(self) -> None:
+    def player_info(self) -> str:
         pass
