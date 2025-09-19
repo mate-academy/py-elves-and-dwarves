7e73c16888ce7787919e6d56a4af9d2f35a9bfe0
HalasOleh <145432434+HalasOleh@users.noreply.github.com>
2025-09-19 17:29:03 +0300

    Update dwarf_warrior.py

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -5,10 +5,10 @@ class DwarfWarrior(Dwarf):
     def __init__(
             self,
             nickname: str,
-            favorite_dish: str,
+            favourite_dish: str,
             hummer_level: int
     ) -> None:
-        super().__init__(nickname, favorite_dish)
+        super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
     def player_info(self) -> str:
@@ -19,4 +19,5 @@ class DwarfWarrior(Dwarf):
 
     def get_rating(self) -> int:
         return self._hummer_level + 4
+
 
