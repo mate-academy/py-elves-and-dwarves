0ccd8746520fca0c31696fada9e6d4a4d08c84d0
Anton Bilous <anton.bilous.job@gmail.com>
2025-09-19 21:33:21 +0300

    s

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from app.players.player import Player
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
-    return sum([player.get_rating() for player in players])
+    return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list[Elf]) -> None:
-    [elv.play_elf_song() for elv in elves]
+    for elf in elves:
+        elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
-    [dwarf.eat_favourite_dish() for dwarf in dwarfs]
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves: 
+        dwarf.eat_favourite_dish()
