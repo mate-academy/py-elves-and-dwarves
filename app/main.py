77f94274d5461cced701bb38b8c3e310b0637356
Tomasz Herman <tomekh2015@gmail.com>
2025-09-25 14:45:03 +0200

    Solution v2

--- app/main.py
+++ app/main.py
@@ -1,54 +1,28 @@
-from players.player import Player
-from players.elves.elf import Elf
-from players.elves.elf_ranger import ElfRanger
-from players.elves.druid import Druid
-from players.dwarves.dwarf import Dwarf
-from players.dwarves.dwarf_warrior import DwarfWarrior
-from players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+import pytest
+import io
+from contextlib import redirect_stdout
 
+from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 def calculate_team_total_rating(players: list[Player]) -> int:
+    """Zwraca sumę ratingów wszystkich graczy w zespole."""
     return sum(player.get_rating() for player in players)
 
 
 def elves_concert(elves: list[Elf]) -> None:
+    """Wywołuje metodę play_elf_song dla wszystkich elfów w liście."""
     for elf in elves:
         elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    """Wywołuje metodę eat_favourite_dish dla wszystkich krasnoludów w liście."""
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
-
-
-if __name__ == "__main__":
-    ranger = ElfRanger("Nardual Chaekian", "flute", 7)
-    print(ranger.player_info())
-    print(ranger.get_rating())
-    ranger.play_elf_song()
-
-    druid = Druid("Elwin", "harp", "Fireball")
-    print(druid.player_info())
-    print(druid.get_rating())
-    druid.play_elf_song()
-
-    warrior = DwarfWarrior("Thiddeal", "French Fries", 7)
-    print(warrior.player_info())
-    print(warrior.get_rating())
-    warrior.eat_favourite_dish()
-
-    blacksmith = DwarfBlacksmith("Borin", "Stew", 10)
-    print(blacksmith.player_info())
-    print(blacksmith.get_rating())
-    blacksmith.eat_favourite_dish()
-
-    team = [ranger, druid, warrior, blacksmith]
-    print("Total team rating:", calculate_team_total_rating(team))
-
-    elves = [ranger, druid]
-    print("\nElves concert:")
-    elves_concert(elves)
-
-    dwarves = [warrior, blacksmith]
-    print("\nFeast of the dwarves:")
-    feast_of_the_dwarves(dwarves)
