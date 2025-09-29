3ad484f121979d747528edbcd33314c40049f390
Sontsevolosa <sontsevolosa@gmail.com>
2025-09-29 13:44:57 +0300

    Add changes according to AI Buddy suggestions

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
-def calculate_team_total_rating(team_players: list) -> int:
-    result = []
-
-    for team_player in team_players:
-        result.append(team_player.get_rating())
-
-    return sum(result)
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-def elves_concert(elves: list) -> None:
-    for elve in elves:
-        elve.play_elf_song()
+def calculate_team_total_rating(team_players: list[Player]) -> int:
+    return sum(team_player.get_rating() for team_player in team_players)
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
