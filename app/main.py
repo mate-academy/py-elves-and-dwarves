9bd360e600c0c6ade2b64f9150b076e4c55c1733
steven42069 <stevendiaz152@gmail.com>
2025-10-01 19:40:10 -0400

    not so

--- app/main.py
+++ app/main.py
@@ -1,21 +1,18 @@
 from __future__ import annotations
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
 from app.players.player import Player
 
 
 def calculate_team_total_rating(team: list["Player"]) -> int:
     return sum(player.get_rating() for player in team)
 
+
 def elves_concert(elves: list["Elf"]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
+
 def feast_of_the_dwarves(dwarves: list["Dwarf"]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
-
