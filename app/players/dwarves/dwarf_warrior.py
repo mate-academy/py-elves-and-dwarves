dff0f50cf1c2815b92b9a26e645380b623d49660
Ihor Zasimenko <boxletter2011@gmail.com>
2025-10-01 17:06:07 +0300

    Fixed code

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -2,7 +2,10 @@ from .dwarf import Dwarf
 
 
 class DwarfWarrior(Dwarf):
-    def __init__(self, nickname: str, favourite_dish: str, hummer_level: int) -> None:
+    def __init__(
+            self, nickname: str, favourite_dish: str,
+            hummer_level: int,
+    ) -> None:
         super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,4 +7,7 @@ class Elf(Player):
         self._musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+        print(
+            f"{self.nickname} is playing a song on "
+            f"the {self._musical_instrument}"
+        )
