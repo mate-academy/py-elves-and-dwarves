6b512fb8249f5b4e7cbc872ed254a5a050ba8342
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-01 10:39:01 +0200

    implementing player_info method for class's

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -5,3 +5,6 @@ class DwarfBlacksmith(Dwarf):
     def __init__(self, nickname: str, favourite_dish: str, skill_level: int) -> None:
         super().__init__(nickname, favourite_dish)
         self._skill_level = skill_level
+
+    def player_info(self) -> str:
+        return f"Dwarf blacksmith {self.nickname} with skill of the {self._skill_level} level"
--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -5,3 +5,6 @@ class DwarfWarrior(Dwarf):
     def __init__(self, nickname: str, favourite_dish: str, hummer_level: int) -> None:
         super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
+
+    def player_info(self) -> str:
+        return f"Dwarf warrior {self.nickname}. {self.nickname} has a hummer of the {self._hummer_level} level"
