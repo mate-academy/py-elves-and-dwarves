869e7615239d4f855680229e988425f53d87e5cd
Anton Tsikhanovich <alluspeh@gmail.com>
2025-10-07 10:22:59 +0200

    Solution after review

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.players.player import Player
 from app.players.elves.elf import Elf
 
 
-def calculate_team_total_rating(list_players: list[Player]) -> int:
-    return sum(player.get_rating() for player in list_players)
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
 
 
-def elves_concert(list_elfs: list[Elf]) -> None:
-    for elf in list_elfs:
+def elves_concert(elfs: list[Elf]) -> None:
+    for elf in elfs:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(list_dwarves: list[Dwarf]) -> None:
-    for dwarve in list_dwarves:
-        dwarve.eat_favourite_dish()
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -7,7 +7,7 @@ class Elf(Player, ABC):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
-    def play_elf_song(self) -> None:
+    def play_elf_song(self) -> str:
         print(f"{self.nickname} "
               f"is playing a song on the "
               f"{self._musical_instrument}")
