043df6942fe041ddab1636252ded3946aadfe99d
Oleh Ilnytskyi <ilnytskyy67922@gmail.com>
2025-10-09 17:05:15 +0100

    Make Elf and Dwarf abstract base classes

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,7 +1,9 @@
+from abc import ABC
+
 from app.players.player import Player
 
 
-class Elf(Player):
+class Elf(Player, ABC):
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
