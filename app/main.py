087dbc7d39815a1a43f9b76000ffe4ec5438760c
Oleh Bozhak <olbozhak@gmail.com>
2025-10-13 18:06:54 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+
+
+def calculate_team_total_rating(players: list) -> int:
+    return sum(rate.get_rating() for rate in players)
+
+
+def elves_concert(elves: list) -> None:
+    for sing in elves:
+        sing.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,15 @@
+from app.players.elves.elf import Elf
+
+class ElfRanger(Elf):
+    def __init__(self, nickname, musical_instrument, bow_level: int) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._bow_level = bow_level
+
+    def player_info(self) -> None:
+        print(
+            f"Elf ranger {self.nickname}. "
+            f"{self.nickname} has bow of the {self._bow_level} level"
+        )
+
+    def get_rating(self) -> int:
+        return 3 * self._bow_level
