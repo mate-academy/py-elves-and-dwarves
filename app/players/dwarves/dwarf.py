4720ed580490276c7e7834a9f586d71dd9137f51
Anastasiia Pohonets <29karasik@gmail.com>
2025-10-18 16:24:32 +0300

    Solution

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,18 @@
+from app.players.player import Player
+
+
+class Dwarf(Player):
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
+
+    # @abstractmethod
+    # def get_rating(self) -> int:
+    #     pass
+    #
+    # @abstractmethod
+    # def player_info(self) -> str:
+    #     pass
