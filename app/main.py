58b103550b2949c4b0b6c69bfad8703ba4e8d13a
Rodion <r.demianetss@gmail.com>
2025-10-19 18:56:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    total = 0
+    for i in team:
+        total += i.get_rating()
+    return total
+
+
+def elves_concert(elves: list) -> None:
+    for i in elves:
+        i.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for i in dwarves:
+        i.eat_favourite_dish()
