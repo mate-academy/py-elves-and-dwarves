1b0e14617ecd6bb936644e2c729d186bb817bc54
Lana Topchii <topchii.svitlana@gmail.com>
2025-10-21 00:24:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from __future__ import annotations
+from typing import Sequence
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+
+
+def calculate_team_total_rating(players: Sequence[Player]) -> int:
+    """Return total rating of all players."""
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(elves: Sequence[Elf]) -> None:
+    """Each elf plays a song."""
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: Sequence[Dwarf]) -> None:
+    """Each dwarf eats favourite dish."""
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
