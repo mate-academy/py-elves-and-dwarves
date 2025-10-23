9af4ca5f87ce156bd8c9c2f993fde4352f40463f
Vorongor <mr.maddarknes@gmail.com>
2025-10-23 14:37:30 +0300

    Start commit

--- app/main.py
+++ app/main.py
@@ -1,17 +1,5 @@
-def calc(a, b):
-    def add():
-        return a + b
+class Elf:
+    pass
 
-    def sub():
-        return a - b
-
-    calc.add = add
-    calc.sub = sub
-
-    return calc
-
-
-user_calc = calc(2, 4)
-print(user_calc.add())
-print(user_calc.sub())
-print(dir(calc))
+class Dwarf:
+    pass
