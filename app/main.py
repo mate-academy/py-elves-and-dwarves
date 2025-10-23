2a125aa0f490599ad133a3152c8c3624046c287b
Vorongor <mr.maddarknes@gmail.com>
2025-10-23 14:35:33 +0300

    commit test fo chech commit rule

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def calc(a, b):
+    def add():
+        return a + b
+
+    def sub():
+        return a - b
+
+    calc.add = add
+    calc.sub = sub
+
+    return calc
+
+
+user_calc = calc(2, 4)
+print(user_calc.add())
+print(user_calc.sub())
+print(dir(calc))
