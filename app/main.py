7bfe5e236ee1b0e6a8395c67033fec93a63d452a
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-10-24 13:23:11 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+from players.dwarves.dwarf import Dwarf
+
+if __name__ == "__main__":
+    dwarf = Dwarf("Thorin", "Roast Mutton")
+    dwarf.eat_favourite_dish()
+    print(dwarf.player_info())
+    print("Rating:", dwarf.get_rating())
