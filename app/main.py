aefab5df6774e136ae656fbb67b904f59a150f62
Vasul <Pavvacil@gmail.com>
2025-10-26 15:42:18 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -4,12 +4,8 @@ from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(team: List[Player]) -> None:
-    total_rating = 0
-    for player in team:
-        player.get_rating()
-        total_rating += player.get_rating()
-    return total_rating
+def calculate_team_total_rating(team: List[Player]) -> int:
+    return sum(player.get_rating()for player in team)
 
 
 def elves_concert(elves: List[Elf]) -> None:
--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -5,7 +5,7 @@ class Dwarf(Player):
 
     def __init__(self, nickname: str, favourite_dish: str) -> None:
         super().__init__(nickname)
-        self._favorite_dish = favourite_dish
+        self._favourite_dish = favourite_dish
 
     def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} is eating {self._favorite_dish}")
+        print(f"{self.nickname} is eating {self._favourite_dish}")
