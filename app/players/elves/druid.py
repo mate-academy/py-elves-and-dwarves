70b37938b214f37baeb6061ee2a8892bb5c3b15a
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 23:07:07 +0100

    implement get_rating and player_info for Druid

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -10,3 +10,12 @@ class Druid(Elf):
     ) -> None:
         super().__init__(nickname, musical_instrument)
         self.favourite_spell = favourite_spell
+
+    def get_rating(self):
+        return len(self.favourite_spell)
+
+    def player_info(self):
+        return (
+            f"Druid {self.nickname}. ",
+            f"{self.nickname} has a favourite spell: {self.favourite_spell}"
+        )
