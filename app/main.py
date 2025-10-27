13221f959ae4a564a89643a084ecb74dc92fe426
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-10-27 12:12:11 +0100

    poprawki

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from app.players.dwarves.dwarf import Dwarf
-
-
-def main() -> None:
-    dwarf = Dwarf("Thorin", "Roast Mutton")
-    dwarf.eat_favourite_dish()
-    print(dwarf.player_info())
-    print("Rating:", dwarf.get_rating())
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
 
 
 def calculate_team_total_rating(players: list) -> int:
@@ -20,3 +17,20 @@ def elves_concert(elves: list) -> None:
 def feast_of_the_dwarves(dwarves: list) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
+
+
+def main() -> None:
+    elf1 = ElfRanger("Nardual", "flute", 7)
+    elf2 = Druid("Elenaril", "flute", "abrakadabra")
+    dwarf1 = DwarfWarrior("Thiddeal", "French Fries", 7)
+    dwarf2 = DwarfBlacksmith("Thiddeal", "French Fries", 14)
+
+    team = [elf1, elf2, dwarf1, dwarf2]
+
+    elves_concert([elf1, elf2])
+    feast_of_the_dwarves([dwarf1, dwarf2])
+    print("Team rating:", calculate_team_total_rating(team))
+
+
+if __name__ == "__main__":
+    main()
