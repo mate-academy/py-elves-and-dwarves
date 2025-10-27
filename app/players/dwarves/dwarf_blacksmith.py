555d6b5adcba6e9b9ce1a2aa8469f922ccf65889
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-28 00:02:05 +0100

    change some attributes to be protected

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -9,13 +9,13 @@ class DwarfBlacksmith(Dwarf):
         skill_level: int
     ) -> None:
         super().__init__(nickname, favourite_dish)
-        self.skill_level = skill_level
+        self._skill_level = skill_level
 
     def get_rating(self) -> int:
-        return self.skill_level
+        return self._skill_level
 
     def player_info(self) -> str:
         return (
             f"Dwarf blacksmith {self.nickname} "
-            f"with skill of the {self.skill_level} level"
+            f"with skill of the {self._skill_level} level"
         )
