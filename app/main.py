9cb4a716fe6cb138668d8583d9ded35e0f9eb8d7
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 23:17:39 +0100

    implement elves_concert function

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from app.players.player import Player
+from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(team_members: list[Player]):
     return sum([member.get_rating() for member in team_members])
+
+def elves_concert(elves: list[Elf]):
+    for elf in elves:
+        elf.play_elf_song()
