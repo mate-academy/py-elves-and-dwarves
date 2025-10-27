10cc6ef039ed877de034b3de223d1602ce949da2
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 22:58:22 +0100

    create DwarfBlacksmith class

---
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -0,0 +1,12 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(
+        self,
+        nickname: str,
+        favourite_dish: str,
+        skill_level: int
+    ) -> None:
+        super().__init__(nickname, favourite_dish)
+        self.skill_level = skill_level
