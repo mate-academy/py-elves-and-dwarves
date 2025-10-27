dbe6fe9ee3a69192afaebe0a34073acc1fe18c7a
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 22:57:50 +0100

    create DwarfWarrior class

---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,12 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(
+        self,
+        nickname: str,
+        favourite_dish: str,
+        hummer_level: int
+    ) -> None:
+        super().__init__(nickname, favourite_dish)
+        self.hummer_level = hummer_level
