a00dce888f986ae3d895c903745c68b3973f313f
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-10-27 12:15:05 +0100

    poprawki 2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from app.players.dwarves.dwarf import Dwarf
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 from app.players.dwarves.dwarf_warrior import DwarfWarrior
 from app.players.elves.elf_ranger import ElfRanger
