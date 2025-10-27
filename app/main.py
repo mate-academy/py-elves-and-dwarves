9500f1ab73d16f784aee7fda1be6d4512eaee356
Yehor <gamersirius053@gmail.com>
2025-10-27 13:12:49 +0300

    solution of py-elves-and-dwarves NUMBER 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def calculate_team_total_rating(players: list) -> int | float:
-    team_rating = []
-    for i in players:
-        team_rating.append(i.get_rating())
-    return sum(team_rating)
+    return sum([player.get_rating() for player in players])
 
 
 def elves_concert(elves: list) -> None:
