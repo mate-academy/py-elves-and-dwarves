c43be8930064e285d1fac8bab3ff387a12bd3b38
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 23:02:22 +0100

    implement get_rating and player_info for DwarfBlacksmith

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -10,3 +10,12 @@ class DwarfBlacksmith(Dwarf):
     ) -> None:
         super().__init__(nickname, favourite_dish)
         self.skill_level = skill_level
+
+    def get_rating(self):
+        return self.skill_level
+
+    def player_info(self):
+        return (
+            f"Dwarf blacksmith {self.nickname} ",
+            f"with skill of the {self.skill_level} level"
+        )
