01b87ab99ddfcbc2f73ea6ebd67e16ad53170b5a
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 23:05:17 +0100

    implement get_rating and player_info for DwarfWarrior

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -10,3 +10,12 @@ class DwarfWarrior(Dwarf):
     ) -> None:
         super().__init__(nickname, favourite_dish)
         self.hummer_level = hummer_level
+
+    def get_rating(self):
+        return self.hummer_level + 4
+
+    def player_info(self):
+        return (
+            f"Dwarf warrior {self.nickname}. ",
+            f"{self.nickname} has a hummer of the {self.hummer_level} level"
+        )
