cbef521cab59a7d9d09a0a3556be9e5f3453a279
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-27 23:55:40 +0100

    fix code as per checklist item 1

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.players.dwarves.dwarf import Dwarf
 
 
 def calculate_team_total_rating(team_members: list[Player]) -> int:
-    return sum([member.get_rating() for member in team_members])
+    return sum(member.get_rating() for member in team_members)
 
 
 def elves_concert(elves: list[Elf]) -> None:
