0673ab7180d078a8e579cac697c8809fb79e9bb2
Yehor <gamersirius053@gmail.com>
2025-10-27 13:03:31 +0300

    solution of py-elves-and-dwarves NUMBER 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def calculate_team_total_rating(players: list) -> int | float:
+    team_rating = []
+    for i in players:
+        team_rating.append(i.get_rating())
+    return sum(team_rating)
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
