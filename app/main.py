1f2fe8bd1c024c37d10c1462f14792c190c5da50
Wojciech Witkowski <witus85@gmail.com>
2025-10-30 19:32:49 +0100

    annotation fix

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
+from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list) -> int | float:
+def calculate_team_total_rating(players: list[Player]) -> int | float:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(players: list) -> None:
+def elves_concert(players: list[Elf]) -> None:
     for player in players:
-        if isinstance(player, Elf):
-            player.play_elf_song()
+        player.play_elf_song()
 
 
-def feast_of_the_dwarves(players: list) -> None:
+def feast_of_the_dwarves(players: list[Dwarf]) -> None:
     for player in players:
-        if isinstance(player, Dwarf):
-            player.eat_favourite_dish()
+        player.eat_favourite_dish()
