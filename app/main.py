84908c0bd0acd0f18eb9827f284a34f2a8e7f13d
Zanko Yaroslav <arikzanko9@gmail.com>
2025-10-31 16:39:29 +0200

    task

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from app.players.player import Player
 from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior # noqa: F401
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith # noqa: F401
 from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
+from app.players.elves.elf_ranger import ElfRanger # noqa: F401
+from app.players.elves.druid import Druid # noqa: F401
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
