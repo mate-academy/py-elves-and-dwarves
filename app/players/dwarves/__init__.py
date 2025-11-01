3042d7071eff3476d7710e43602306b924016ada
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-01 18:01:17 +0200

    Solution

---
+++ app/players/dwarves/__init__.py
@@ -0,0 +1,9 @@
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+
+__all__ = [
+    "Dwarf",
+    "DwarfBlacksmith",
+    "DwarfWarrior",
+]
---
+++ app/players/elves/__init__.py
@@ -0,0 +1,9 @@
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+
+__all__ = [
+    "Elf",
+    "Druid",
+    "ElfRanger",
+]
