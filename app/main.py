1fe5d556cbd716d54f8507e9eb402ec390f2dfb1
Ostap Shp <stark171705@gmail.com>
2025-11-02 16:26:20 +0200

    Completed task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    total = 0
+    for player in players:
+        total += player.get_rating()
+    return total
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
