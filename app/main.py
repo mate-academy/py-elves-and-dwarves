13dcbaf74f0933465e291e2e709d20faccb907f0
Andrii Chychkan <mywork1728@gmail>
2025-11-03 13:59:47 +0200

    added solution

--- app/main.py
+++ app/main.py
@@ -1,45 +1,15 @@
-from abc import abstractmethod, ABC
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-class Player(ABC):
-    def __init__(self, nickname: str) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self) -> None:
-        pass
-
-    @abstractmethod
-    def player_info(self) -> None:
-        pass
+def calculate_team_total_rating(players_list: list[Player]) -> int:
+    return sum(el.get_rating() for el in players_list)
 
 
+def elves_concert(elves: list[Elf]) -> str:
+    return [el.play_elf_song() for el in elves]
 
-class Elf(Player):
-    def __init__(self, nickname: str, musical_instrument: str) -> None:
-        super().__init__(nickname)
-        self._musical_instrument  = musical_instrument
 
-    def gat_rating(self):
-        ...
-
-    def player_info(self) -> None:
-        ...
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
-
-class Dwarf(Player):
-    def __init__(self, nickname: str, favourite_dish: str) -> None:
-        super().__init__(nickname)
-        self._favourite_dish = favourite_dish
-
-    def gat_rating(self):
-        ...
-
-    def player_info(self) -> None:
-        ...
-
-    def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} is eating {self._favourite_dish}")
-
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> str:
+    return [el.eat_favourite_dish() for el in dwarves]
