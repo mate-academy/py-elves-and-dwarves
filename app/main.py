ab31fc9c63a2bc815dfdf6e3d87f1c49535f5c83
Artem <artemp2208@gmail.com>
2025-11-06 14:17:45 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,48 +1,18 @@
 from typing import List
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(team: List[object]) -> int:
+def calculate_team_total_rating(team: List[Player]) -> int:
     return sum(player.get_rating() for player in team)
 
 
-def elves_concert(elves: List[object]) -> None:
+def elves_concert(elves: List[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: List[object]) -> None:
+def feast_of_the_dwarves(dwarves: List[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
-
-
-if __name__ == "__main__":
-    ranger = ElfRanger("Nardual Chaekian", "flute", 7)
-    print(ranger.get_rating())  # 21
-    print(ranger.player_info())
-    ranger.play_elf_song()
-
-    warrior = DwarfWarrior("Thiddeal", "French Fries", 7)
-    print(warrior.get_rating())  # 11
-    print(warrior.player_info())
-    warrior.eat_favourite_dish()
-
-    team = [
-        Druid("Druid", "flute", "ABC"),
-        ElfRanger("Ranger", "trumpet", 33),
-    ]
-    print(calculate_team_total_rating(team))  # 102
-
-    elves = [
-        Druid("Nardual", "flute", "aaa"),
-        ElfRanger("Rothilion", "trumpet", 33),
-    ]
-    elves_concert(elves)
-
-    dwarves = [
-        DwarfWarrior("Thiddeal", "French Fries", 3),
-        DwarfWarrior("Dwarf", "Caesar Salad", 3),
-    ]
-    feast_of_the_dwarves(dwarves)
