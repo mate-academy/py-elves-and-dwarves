b9e5062f85680f96c779e11d94874c3cfb047a66
Ievgen German <german.kbrza@gmail.com>
2025-11-07 16:25:46 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+class Vehicle:
+    def __init__(self, brand: str):
+        self.brand = brand  # Public
+        self._model = "Unknown"  # Protected
+        self.__year = 2024  # Private
+
+    def get_year(self):
+      return self.__year
+
+
+class Car(Vehicle):
+
+    def show_info(self):
+        print(f"Brand: {self.brand}")  # ✅ Public доступний
+        print(f"Model: {self._model}")  # ✅ Protected доступний
+        #print(f"Year: {self.__year}")      # ❌ Private НЕ доступний
+        print(f"Year: {self.get_year()}")  # ✅ Через метод батьківського класу
+
+
+car = Car("Toyota")
+car.show_info()
