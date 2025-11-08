7b99a140478524851386ded59076b7bd08603c31
Ievgen German <german.kbrza@gmail.com>
2025-11-08 15:27:55 +0200

    solution 1

--- app/main.py
+++ app/main.py
@@ -1,21 +1,12 @@
-class Vehicle:
-    def __init__(self, brand: str):
-        self.brand = brand  # Public
-        self._model = "Unknown"  # Protected
-        self.__year = 2024  # Private
-
-    def get_year(self):
-      return self.__year
+def calculate_team_total_rating(list_of_players: list) -> int:
+    return sum(players.get_rating() for players in list_of_players)
 
 
-class Car(Vehicle):
-
-    def show_info(self):
-        print(f"Brand: {self.brand}")  # ✅ Public доступний
-        print(f"Model: {self._model}")  # ✅ Protected доступний
-        #print(f"Year: {self.__year}")      # ❌ Private НЕ доступний
-        print(f"Year: {self.get_year()}")  # ✅ Через метод батьківського класу
+def elves_concert(list_of_elf: list) -> None:
+    for elf in list_of_elf:
+        elf.play_elf_song()
 
 
-car = Car("Toyota")
-car.show_info()
+def feast_of_the_dwarves(list_of_the_dwarves: list) -> None:
+    for dwarves in list_of_the_dwarves:
+        dwarves.eat_favourite_dish()
