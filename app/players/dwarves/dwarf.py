b2f61a0dcaf6bfde19169ea9aafb88d2789b9132
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-08 11:30:18 +0100

    quest

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from ..player import Player
+
+
+class Dwarf(Player, ABC):
+
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self.__favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self.__favourite_dish}")
