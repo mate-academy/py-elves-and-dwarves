ec902a7a0da743e7d91ba3b60f768607d11377e4
Paweł Dudek <dudek136@gmail.com>
2025-11-10 15:19:12 +0100

    Sulotiona

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(player_list: list) -> int:
+    return sum([player.get_rating() for player in player_list])
+
+
+def elves_concert(elves_list: list) -> None:
+    for elf in elves_list:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves_list: list) -> None:
+    for dwarf in dwarves_list:
+        dwarf.eat_favourite_dish()
