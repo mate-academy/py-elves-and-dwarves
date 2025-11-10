f68b02c64e26e7e4233c73e2411d963e4c71ed5d
Paweł Dudek <dudek136@gmail.com>
2025-11-10 15:25:44 +0100

    Sulotiona v.2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def calculate_team_total_rating(player_list: list) -> int:
-    return sum([player.get_rating() for player in player_list])
+    return sum(player.get_rating() for player in player_list)
 
 
 def elves_concert(elves_list: list) -> None:
