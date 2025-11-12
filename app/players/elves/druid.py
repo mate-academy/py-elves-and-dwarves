5c9f11b39de8a1a74cd087a5d07fc78f1b02a035
Yura Valko <yuravalko166@gmail.com>
2025-11-12 20:44:03 +0200

    fix elf

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -2,12 +2,14 @@ from app.players.elves.elf import Elf
 
 
 class Druid(Elf):
-    def __init__(self, nickname: str, musical_instrument: str, favorite_spell: str) -> None:
+    def __init__(self, nickname: str, musical_instrument: str,
+                 favorite_spell: str) -> None:
         super().__init__(nickname, musical_instrument)
         self.favorite_spell = favorite_spell
 
     def player_info(self) -> str:
-        return f"Druid{self.nickname}. {self.nickname} has a favorite spell: {self.favorite_spell}"
+        return (f"Druid{self.nickname}. {self.nickname} has a favorite spell:"
+                f" {self.favorite_spell}")
 
     def get_rating(self) -> int:
         return len(self.favorite_spell)
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,10 +1,13 @@
 from app.players.player import Player
 from abc import ABC
 
+
 class Elf(Player, ABC):
+
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self.musical_instrument = musical_instrument
 
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song on the {self.musical_instrument}")
+        print(f"{self.nickname} "
+              f"is playing a song on the {self.musical_instrument}")
