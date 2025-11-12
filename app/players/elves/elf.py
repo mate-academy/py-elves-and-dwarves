6519d5a4428be7e3fda9f5787632de0176e5ea30
Yura Valko <yuravalko166@gmail.com>
2025-11-12 20:31:05 +0200

    Solution

---
+++ app/players/elves/elf.py
@@ -0,0 +1,10 @@
+from app.players.player import Player
+from abc import ABC
+
+class Elf(Player, ABC):
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self.musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self.musical_instrument}")
