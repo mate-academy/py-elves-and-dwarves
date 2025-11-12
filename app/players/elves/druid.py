943a5dbc4eb648d89f78605992eaabe7f3a1b44f
Yura Valko <yuravalko166@gmail.com>
2025-11-12 21:37:58 +0200

    please2

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -8,8 +8,9 @@ class Druid(Elf):
         self.favourite_spell = favourite_spell
 
     def player_info(self) -> str:
-        return (f"Druid {self.nickname}. {self.nickname} has a favourite spell:"
-                f" {self.favourite_spell}")
+        return (f"Druid {self.nickname}. "
+                f"{self.nickname} has a favourite spell:"
+                f"{self.favourite_spell}")
 
     def get_rating(self) -> int:
         return len(self.favourite_spell)
