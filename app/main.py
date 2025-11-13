7063b59b35f6d72d38aba74a6f9c17a3e6f7b844
Vova <vovakucin082@gmail.com>
2025-11-13 11:31:09 +0100

    Code write

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(ls_players: list) -> int:
+    return sum([i.get_rating() for i in ls_players])
+
+
+def elves_concert(ls_elf: list) -> None:
+    for elf in ls_elf:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(ls_dwarf: list) -> None:
+    for dwarf in ls_dwarf:
+        dwarf.eat_favourite_dish()
