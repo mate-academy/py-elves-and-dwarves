78ac7e7894221eca167c6f9f421b61f38e7cec88
Farlinder <sashakost2006e@gmail.com>
2025-11-27 20:21:17 +0100

    Solving2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 def calculate_team_total_rating(team: list) -> float:
-    total_rating = 0
-    for player in team:
-        total_rating += player.get_rating()
-    return total_rating
+    return sum([player.get_rating() for player in team])
 
 
 def elves_concert(elves: list) -> None:
-    for elves in elves:
-        elves.play_elf_song()
+    for elf in elves:
+        elf.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves: list) -> None:
-    for dwarves in dwarves:
-        dwarves.eat_favourite_dish()
+    for dwarv in dwarves:
+        dwarv.eat_favourite_dish()
