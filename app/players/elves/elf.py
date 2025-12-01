73182dacf5911931d8f59ac215af2a1ecd9f1304
Oleh Klivanets <oleg.klivanets@gmail.com>
2025-12-01 19:40:19 +0200

    solution

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -2,10 +2,11 @@ from app.players.player import Player
 
 
 class Elf(Player):
-    def __init__(self,
-                 nickname: str,
-                 musical_instrument: str
-                 ) -> None:
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str
+    ) -> None:
         super().__init__(nickname)
 
         self._musical_instrument = musical_instrument
