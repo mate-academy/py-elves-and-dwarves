3da554953975530b9b4d8c8782cfb6b1a6812bb5
Bielzin <gabriel.cabecao.lol@gmail.com>
2025-12-04 22:09:45 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+from .players.elves.elf_ranger import ElfRanger
+from .players.dwarves.dwarf_warrior import DwarfWarrior
+
+
+def calculate_team_total_rating(team: list) -> int:
+    return sum(player.get_rating() for player in team)
+
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
+
+
+if __name__ == "__main__":
+    ranger = ElfRanger(
+        nickname="Nardual Chaekian",
+        musical_instrument="flute",
+        bow_level=7
+    )
+    print(ranger.get_rating())
+    print(ranger.player_info())
+    ranger.play_elf_song()
+
+    warrior = DwarfWarrior(
+        nickname="Thiddeal",
+        favourite_dish="French Fries",
+        hummer_level=7
+    )
+    print(warrior.get_rating())
+    print(warrior.player_info())
+    warrior.eat_favourite_dish()
