3d95821235aaf25dfec2de176fc1af6e3ce539f1
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-12-10 19:11:32 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
+from typing import List
+
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 from app.players.player import Player
 
-def calculate_team_total_rating(players: list[Player]):
+def calculate_team_total_rating(players: List[Player]):
     return sum(player.get_rating() for player in players)
 
-def elves_concert(elves: list[Elf]):
+def elves_concert(elves: List[Elf]):
     for elf in elves:
         elf.play_elf_song()
 
-def feast_of_the_dwarves(dwarves: list[Dwarf]):
+def feast_of_the_dwarves(dwarves: List[Dwarf]):
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
