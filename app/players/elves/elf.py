2c79c12d1568d54c1bbb17e78811cd56fcd1b344
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-26 18:03:07 -0500

    fixed str

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -10,4 +10,4 @@ class Elf(Player):
 
     def play_elf_song(self) -> None:
         print((f"{self.nickname} is playing a song "
-               f" on the {self._musical_instrument}"))
+               f"on the {self._musical_instrument}"))
