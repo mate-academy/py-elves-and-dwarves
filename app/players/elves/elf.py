1fa5d73d478e0ced06b4135be77f213880983c56
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2025-12-31 14:03:50 -0500

    'Solution'

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -11,19 +11,3 @@ class Elf(Player, ABC):
     def play_elf_song(self) -> None:
         print(f"{self.nickname}"
               f" is playing a song on the {self._musical_instrument}")
-
-
-class ElfRanger(Elf):
-    def __init__(self, nickname: str, musical_instrument: str,
-                 bow_level: int) -> None:
-        super().__init__(nickname, musical_instrument)
-        self._bow_level = bow_level
-
-    def player_info(self) -> str:
-        return (
-            f"Elf ranger {self.nickname}. "
-            f"{self.nickname} has bow of the {self._bow_level} level"
-        )
-
-    def get_rating(self) -> int:
-        return 3 * self._bow_level
