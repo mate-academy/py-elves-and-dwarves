2589866d6441ff38cb5794781d5856a76cef2d4b
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2025-12-31 13:57:07 -0500

    'Solution'

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -11,32 +11,3 @@ class Dwarf(Player, ABC):
     def eat_favourite_dish(self) -> None:
         print(f"{self.nickname} is eating {self._favourite_dish}")
 
-
-class DwarfWarrior(Dwarf):
-    def __init__(self, nickname: str, favourite_dish: str,
-                 hummer_level: int) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._hummer_level = hummer_level
-
-    def player_info(self) -> str:
-        return (
-            f"Dwarf warrior {self.nickname}. "
-            f"{self.nickname} has a hummer of the {self._hummer_level} level"
-        )
-
-    def get_rating(self) -> int:
-        return self._hummer_level + 4
-
-
-class DwarfBlacksmith(Dwarf):
-    def __init__(self, nickname: str, favourite_dish: str,
-                 skill_level: int) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._skill_level = skill_level
-
-    def player_info(self) -> str:
-        return (f"Dwarf blacksmith {self.nickname}"
-                f" with skill of the {self._skill_level} level")
-
-    def get_rating(self) -> int:
-        return self._skill_level
