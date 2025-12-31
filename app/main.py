370aebcc5f6437c335eefb398d02831bdc22c075
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2025-12-31 13:09:29 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -94,15 +94,18 @@ class DwarfBlacksmith(Dwarf):
     def get_rating(self) -> int:
         return self._skill_level
 
+
 def calculate_team_total_rating(players: list[Player]) -> int:
     total_rating = 0
     for player in players:
         total_rating += player.get_rating()
     return total_rating
 
+
 def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
+
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
