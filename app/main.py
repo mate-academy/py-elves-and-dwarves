caed9b5d002ff6d1f31d8bed321a1592bd742193
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-01-02 21:32:17 +0000

    1st save

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def calculate_team_total_rating(team: list) -> int:
+    result = 0
+    for player in team:
+        result += player.get_rating()
+    return result
+
+def elves_concert(elves: list) -> None:
+    for elf in elves:
+        elf.play_elf_song()
+
+def feast_of_the_dwarves(dwarves: list) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
