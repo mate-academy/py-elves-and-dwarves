13f2c1d578a93014c358296dd78f716ed0467adc
Kateryna Ivanenko <kateryna578ivanenko@gmail.com>
2026-01-03 17:23:19 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 def calculate_team_total_rating(players_list: list) -> int:
-    total_rating = 0
-    for player in players_list:
-        total_rating += player.get_rating()
-    return total_rating
+    return sum(player.get_rating() for player in players_list)
 
 
 def elves_concert(elves_list: list) -> None:
