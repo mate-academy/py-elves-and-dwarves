7b3af2c16652e74e637c6a9085898029c539dd33
Alex <alex@example.com>
2026-01-05 12:04:59 +0200

    Finish 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+
+
+def calculate_team_total_rating(team: list) -> int:
+    s = 0
+    for i in team:
+        s += i.get_rating()
+    return s
+
+
+def elves_concert(elves: list) -> str:
+    for i in elves:
+        i.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> str:
+    for i in dwarves:
+        i.eat_favourite_dish()
---
+++ app/players/dwarves/dwarf_warrior.py
@@ -0,0 +1,19 @@
+from app.players.dwarves.dwarf import Dwarf
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(self,
+                 nickname: str,
+                 favourite_dish: str,
+                 hummer_level: str
+                 ) -> None:
+        self.hummer_level = hummer_level
+        super().__init__(nickname, favourite_dish)
+
+    def get_rating(self) -> int:
+        return self.hummer_level + 4
+
+    def player_info(self) -> str:
+        return (f"Dwarf warrior {self.nickname}. {self.nickname} "
+                f"has a hummer of the {self.hummer_level} level"
+                )
