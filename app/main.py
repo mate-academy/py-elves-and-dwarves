34564a5686b99546496c421d74cae0beb1535344
Bohdan Borsuk <barsuchellos@gmail.com>
2026-01-05 20:28:08 +0000

    fixed some main mistakes

--- app/main.py
+++ app/main.py
@@ -5,17 +5,14 @@ from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(list_of_ratings: list[Player]) -> int:
-    total_rating = 0
-    for rating in list_of_ratings:
-        total_rating += rating.get_rating()
-    return total_rating
+    return sum(player.get_rating() for player in list_of_ratings)
 
 
-def elves_concert(elves_list: list[Elf]) -> None:
+def elves_concert(elves_list: list) -> None:
     for elv in elves_list:
         elv.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarf_list: list[Dwarf]) -> None:
+def feast_of_the_dwarves(dwarf_list: list) -> None:
     for dwarf in dwarf_list:
         dwarf.eat_favourite_dish()
