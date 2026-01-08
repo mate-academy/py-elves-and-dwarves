8ca0f8e461517c69c473e1ad870e099f76907c18
Yurii Medvid <medvedimedved16@gmail.com>
2026-01-08 17:29:58 +0100

    solution#2

--- app/main.py
+++ app/main.py
@@ -1,24 +1,17 @@
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
+from app.players.player import Player
 
 
-def calculate_team_total_rating(
-        team: list[ElfRanger, Druid, DwarfWarrior, DwarfBlacksmith]
-) -> int:
+def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(person.get_rating() for person in team)
 
 
-def elves_concert(team: list[ElfRanger | Druid]) -> None:
+def elves_concert(team: list[Elf]) -> None:
     for person in team:
-        if isinstance(person, (Druid, ElfRanger)):
-            person.play_elf_song()
-        continue
+        person.play_elf_song()
 
 
-def feast_of_the_dwarves(team: list[DwarfWarrior | DwarfBlacksmith]) -> None:
+def feast_of_the_dwarves(team: list[Dwarf]) -> None:
     for person in team:
-        if isinstance(person, (DwarfBlacksmith, DwarfWarrior)):
-            person.eat_favourite_dish()
-        continue
+        person.eat_favourite_dish()
