e4ad152a3fbd8dbf2cc66b1ef982d803881f01e6
Yatsenko Dmytro <sonituensem@gmail.com>
2026-01-08 13:10:19 +0200

    fixed blank line issue

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,6 +1,7 @@
 from app.players.player import Player
 from abc import ABC
 
+
 class Dwarf(Player, ABC):
     def __init__(self, nickname: str, favourite_dish: str) -> None:
         super().__init__(nickname)
