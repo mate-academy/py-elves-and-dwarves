dd924a0ee6aac521635a2763fd94cee8be0f3b4f
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-01-08 11:33:15 +0200

    covered requirements

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def calculate_team_total_rating(player_list: list) -> int:
+    return sum(player.get_rating() for player in player_list)
+
+
+def elves_concert(elf_list: list) -> None:
+    for elf in elf_list:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarf_list: list) -> None:
+    for dwarf in dwarf_list:
+        dwarf.eat_favourite_dish()
