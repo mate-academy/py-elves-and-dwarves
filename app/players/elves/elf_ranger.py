ffe8e6d754fad97ca429d2fe5f11938dadc35d37
Yatsenko Dmytro <sonituensem@gmail.com>
2026-01-08 13:00:05 +0200

    Fixed missed annotations

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -15,5 +15,5 @@ class ElfRanger(Elf):
             f"Elf ranger {self.nickname}."
             f" {self.nickname} has bow of the {self._bow_level} level")
 
-    def get_rating(self):
+    def get_rating(self) -> int:
         return 3 * self._bow_level
